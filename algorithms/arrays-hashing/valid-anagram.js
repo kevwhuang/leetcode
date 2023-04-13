30f32da47bd8db96df4006b800164c6d263c0471
Kevin Huang <kevwhuang@gmail.com>
2023-04-13 16:34:48 -0500

    Finished isAnagram & groupAnagrams

---
+++ algorithms/arrays-hashing/valid-anagram.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+module.exports = function isAnagram(s, t) {
+    return s.split('').sort().join('') === t.split('').sort().join('');
+}
--- test/arrays-hashing/contains-duplicate.test.js
+++ test/arrays-hashing/contains-duplicate.test.js
@@ -2,14 +2,14 @@ const containsDuplicate = require('../../algorithms/arrays-hashing/contains-dupl
 
 describe('Main', () => {
     test('Case 1', () => {
-        expect(containsDuplicate([1, 2, 3, 1])).toStrictEqual(true);
+        expect(containsDuplicate([1, 2, 3, 1])).toBeTruthy();
     });
 
     test('Case 2', () => {
-        expect(containsDuplicate([1, 2, 3, 4])).toStrictEqual(false);
+        expect(containsDuplicate([1, 2, 3, 4])).toBeFalsy();
     });
 
     test('Case 3', () => {
-        expect(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])).toStrictEqual(true);
+        expect(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])).toBeTruthy();
     });
 });
