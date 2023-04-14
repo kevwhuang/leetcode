cc40e63da662b3d9bf5d36c5b60912ee6a8b6d42
Kevin Huang <kevwhuang@gmail.com>
2023-04-13 23:34:31 -0500

    Finished productExceptSelf and longestConsecutive

---
+++ algorithms/arrays-hashing/product-of-array-except-self.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+module.exports = function productExceptSelf(nums) {
+    const L = nums.length;
+    const output = [];
+    let zeroExists = false;
+    let product = 1;
+    let zeroCount = 0;
+    for (let i = 0; i < L; i++) {
+        if (nums[i]) product *= nums[i];
+        else {
+            zeroExists = true;
+            zeroCount++;
+        }
+    }
+    if (zeroCount > 1) return Array(L).fill(0);
+    for (let i = 0; i < L; i++) {
+        if (!zeroExists && nums[i]) output[i] = product * nums[i] ** -1;
+        else nums[i] ? output[i] = 0 : output[i] = product;
+    }
+    return output;
+};
