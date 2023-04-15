cb41963fc379ba41ff40bbe069fd25cbe6516000
Kevin Huang <kevwhuang@gmail.com>
2023-04-15 04:14:14 -0500

    Finished maxProfit

---
+++ test/sliding-window/best-time-to-buy-and-sell-stock.test.js
@@ -0,0 +1,11 @@
+const maxProfit = require('../../algorithms/sliding-window/best-time-to-buy-and-sell-stock');
+
+describe('Main', () => {
+    test('Case 1', () => {
+        expect(maxProfit([7, 1, 5, 3, 6, 4])).toStrictEqual(5);
+    });
+
+    test('Case 2', () => {
+        expect(maxProfit([7, 6, 4, 3, 1])).toStrictEqual(0);
+    });
+});
