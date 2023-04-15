f007e5b8eca3897aeccc87306f6f29513fa1c140
Kevin Huang <kevwhuang@gmail.com>
2023-04-15 03:31:15 -0500

    Finished KthLargest

---
+++ test/heap-priority-queue/kth-largest-element-in-a-stream.test.js
@@ -0,0 +1,13 @@
+const KthLargest = require('../../algorithms/heap-priority-queue/kth-largest-element-in-a-stream');
+
+describe('Main', () => {
+    test('Case 1', () => {
+        const kth = new KthLargest(3, [4, 5, 8, 2]);
+
+        expect(kth.add(3)).toStrictEqual(4);
+        expect(kth.add(5)).toStrictEqual(5);
+        expect(kth.add(10)).toStrictEqual(5);
+        expect(kth.add(9)).toStrictEqual(8);
+        expect(kth.add(4)).toStrictEqual(8);
+    });
+});
