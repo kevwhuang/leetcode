66513bcc4e99ae46d28e4dc1dd560688e849f4c1
Kevin Huang <kevwhuang@gmail.com>
2023-04-30 17:32:38 -0500

    Finished countSubstrings

---
+++ test/1d-dynamic-programming/palindromic-substrings.test.js
@@ -0,0 +1,11 @@
+const countSubstrings = require('../../algorithms/1d-dynamic-programming/palindromic-substrings');
+
+describe('Main', () => {
+    test('Case 1', () => {
+        expect(countSubstrings('abc')).toStrictEqual(3);
+    });
+
+    test('Case 2', () => {
+        expect(countSubstrings('aaa')).toStrictEqual(6);
+    });
+});
