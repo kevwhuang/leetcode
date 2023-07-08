a9305a408a00ee64716c61d3097cf8aa6fa9ec3c
Kevin Huang <kevwhuang@gmail.com>
2023-07-08 16:28:10 -0500

    Revisited isAnagram

--- algorithms/arrays-hashing/valid-anagram.js
+++ algorithms/arrays-hashing/valid-anagram.js
@@ -5,5 +5,14 @@
  */
 
 module.exports = function isAnagram(s, t) {
-    return s.split('').sort().join('') === t.split('').sort().join('');
+    if (s.length !== t.length) return false;
+    const map = new Map();
+    for (const letter of s) {
+        map.set(letter, map.get(letter) + 1 || 1);
+    }
+    for (const letter of t) {
+        if (!map.get(letter)) return false;
+        map.set(letter, map.get(letter) - 1);
+    }
+    return true;
 };
