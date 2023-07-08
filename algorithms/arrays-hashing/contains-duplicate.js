2f3823b3e7eabac837d28349989efbc12097c3aa
Kevin Huang <kevwhuang@gmail.com>
2023-07-08 12:05:00 -0500

    Revisited containsDuplicate

--- algorithms/arrays-hashing/contains-duplicate.js
+++ algorithms/arrays-hashing/contains-duplicate.js
@@ -4,7 +4,5 @@
  */
 
 module.exports = function containsDuplicate(nums) {
-    nums.sort();
-    for (let i = 0; i < nums.length; i++) if (nums[i] === nums[i + 1]) return true;
-    return false;
+    return new Set(nums).size !== nums.length;
 };
