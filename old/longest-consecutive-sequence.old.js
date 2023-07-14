8456a86c05fedefd48d29b2754c76ac2a4fcaf33
Kevin Huang <kevwhuang@gmail.com>
2023-07-14 16:21:20 -0500

    Revisited longestConsecutive

--- old/longest-consecutive-sequence.old.js
+++ old/longest-consecutive-sequence.old.js
@@ -4,11 +4,12 @@
  */
 
 module.exports = function longestConsecutive(nums) {
-    if (!nums.length) return 0;
+    const L = nums.length;
     let max = 1;
     let tally = 1;
+    if (!L) return 0;
     nums.sort((a, b) => a - b);
-    for (let i = 0; i < nums.length - 1; i++) {
+    for (let i = 0; i < L; i++) {
         if (nums[i] === nums[i + 1]) continue;
         nums[i] + 1 === nums[i + 1] ? tally++ : tally = 1;
         if (tally > max) max = tally;
