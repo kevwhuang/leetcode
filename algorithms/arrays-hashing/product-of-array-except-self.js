c52883506436adf4a0bf9ddc8eadf0210ce6614b
Kevin Huang <kevwhuang@gmail.com>
2023-07-14 16:02:17 -0500

    Revisited productExceptSelf

--- algorithms/arrays-hashing/product-of-array-except-self.js
+++ algorithms/arrays-hashing/product-of-array-except-self.js
@@ -3,23 +3,19 @@
  * @return {number[]}
  */
 
-module.exports = function productExceptSelf(nums) {
-    const L = nums.length;
-    const output = [];
-    let zeroExists = false;
-    let product = 1;
-    let zeroCount = 0;
-    for (let i = 0; i < L; i++) {
-        if (nums[i]) product *= nums[i];
-        else {
-            zeroExists = true;
-            zeroCount++;
-        }
+module.exports = productExceptSelf;
+
+function productExceptSelf(nums) {
+    const result = [];
+    let leftMult = 1;
+    let rightMult = 1;
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = leftMult;
+        leftMult *= nums[i];
     }
-    if (zeroCount > 1) return Array(L).fill(0);
-    for (let i = 0; i < L; i++) {
-        if (!zeroExists && nums[i]) output[i] = product * nums[i] ** -1;
-        else nums[i] ? output[i] = 0 : output[i] = product;
+    for (let i = nums.length - 1; i > 0; i--) {
+        rightMult *= nums[i];
+        result[i - 1] *= rightMult;
     }
-    return output;
+    return result;
 };
