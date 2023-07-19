5aa299869c496a471a8cc70abe61fcb2036653a6
Kevin Huang <kevwhuang@gmail.com>
2023-07-19 14:43:09 -0500

    LeetCode beginner's guide part 2

--- leetcode/the-leetcode-beginner's-guide.js
+++ leetcode/the-leetcode-beginner's-guide.js
@@ -2,3 +2,11 @@
 function sum(num1, num2) {
     return num1 + num2;
 }
+
+// 1480 - Running Sum of 1d Array
+function runningSum(nums) {
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] += nums[i - 1];
+    }
+    return nums;
+}
