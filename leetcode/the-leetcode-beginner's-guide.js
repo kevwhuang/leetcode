26ad21683084a45c22222ecfbdfa33b78fc7a233
Kevin Huang <kevwhuang@gmail.com>
2023-07-22 23:58:31 -0500

    LeetCode beginner's guide part 3

--- leetcode/the-leetcode-beginner's-guide.js
+++ leetcode/the-leetcode-beginner's-guide.js
@@ -10,3 +10,12 @@ function runningSum(nums) {
     }
     return nums;
 }
+
+// 1672 - Richest Customer Wealth
+function maximumWealth(accounts) {
+    let max = 0;
+    for (const customer of accounts) {
+        max = Math.max(customer.reduce((s, i) => s + i, 0), max);
+    }
+    return max;
+}
