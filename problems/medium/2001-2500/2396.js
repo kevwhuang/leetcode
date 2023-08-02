efdf5251486398125291c283e3a0f5c341e1f241
Kevin Huang <kevwhuang@gmail.com>
2023-08-02 02:29:45 -0500

    Challenges 535/1282/1329/2396

---
+++ problems/medium/2001-2500/2396.js
@@ -0,0 +1,5 @@
+// 2396 - Strictly Palindromic Number
+
+function isStrictlyPalindromic(n) {
+    return false;
+}
---
+++ problems/medium/501-1000/535.js
@@ -0,0 +1,9 @@
+// 535 - Encode and Decode TinyURL
+
+function encode(longUrl) {
+    return longUrl;
+}
+
+function decode(shortUrl) {
+    return shortUrl;
+}
