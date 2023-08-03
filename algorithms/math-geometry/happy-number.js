dd44bcc913750b4a8dc6fd5a5c14149fd43487b1
Kevin Huang <kevwhuang@gmail.com>
2023-08-03 14:29:18 -0500

    Challenges 67/69/100

--- algorithms/math-geometry/happy-number.js
+++ algorithms/math-geometry/happy-number.js
@@ -7,15 +7,15 @@ module.exports = isHappy;
 
 function isHappy(n) {
     const set = new Set([n]);
-    let previous = n;
-    while (previous !== 1) {
+    while (n !== 1) {
+        const str = String(n);
         let sum = 0;
-        for (const digit of String(previous)) {
-            sum += digit ** 2;
+        for (let i = 0; i < str.length; i++) {
+            sum += str[i] ** 2;
         }
         if (set.has(sum)) return false;
         set.add(sum);
-        previous = sum;
+        n = sum;
     }
     return true;
 }
