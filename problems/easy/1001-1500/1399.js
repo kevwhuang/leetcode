67cbb66a691993e2a33289a06d39ec989d0677b0
Kevin Huang <kevwhuang@gmail.com>
2023-08-05 20:29:26 -0500

    Challenges 169/1399/1742/2154/2363

---
+++ problems/easy/1001-1500/1399.js
@@ -0,0 +1,23 @@
+// 1399 - Count Largest Group
+
+function countLargestGroup(n) {
+    const map = new Map();
+    let maxSum = 1,
+        maxGroups = [];
+    for (let i = 1; i <= n; i++) {
+        let num = i,
+            sum = 0;
+        while (num) {
+            sum += num % 10;
+            num = Math.floor(num / 10);
+        }
+        map.set(sum, map.get(sum) + 1 || 1);
+        if (map.get(sum) === maxSum) {
+            maxGroups.push(i);
+        } else if (map.get(sum) > maxSum) {
+            maxSum++;
+            maxGroups = [i];
+        }
+    }
+    return maxGroups.length;
+}
---
+++ problems/easy/2001-2500/2363.js
@@ -0,0 +1,14 @@
+// 2363 - Merge Similar Items
+
+function mergeSimilarItems(items1, items2) {
+    const obj = {};
+    for (let i = 0; i < items1.length; i++) {
+        const item = items1[i];
+        obj[item[0]] = (obj[item[0]] || 0) + item[1];
+    }
+    for (let i = 0; i < items2.length; i++) {
+        const item = items2[i];
+        obj[item[0]] = (obj[item[0]] || 0) + item[1];
+    }
+    return Object.entries(obj);
+}
