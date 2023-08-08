2a7e8ef88683b2da4ea781e293a4cdf9045fec78
Kevin Huang <kevwhuang@gmail.com>
2023-08-08 07:02:29 -0500

    NC MedianFinder

--- neetcode/heap-priority-queue/find-median-from-data-stream.js
+++ neetcode/heap-priority-queue/find-median-from-data-stream.js
@@ -2,7 +2,8 @@
 
 class MedianFinder {
     constructor() {
-
+        this.heap1 = new PriorityQueue({ compare: (a, b) => b - a });
+        this.heap2 = new PriorityQueue({ compare: (a, b) => a - b });
     }
 
     /**
@@ -11,7 +12,10 @@ class MedianFinder {
      */
 
     addNum(num) {
-
+        const heap1 = this.heap1, heap2 = this.heap2;
+        heap2.enqueue(num);
+        heap1.enqueue(heap2.dequeue());
+        if (heap2.size() < heap1.size()) heap2.enqueue(heap1.dequeue());
     }
 
     /**
@@ -19,7 +23,9 @@ class MedianFinder {
      */
 
     findMedian() {
-
+        const heap1 = this.heap1, heap2 = this.heap2;
+        if (heap1.size() !== heap2.size()) return heap2.front();
+        return (heap1.front() + heap2.front()) / 2;
     }
 }
 
--- problems/hard/1-500/295.js
+++ problems/hard/1-500/295.js
@@ -1,3 +1,19 @@
 // 295 - Find Median From Data Stream
 
-
+class MedianFinder {
+    constructor() {
+        this.heap1 = new PriorityQueue({ compare: (a, b) => b - a });
+        this.heap2 = new PriorityQueue({ compare: (a, b) => a - b });
+    }
+    addNum(num) {
+        const heap1 = this.heap1, heap2 = this.heap2;
+        heap2.enqueue(num);
+        heap1.enqueue(heap2.dequeue());
+        if (heap2.size() < heap1.size()) heap2.enqueue(heap1.dequeue());
+    }
+    findMedian() {
+        const heap1 = this.heap1, heap2 = this.heap2;
+        if (heap1.size() !== heap2.size()) return heap2.front();
+        return (heap1.front() + heap2.front()) / 2;
+    }
+}
