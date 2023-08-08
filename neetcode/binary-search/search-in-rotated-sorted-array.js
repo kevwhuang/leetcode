a282eefb702f22b3afa596e8a0ef18ae96ddaf38
Kevin Huang <kevwhuang@gmail.com>
2023-08-08 01:31:19 -0500

    Revisited search (2)

---
+++ neetcode/binary-search/search-in-rotated-sorted-array.js
@@ -0,0 +1,13 @@
+// 33 - Search in Rotated Sorted Array
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+function search(nums, target) {
+    return nums.indexOf(target);
+}
+
+module.exports = search;
