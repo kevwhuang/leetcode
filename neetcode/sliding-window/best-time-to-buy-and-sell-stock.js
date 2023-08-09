40633d907637d5fdef7850c030c3f0be095f8047
Kevin Huang <kevwhuang@gmail.com>
2023-08-08 21:46:33 -0500

    Revisited search & maxProfit

---
+++ neetcode/sliding-window/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,18 @@
+// 121 - Best Time to Buy and Sell Stock
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+function maxProfit(prices) {
+    let buyPrice = prices[0],
+        maxProfit = 0;
+    for (let i = 1; i < prices.length; i++) {
+        prices[i] < buyPrice && (buyPrice = prices[i]);
+        prices[i] - buyPrice > maxProfit && (maxProfit = prices[i] - buyPrice);
+    }
+    return maxProfit;
+}
+
+module.exports = maxProfit;
