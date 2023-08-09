0d7d9c49fc203dbdb0a2fefeeba76ba2d65ab33e
Kevin Huang <kevwhuang@gmail.com>
2023-08-09 09:13:42 -0500

    NC mergeKLists

--- neetcode/linked-list/merge-k-sorted-lists.js
+++ neetcode/linked-list/merge-k-sorted-lists.js
@@ -6,7 +6,37 @@
  */
 
 function mergeKLists(lists) {
+    let size = 0, cur;
+    for (let i = 0; i < lists.length; i++) {
+        cur = lists[i];
+        while (cur) {
+            size++;
+            cur = cur.next;
+        }
+    }
+    const arr = new Int16Array(size);
+    for (let i = 0, j = 0; i < lists.length; i++) {
+        cur = lists[i];
+        while (cur) {
+            arr[j++] = cur.val;
+            cur = cur.next;
+        }
+    }
+    arr.sort();
+    const sentinel = new ListNode();
+    cur = sentinel;
+    for (let i = 0; i < arr.length; i++) {
+        cur.next = new ListNode(arr[i]);
+        cur = cur.next;
+    }
+    return sentinel.next;
+}
 
+class ListNode {
+    constructor(val, next) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
 }
 
 module.exports = mergeKLists;
--- problems/hard/1-500/23.js
+++ problems/hard/1-500/23.js
@@ -1,3 +1,28 @@
 // 23 - Merge K Sorted Lists
 
-
+function mergeKLists(lists) {
+    let size = 0, cur;
+    for (let i = 0; i < lists.length; i++) {
+        cur = lists[i];
+        while (cur) {
+            size++;
+            cur = cur.next;
+        }
+    }
+    const arr = new Int16Array(size);
+    for (let i = 0, j = 0; i < lists.length; i++) {
+        cur = lists[i];
+        while (cur) {
+            arr[j++] = cur.val;
+            cur = cur.next;
+        }
+    }
+    arr.sort();
+    const sentinel = new ListNode();
+    cur = sentinel;
+    for (let i = 0; i < arr.length; i++) {
+        cur.next = new ListNode(arr[i]);
+        cur = cur.next;
+    }
+    return sentinel.next;
+}
