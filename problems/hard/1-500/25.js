b2fbf3855d45eb068bab2b8838046ee2d340b85f
Kevin Huang <kevwhuang@gmail.com>
2023-08-10 09:30:22 -0500

    NC reverseKGroup

--- problems/hard/1-500/25.js
+++ problems/hard/1-500/25.js
@@ -1,3 +1,19 @@
 // 25 - Reverse Nodes in K-Group
 
-
+function reverseKGroup(head, k) {
+    const vals = new Array(k);
+    let slow = head, fast = head;
+    while (fast) {
+        let i = k - 1;
+        while (fast && ~i) {
+            vals[i--] = fast.val;
+            fast = fast.next;
+        }
+        if (i !== -1) break;
+        for (let i = 0; i < k; i++) {
+            slow.val = vals[i];
+            slow = slow.next;
+        }
+    }
+    return head;
+}
