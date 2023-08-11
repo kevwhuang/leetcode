de987097babc97c40057f87bd6e06dbb2b009b44
Kevin Huang <kevwhuang@gmail.com>
2023-08-11 17:19:18 -0500

    NC solveNQueens

--- problems/hard/1-500/51.js
+++ problems/hard/1-500/51.js
@@ -1,3 +1,27 @@
 // 51 - N-Queens
 
-
+function solveNQueens(n) {
+    function backtrack(r) {
+        if (r === n) return res.push(board.map(row => row.join('')));
+        for (let c = 0; c < n; c++) {
+            if (invalidCol(0, c)) continue;
+            if (invalidDiag(r, c, -1)) continue;
+            if (invalidDiag(r, c, 1)) continue;
+            board[r][c] = 'Q';
+            backtrack(r + 1);
+            board[r][c] = '.';
+        }
+    }
+    function invalidCol(r, c) {
+        while (r < n) if (board[r++][c] === 'Q') return true;
+    }
+    function invalidDiag(r, c, d) {
+        while (~--r && ~c) if (board[r][c += d] === 'Q') return true;
+    }
+    if (n === 1) return [['Q']];
+    if (n === 2 || n === 3) return [];
+    const res = [];
+    const board = Array.from({ length: n }, () => new Array(n).fill('.'));
+    backtrack(0);
+    return res;
+}
