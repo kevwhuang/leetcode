318efee153b72880b967dce02e5eecd2e25fe7ac
Kevin Huang <kevwhuang@gmail.com>
2023-08-16 15:29:57 -0500

    Problem 697

---
+++ problems/easy/501-1000/697.js
@@ -0,0 +1,25 @@
+// 697 - Degree of an Array
+
+function findShortestSubArray(nums) {
+    const freqs = {};
+    const indices = {};
+    let degree = 0;
+    let shortest = Infinity;
+    for (let i = 0; i < nums.length; i++) {
+        const n = nums[i];
+        if (freqs[n]) {
+            freqs[n]++;
+        } else {
+            freqs[n] = 1;
+            indices[n] = i;
+        }
+        const freq = freqs[n];
+        if (freq > degree) {
+            degree = freq;
+            shortest = i - indices[n] + 1;
+        } else if (freq === degree) {
+            shortest = Math.min(i - indices[n] + 1, shortest);
+        }
+    }
+    return shortest;
+}
