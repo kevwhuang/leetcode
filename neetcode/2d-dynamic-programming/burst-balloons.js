3a11f3cb04c3e3a4e870e445d0c5472e9e922030
Kevin Huang <kevwhuang@gmail.com>
2023-08-22 02:03:43 -0500

    NC maxCoins

--- neetcode/2d-dynamic-programming/burst-balloons.js
+++ neetcode/2d-dynamic-programming/burst-balloons.js
@@ -6,7 +6,22 @@
  */
 
 function maxCoins(nums) {
-
+    const mapper = () => new Array(nums.length).fill(0);
+    nums = [1, ...nums, 1];
+    const dp = Array.from({ length: nums.length }, mapper);
+    const n = nums.length - 2;
+    for (let j = 1; j <= n; j++) {
+        for (let i = Math.min(j, n); i; i--) {
+            let max = 0;
+            for (let k = i; k <= j; k++) {
+                let coins = dp[i][k - 1] + dp[k + 1][j];
+                coins += nums[i - 1] * nums[k] * nums[j + 1];
+                if (coins > max) max = coins;
+            }
+            dp[i][j] = max;
+        }
+    }
+    return dp[1][n];
 }
 
 module.exports = maxCoins;
