00c17a45ac90d66842f3622e825e6ef4b6109e4c
Kevin Huang <kevwhuang@gmail.com>
2023-08-24 03:42:25 -0500

    NC findItinerary

--- problems/hard/1-500/332.js
+++ problems/hard/1-500/332.js
@@ -1,3 +1,20 @@
 // 332 - Reconstruct Itinerary
 
-
+function findItinerary(tickets) {
+    const adj = {};
+    for (let i = 0; i < tickets.length; i++) {
+        adj[tickets[i][0]] ??= [];
+        adj[tickets[i][0]].push(tickets[i][1]);
+    }
+    for (const key in adj) {
+        adj[key].sort().reverse();
+    }
+    const itinerary = [], stack = ['JFK'];
+    while (stack.length) {
+        while (adj[stack.at(-1)]?.length) {
+            stack.push(adj[stack.at(-1)].pop());
+        }
+        itinerary.push(stack.pop());
+    }
+    return itinerary.reverse();
+}
