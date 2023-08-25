ff02ef270e451b1c6174268390262805c7c964cd
Kevin Huang <kevwhuang@gmail.com>
2023-08-25 00:51:20 -0500

    NC minCostClimbingStairs

---
+++ neetcode/1d-dynamic-programming/min-cost-climbing-stairs.js
@@ -0,0 +1,15 @@
+// 746 - Min Cost Climbing Stairs
+
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+
+function minCostClimbingStairs(cost) {
+    for (let i = cost.length - 3; i >= 0; i--) {
+        cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+    }
+    return Math.min(cost[0], cost[1]);
+}
+
+module.exports = minCostClimbingStairs;
---
+++ tests/1d-dynamic-programming/min-cost-climbing-stairs.test.js
@@ -0,0 +1,11 @@
+const minCostClimbingStairs = require('../../neetcode/1d-dynamic-programming/min-cost-climbing-stairs');
+
+describe('Main', () => {
+    test('Case 1', () => {
+        expect(minCostClimbingStairs([10, 15, 20])).toStrictEqual(15);
+    });
+
+    test('Case 2', () => {
+        expect(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])).toStrictEqual(6);
+    });
+});
