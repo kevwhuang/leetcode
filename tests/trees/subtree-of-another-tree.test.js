103dfca1e5a748ba390c2c4c3496a79a1900fbb3
Kevin Huang <kevwhuang@gmail.com>
2023-09-03 01:55:25 -0500

    NC isSubtree

---
+++ tests/trees/subtree-of-another-tree.test.js
@@ -0,0 +1,29 @@
+const isSubtree = require('../../neetcode/trees/subtree-of-another-tree');
+
+class TreeNode {
+    constructor(val, left, right) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+describe('Main', () => {
+    const root = new TreeNode(3);
+    root.left = new TreeNode(4);
+    root.left.left = new TreeNode(1);
+    root.left.right = new TreeNode(2);
+    root.right = new TreeNode(5);
+    const subRoot = new TreeNode(4);
+    subRoot.left = new TreeNode(1);
+    subRoot.right = new TreeNode(2);
+
+    test('Case 1', () => {
+        expect(isSubtree(root, subRoot)).toBeTruthy();
+    });
+
+    test('Case 1', () => {
+        root.left.right.left = new TreeNode(0);
+        expect(isSubtree(root, subRoot)).toBeFalsy();
+    });
+});
