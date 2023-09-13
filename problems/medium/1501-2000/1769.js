03b00b4e90c7ac488f1796e371e2ce0542ca7470
Kevin Huang <kevwhuang@gmail.com>
2023-09-13 13:23:05 -0500

    LC 1630/1769

---
+++ problems/medium/1501-2000/1769.js
@@ -0,0 +1,20 @@
+// 1769 - Minimum Number of Operations to Move All Balls to Each Box
+
+function minOperations(boxes) {
+    const ops = new Array(boxes.length);
+    ops[0] = 0;
+    let countLeft = Number(boxes[0] === '1');
+    let countRight = 0;
+    for (let i = 1; i < boxes.length; i++) {
+        if (boxes[i] === '0') continue;
+        ops[0] += i;
+        countRight++;
+    }
+    for (let i = 1; i < boxes.length; i++) {
+        ops[i] = ops[i - 1] + countLeft - countRight;
+        if (boxes[i] === '0') continue;
+        countLeft++;
+        countRight--;
+    }
+    return ops;
+}
