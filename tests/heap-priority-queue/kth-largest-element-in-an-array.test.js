bdbb20c960c87b93b72d41b53554bf2d81623938
Kevin Huang <kevwhuang@gmail.com>
2023-09-15 02:55:26 -0500

    NC findKthLargest

---
+++ tests/heap-priority-queue/kth-largest-element-in-an-array.test.js
@@ -0,0 +1,11 @@
+const findKthLargest = require('../../neetcode/heap-priority-queue/kth-largest-element-in-an-array');
+
+describe('Main', () => {
+    test('Case 1', () => {
+        expect(findKthLargest([3, 2, 1, 5, 6, 4], 2)).toStrictEqual(5);
+    });
+
+    test('Case 2', () => {
+        expect(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)).toStrictEqual(4);
+    });
+});
