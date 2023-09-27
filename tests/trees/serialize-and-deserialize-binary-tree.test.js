8a1e6d8946a6a4870dad0d14971fea2c3e3e29f4
Kevin Huang <kevwhuang@gmail.com>
2023-09-27 15:02:07 +0800

    NC serialize/deserialize

---
+++ tests/trees/serialize-and-deserialize-binary-tree.test.js
@@ -0,0 +1,20 @@
+const { serialize, deserialize } = require('../../neetcode/trees/serialize-and-deserialize-binary-tree');
+
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+describe('Main', () => {
+    test('Case 1', () => {
+        const solution = [1, 2, 3, null, null, 4, 5];
+        expect(deserialize(serialize([1, 2, 3, null, null, 4, 5]))).toStrictEqual(solution);
+    });
+
+    test('Case 2', () => {
+        expect(deserialize(serialize([]))).toStrictEqual([]);
+    });
+});
