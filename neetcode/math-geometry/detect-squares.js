4a0885002394e17f597f3010e409647096861762
Kevin Huang <kevwhuang@gmail.com>
2023-09-28 14:05:47 +0800

    NC DetectSquares

---
+++ neetcode/math-geometry/detect-squares.js
@@ -0,0 +1,42 @@
+// 2013 - Detect Squares
+
+class DetectSquares {
+    constructor() {
+        this.points = new Map();
+    }
+
+    /**
+     * @param {number[]} point
+     * @return {void}
+     */
+
+    add(point) {
+        const [x, y] = point;
+        if (!this.points.has(x)) this.points.set(x, new Map([[y, 1]]));
+        else this.points.get(x).set(y, this.points.get(x).get(y) + 1 || 1);
+    }
+
+    /**
+     * @param {number[]} point
+     * @return {number}
+     */
+
+    count(point) {
+        const [x, y] = point;
+        if (!this.points.has(x)) return 0;
+        let total = 0;
+        for (const [yy, freq1] of this.points.get(x)) {
+            if (y === yy) continue;
+            const delta = y - yy;
+            const freq2_1 = this.points.get(x + delta)?.get(y) || 0;
+            const freq2_2 = this.points.get(x - delta)?.get(y) || 0;
+            let freq3_1 = 0, freq3_2 = 0;
+            if (freq2_1) freq3_1 = this.points.get(x + delta)?.get(yy) || 0;
+            if (freq2_2) freq3_2 = this.points.get(x - delta)?.get(yy) || 0;
+            total += freq1 * (freq2_1 * freq3_1 + freq2_2 * freq3_2);
+        }
+        return total;
+    }
+}
+
+module.exports = DetectSquares;
