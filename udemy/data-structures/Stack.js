e07cfa9f45f589d50f21b9f60605eef8836885bb
Kevin Huang <kevwhuang@gmail.com>
2023-10-13 18:52:26 +0800

    UD Stack

--- udemy/data-structures/Stack.js
+++ udemy/data-structures/Stack.js
@@ -1,5 +1,30 @@
+class StackNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
 class Stack {
     constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
 
+    pop() {
+        if (!this.first) return null;
+        const node = this.first;
+        this.first = this.first.next;
+        this.size--;
+        if (!this.first) this.last = null;
+        return node.val;
+    }
+
+    push(val) {
+        const node = new StackNode(val);
+        if (this.first) [this.first, this.first.next] = [node, this.first];
+        else this.first = this.last = node;
+        return ++this.size;
     }
 }
