8ecf57d7ac089c59bdde6109e3f76a2320c3829d
Kevin Huang <kevwhuang@gmail.com>
2023-10-13 20:43:04 +0800

    UD Queue

--- udemy/data-structures/Queue.js
+++ udemy/data-structures/Queue.js
@@ -1,5 +1,35 @@
+class QueueNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
 class Queue {
     constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
 
+    dequeue() {
+        if (!this.first) return null;
+        const node = this.first;
+        this.first = this.first.next;
+        this.size--;
+        if (!this.first) this.last = null;
+        return node.val;
+    }
+
+    enqueue(val) {
+        const node = new QueueNode(val);
+        if (this.first) {
+            this.last.next = node;
+            this.last = node;
+        } else {
+            this.first = node;
+            this.last = this.first;
+        }
+        return ++this.size;
     }
 }
