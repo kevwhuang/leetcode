670fd1f6170bfaa0be581530ba2455882b4d5c27
Kevin Huang <kevwhuang@gmail.com>
2023-10-16 23:45:50 +0800

    UD MinPriorityQueue

--- udemy/data-structures/MaxHeap.js
+++ udemy/data-structures/MaxHeap.js
@@ -5,11 +5,12 @@ class MaxHeap {
 
     extract() {
         if (this.vals.length === 1) return null;
+        if (this.data.length === 2) return this.vals.pop();
         const root = this.vals[1];
         this.vals[1] = this.vals.pop();
         let top = 1, l = 2, r = 3;
         let next = !this.vals[r] || this.vals[l] > this.vals[r] ? l : r;
-        while (this.vals[next] && this.vals[top] < this.vals[next]) {
+        while (this.vals[top] < this.vals[next]) {
             [this.vals[top], this.vals[next]] = [this.vals[next], this.vals[top]];
             [top, l, r] = [next, 2 * next, 2 * next + 1];
             next = !this.vals[r] || this.vals[l] > this.vals[r] ? l : r;
