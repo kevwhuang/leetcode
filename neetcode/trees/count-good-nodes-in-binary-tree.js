29b145122d698e7e0f0caaeb3ea5515bfb45083e
Kevin Huang <kevwhuang@gmail.com>
2023-10-19 10:52:01 +0800

    NC goodNodes

---
+++ neetcode/trees/count-good-nodes-in-binary-tree.js
@@ -0,0 +1,20 @@
+// 1448 - Count Good Nodes in Binary Tree
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+function goodNodes(root) {
+    function dfs(node, max) {
+        node.val >= max && good++;
+        max = Math.max(node.val, max);
+        node.left && dfs(node.left, max);
+        node.right && dfs(node.right, max);
+    }
+    let good = 0;
+    dfs(root, root.val);
+    return good;
+}
+
+module.exports = goodNodes;
---
+++ tests/trees/count-good-nodes-in-binary-tree.test.js
@@ -0,0 +1,28 @@
+const goodNodes = require('../../neetcode/trees/count-good-nodes-in-binary-tree');
+
+class TreeNode {
+    constructor(val, left, right) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+describe('Main', () => {
+    test('Case 1', () => {
+        const root = new TreeNode(3);
+        root.left = new TreeNode(1, new TreeNode(3));
+        root.right = new TreeNode(4, new TreeNode(1), new TreeNode(5));
+        expect(goodNodes(root)).toStrictEqual(4);
+    });
+
+    test('Case 2', () => {
+        const root = new TreeNode(3);
+        root.left = new TreeNode(3, new TreeNode(4), new TreeNode(2));
+        expect(goodNodes(root)).toStrictEqual(3);
+    });
+
+    test('Case 3', () => {
+        expect(goodNodes(new TreeNode(1))).toStrictEqual(1);
+    });
+});
