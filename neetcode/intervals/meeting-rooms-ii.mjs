6c49ac4e7caea470e6df75de72cf636a7016a91b
Kevin Huang <kevwhuang@gmail.com>
2023-10-19 16:19:02 +0800

    NC minMeetingRooms

--- neetcode/intervals/meeting-rooms-ii.mjs
+++ neetcode/intervals/meeting-rooms-ii.mjs
@@ -5,3 +5,14 @@
  * @return {number}
  */
 
+function minMeetingRooms(intervals) {
+    const starts = intervals.sort((a, b) => a[0] - b[0]);
+    const ends = intervals.map(e => e[1]).sort((a, b) => a - b);
+    let j = 0;
+    for (let i = 0; i < intervals.length; i++) {
+        if (starts[i][0] >= ends[j]) j++;
+    }
+    return intervals.length - j;
+}
+
+module.exports = minMeetingRooms;
---
+++ tests/intervals/meeting-rooms-ii.test.js
@@ -0,0 +1,11 @@
+const minMeetingRooms = require('../../neetcode/intervals/meeting-rooms-ii');
+
+describe('Main', () => {
+    test('Case 1', () => {
+        expect(minMeetingRooms([[0, 30], [5, 10], [15, 20]])).toStrictEqual(2);
+    });
+
+    test('Case 2', () => {
+        expect(minMeetingRooms([[7, 10], [2, 4]])).toStrictEqual(1);
+    });
+});
