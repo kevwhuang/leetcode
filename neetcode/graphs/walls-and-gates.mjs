0546a281cc7f9ea49373ac94bc1b63e0a71ef758
Kevin Huang <kevwhuang@gmail.com>
2023-10-30 12:21:06 +0800

    NC wallsAndGates

---
+++ neetcode/graphs/walls-and-gates.mjs
@@ -0,0 +1,34 @@
+// 286 - Walls and Gates
+
+/**
+ * @param {number[][]} rooms
+ * @return {void}
+ */
+
+function wallsAndGates(rooms) {
+    const m = rooms.length, n = rooms[0].length;
+    const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    for (let r = 0; r < m; r++) {
+        for (let c = 0; c < n; c++) {
+            if (rooms[r][c] !== 0) continue;
+            let dist = 1, queue = [[r, c]];
+            while (queue.length) {
+                const newQueue = [];
+                for (let i = 0; i < queue.length; i++) {
+                    const [rr, cc] = queue[i];
+                    for (const [dr, dc] of dirs) {
+                        if (rooms[rr + dr]?.[cc + dc] > dist) {
+                            rooms[rr + dr][cc + dc] = dist;
+                            newQueue.push([rr + dr, cc + dc]);
+                        }
+                    }
+                }
+                dist++;
+                queue = newQueue;
+            }
+        }
+    }
+    return rooms;
+}
+
+module.exports = wallsAndGates;
---
+++ tests/graphs/walls-and-gates.test.js
@@ -0,0 +1,19 @@
+const wallsAndGates = require('../../neetcode/graphs/walls-and-gates');
+
+describe('Main', () => {
+    test('Case 1', () => {
+        const INF = 2147483647;
+        const rooms = [
+            [INF, -1, 0, INF],
+            [INF, INF, INF, -1],
+            [INF, -1, INF, -1],
+            [0, -1, INF, INF],
+        ];
+        const solution = [[3, -1, 0, 1], [2, 2, 1, -1], [1, -1, 2, -1], [0, -1, 3, 4]];
+        expect(wallsAndGates(rooms)).toStrictEqual(solution);
+    });
+
+    test('Case 2', () => {
+        expect(wallsAndGates([[-1]])).toStrictEqual([[-1]]);
+    });
+});
