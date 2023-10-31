8ba26b84e5dbcbf054a10e64d2181827428a244c
Kevin Huang <kevwhuang@gmail.com>
2023-11-01 02:36:22 +0800

    NC eraseOverlapIntervals

---
+++ neetcode/intervals/non-overlapping-intervals.js
@@ -0,0 +1,18 @@
+// 435 - Non-Overlapping Intervals
+
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+
+function eraseOverlapIntervals(intervals) {
+    intervals.sort((a, b) => a[1] - b[1]);
+    let removed = 0;
+    for (let i = 0, bound = -Infinity; i < intervals.length; i++) {
+        if (intervals[i][0] < bound) removed++;
+        else bound = intervals[i][1];
+    }
+    return removed;
+}
+
+module.exports = eraseOverlapIntervals;
---
+++ tests/intervals/non-overlapping-intervals.test.js
@@ -0,0 +1,16 @@
+const eraseOverlapIntervals = require('../../neetcode/intervals/non-overlapping-intervals');
+
+describe('Main', () => {
+    test('Case 1', () => {
+        const intervals = [[1, 2], [2, 3], [3, 4], [1, 3]];
+        expect(eraseOverlapIntervals(intervals)).toStrictEqual(1);
+    });
+
+    test('Case 2', () => {
+        expect(eraseOverlapIntervals([[1, 2], [1, 2], [1, 2]])).toStrictEqual(2);
+    });
+
+    test('Case 3', () => {
+        expect(eraseOverlapIntervals([[1, 2], [2, 3]])).toStrictEqual(0);
+    });
+});
