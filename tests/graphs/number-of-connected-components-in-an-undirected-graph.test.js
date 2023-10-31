f811dbb14c20772845fb66358ae2cd39402e80d4
Kevin Huang <kevwhuang@gmail.com>
2023-10-31 14:17:40 +0800

    NC countComponents

---
+++ tests/graphs/number-of-connected-components-in-an-undirected-graph.test.js
@@ -0,0 +1,13 @@
+const countComponents = require('../../neetcode/graphs/number-of-connected-components-in-an-undirected-graph');
+
+describe('Main', () => {
+    test('Case 1', () => {
+        const edges = [[0, 1], [1, 2], [3, 4]];
+        expect(countComponents(5, edges)).toStrictEqual(2);
+    });
+
+    test('Case 2', () => {
+        const edges = [[0, 1], [1, 2], [2, 3], [3, 4]];
+        expect(countComponents(5, edges)).toStrictEqual(1);
+    });
+});
