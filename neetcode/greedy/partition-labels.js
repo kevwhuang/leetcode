38c1afa9827842de39f52de5f209252aa65c1b56
Kevin Huang <kevwhuang@gmail.com>
2023-11-01 21:28:26 +0800

    NC partitionLabels

---
+++ neetcode/greedy/partition-labels.js
@@ -0,0 +1,30 @@
+// 763 - Partition Labels
+
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+
+function partitionLabels(s) {
+    const len = s.length;
+    const lastIndex = new Array(26);
+    for (let i = 0; i < len; i++) {
+        lastIndex[s.charCodeAt(i) - 97] = i;
+    }
+    const partitions = [];
+    let i = 0;
+    while (i < len) {
+        let end = lastIndex[s.charCodeAt(i) - 97];
+        for (let j = i; j < len; j++) {
+            if (j === end) {
+                partitions.push(j - i + 1);
+                i = j + 1;
+                break;
+            }
+            end = Math.max(lastIndex[s.charCodeAt(j) - 97], end);
+        }
+    }
+    return partitions;
+}
+
+module.exports = partitionLabels;
---
+++ tests/greedy/partition-labels.test.js
@@ -0,0 +1,11 @@
+const partitionLabels = require('../../neetcode/greedy/partition-labels');
+
+describe('Main', () => {
+    test('Case 1', () => {
+        expect(partitionLabels('ababcbacadefegdehijhklij')).toStrictEqual([9, 7, 8]);
+    });
+
+    test('Case 2', () => {
+        expect(partitionLabels('eccbbbbdec')).toStrictEqual([10]);
+    });
+});
