1114de3df39839ee99a245b4cb041a27c9752506
Kevin Huang <kevwhuang@gmail.com>
2023-11-01 23:05:00 +0800

    NC mergeTriplets

---
+++ neetcode/greedy/merge-triplets-to-form-target-triplet.js
@@ -0,0 +1,23 @@
+// 1899 - Merge Triplets to Form Target Triplet
+
+/**
+ * @param {number[][]} triplets
+ * @param {number[]} target
+ * @return {boolean}
+ */
+
+function mergeTriplets(triplets, target) {
+    const [x, y, z] = target;
+    let foundX = false, foundY = false, foundZ = false;
+    for (let i = 0; i < triplets.length; i++) {
+        const [a, b, c] = triplets[i];
+        if (a > x || b > y || c > z) continue;
+        if (a === x) foundX = true;
+        if (b === y) foundY = true;
+        if (c === z) foundZ = true;
+        if (foundX && foundY && foundZ) return true;
+    }
+    return foundX && foundY && foundZ;
+}
+
+module.exports = mergeTriplets;
