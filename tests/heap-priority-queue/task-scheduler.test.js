3d0b17aef3e4989be3ede3c6b639f01a1b01760c
Kevin Huang <kevwhuang@gmail.com>
2023-11-01 12:43:16 +0800

    NC leastInterval

---
+++ tests/heap-priority-queue/task-scheduler.test.js
@@ -0,0 +1,16 @@
+const leastInterval = require('../../neetcode/heap-priority-queue/task-scheduler');
+
+describe('Main', () => {
+    test('Case 1', () => {
+        expect(leastInterval(['A', 'A', 'A', 'B', 'B', 'B'], 2)).toStrictEqual(8);
+    });
+
+    test('Case 2', () => {
+        expect(leastInterval(['A', 'A', 'A', 'B', 'B', 'B'], 0)).toStrictEqual(6);
+    });
+
+    test('Case 3', () => {
+        const tasks = ['A', 'A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F', 'G'];
+        expect(leastInterval(tasks, 2)).toStrictEqual(16);
+    });
+});
