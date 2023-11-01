6c64b4e9fc5b06d2365e4aa0682eaf548506c0e7
Kevin Huang <kevwhuang@gmail.com>
2023-11-02 03:25:47 +0800

    NC canCompleteCircuit

---
+++ neetcode/greedy/gas-station.js
@@ -0,0 +1,22 @@
+// 134 - Gas Station
+
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+
+function canCompleteCircuit(gas, cost) {
+    let total = 0, cur = 0, start = 0;
+    for (let i = 0; i < gas.length; i++) {
+        const net = gas[i] - cost[i];
+        total += net;
+        cur += net;
+        if (cur >= 0) continue;
+        start = i + 1;
+        cur = 0;
+    }
+    return total >= 0 ? start : -1;
+}
+
+module.exports = canCompleteCircuit;
