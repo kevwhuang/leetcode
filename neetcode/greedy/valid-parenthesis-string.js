fc3e5d633bb412d75febf86b06dfca32ec9e5650
Kevin Huang <kevwhuang@gmail.com>
2023-11-30 01:50:35 +0800

    NC checkValidString

--- neetcode/greedy/valid-parenthesis-string.js
+++ neetcode/greedy/valid-parenthesis-string.js
@@ -6,7 +6,22 @@
  */
 
 function checkValidString(s) {
-
+    let left = 0, right = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            left++;
+            right++;
+        } else if (s[i] === ')') {
+            left--;
+            right--;
+        } else {
+            left--;
+            right++;
+        }
+        left = Math.max(left, 0);
+        if (right < 0) return false;
+    }
+    return left === 0;
 }
 
-module.exporst = checkValidString;
+module.exports = checkValidString;
