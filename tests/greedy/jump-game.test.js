2254ebcff0fd8775abf7afb39ebd7c0b3fd8c4e9
Kevin Huang <kevwhuang@gmail.com>
2023-11-29 14:37:59 +0800

    NC canJump

---
+++ tests/greedy/jump-game.test.js
@@ -0,0 +1,11 @@
+const canJump = require('../../neetcode/greedy/jump-game');
+
+describe('Main', () => {
+    test('Case 1', () => {
+        expect(canJump([2, 3, 1, 1, 4])).toBeTruthy();
+    });
+
+    test('Case 2', () => {
+        expect(canJump([3, 2, 1, 0, 4])).toBeFalsy();
+    });
+});
