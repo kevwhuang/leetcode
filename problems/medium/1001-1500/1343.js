4618aa315bb665a0f8db4d985c389d1a30d63c92
Kevin Huang <kevwhuang@gmail.com>
2023-12-05 00:19:58 +0800

    LC 1343

---
+++ problems/medium/1001-1500/1343.js
@@ -0,0 +1,14 @@
+// 1343 - Number of Sub-Arrays of Size K and Average Greater Than or Equal to Threshold
+
+function numOfSubarrays(arr, k, threshold) {
+    let sum = 0;
+    for (let i = 0; i < k; i++) {
+        sum += arr[i];
+    }
+    let count = sum / k >= threshold ? 1 : 0;
+    for (let i = k; i < arr.length; i++) {
+        sum += arr[i] - arr[i - k];
+        if (sum / k >= threshold) count++;
+    }
+    return count;
+}
