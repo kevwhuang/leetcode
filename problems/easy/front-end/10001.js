ad1845f715e2bc95f2dfcacc375121bcb7918960
Kevin Huang <kevwhuang@gmail.com>
2023-12-07 16:09:54 +0800

    LC 10001/10002/10003

---
+++ problems/easy/front-end/10001.js
@@ -0,0 +1,7 @@
+// 10001 - Create DOM Node
+
+export function createDomNode() {
+    const div = document.createElement('div');
+    div.innerText = 'Hello World';
+    return div;
+}
---
+++ problems/easy/front-end/10003.js
@@ -0,0 +1,8 @@
+// 10003 - Apply Styles to Node
+
+export function applyStylesToNode(node) {
+    node.style.backgroundColor = '#ffa015';
+    node.style.borderStyle = 'none';
+    node.style.color = 'white';
+    node.style.fontSize = '16px';
+}
