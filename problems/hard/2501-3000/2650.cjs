38457a8b5edab418b44e07ac8e2aebc378b37053
Kevin Huang <kevwhuang@gmail.com>
2024-01-04 18:45:06 -0600

    LC 2650

--- problems/hard/2501-3000/2650.cjs
+++ problems/hard/2501-3000/2650.cjs
@@ -1,3 +1,18 @@
 // 2650 - Design Cancellable Function
 
-
+function cancellable(generator) {
+    let cancel;
+    const promise = (async function () {
+        const cancelPromise = new Promise((_, rej) => cancel = () => rej('Cancelled'));
+        let next = generator.next();
+        while (!next.done) {
+            try {
+                next = generator.next(await Promise.race([next.value, cancelPromise]));
+            } catch (e) {
+                next = generator.throw(e);
+            }
+        }
+        return next.value;
+    }());
+    return [cancel, promise];
+}
