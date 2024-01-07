275a22930ecab7f19813aa53ac0f37e830b3414a
Kevin Huang <kevwhuang@gmail.com>
2024-01-06 23:52:50 -0600

    NC minCostConnectPoints

--- neetcode/advanced-graphs/min-cost-to-connect-all-points.js
+++ neetcode/advanced-graphs/min-cost-to-connect-all-points.js
@@ -6,7 +6,28 @@
  */
 
 function minCostConnectPoints(points) {
-
+    let min = 0;
+    const len = points.length;
+    const seen = new Set();
+    const costs = new Array(len).fill(Infinity);
+    costs[0] = 0;
+    for (let i = 0; i < len; i++) {
+        let u, localMin = Infinity;
+        for (let v = 0; v < len; v++) {
+            if (seen.has(v) || costs[v] >= localMin) continue;
+            u = v;
+            localMin = costs[v];
+        }
+        seen.add(u);
+        min += costs[u];
+        for (let v = 0; v < len; v++) {
+            if (seen.has(v)) continue;
+            let manhattan = Math.abs(points[v][0] - points[u][0]);
+            manhattan += Math.abs(points[v][1] - points[u][1]);
+            costs[v] = Math.min(manhattan, costs[v]);
+        }
+    }
+    return min;
 }
 
 module.exports = minCostConnectPoints;
