e6c18839733488e1cb427f1ae7a7b9acfbe81504
Kevin Huang <kevwhuang@gmail.com>
2024-01-06 20:52:47 -0600

    NC networkDelayTime

--- neetcode/advanced-graphs/network-delay-time.js
+++ neetcode/advanced-graphs/network-delay-time.js
@@ -8,7 +8,19 @@
  */
 
 function networkDelayTime(times, n, k) {
-
+    const bf = new Array(n + 1).fill(Infinity);
+    bf[k] = 0;
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < times.length; j++) {
+            const edge = times[j];
+            bf[edge[1]] = Math.min(bf[edge[0]] + edge[2], bf[edge[1]]);
+        }
+    }
+    let max = -1;
+    for (let i = 1; i < n + 1; i++) {
+        max = Math.max(bf[i], max);
+    }
+    return max === Infinity ? -1 : max;
 }
 
 module.exports = networkDelayTime;
