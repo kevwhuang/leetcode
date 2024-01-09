f872e8be737e6b7136ea9e5aecfe3a035e6ff9cb
Kevin Huang <kevwhuang@gmail.com>
2024-01-08 19:10:02 -0600

    LC 2980

---
+++ problems/easy/2501-3000/2980.js
@@ -0,0 +1,10 @@
+// 2980 - Check If Bitwise Or Has Trailing Zeros
+
+function hasTrailingZeros(nums) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] & 1) continue;
+        if (++count === 2) return true;
+    }
+    return false;
+}
