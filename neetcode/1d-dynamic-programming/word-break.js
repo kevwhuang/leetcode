95882a21bb37fb15828b3fec0b9df103fc168fc7
Kevin Huang <kevwhuang@gmail.com>
2024-01-25 21:05:34 -0600

    NC wordBreak

--- neetcode/1d-dynamic-programming/word-break.js
+++ neetcode/1d-dynamic-programming/word-break.js
@@ -7,7 +7,16 @@
  */
 
 function wordBreak(s, wordDict) {
-
+    const dp = new Array(s.length + 1).fill(0);
+    dp[s.length] = 1;
+    for (let i = s.length - 1; i >= 0; i--) {
+        for (let j = 0; j < wordDict.length; j++) {
+            if (!s.startsWith(wordDict[j], i)) continue;
+            dp[i] = dp[i + wordDict[j].length];
+            if (dp[i]) break;
+        }
+    }
+    return Boolean(dp[0]);
 }
 
 module.exports = wordBreak;
