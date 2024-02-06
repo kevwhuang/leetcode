7a49e38999e78d0962b16b4b16cc1f6d0cb3f126
Kevin Huang <kevwhuang@gmail.com>
2024-02-05 19:52:38 -0600

    LC 3010/3014

--- problems/easy/3001-3500/3010.js
+++ problems/easy/3001-3500/3010.js
@@ -1,3 +1,7 @@
 // 3010 - Divide an Array Into Subarrays With Minimum Cost I
 
-
+function minimumCost(nums) {
+    const first = nums.shift();
+    nums.sort((a, b) => a - b);
+    return first + nums[0] + nums[1];
+}
--- problems/easy/3001-3500/3014.js
+++ problems/easy/3001-3500/3014.js
@@ -1,3 +1,10 @@
 // 3014 - Minimum Number of Pushes to Type Word I
 
-
+function minimumPushes(word) {
+    let pushes = 0;
+    for (let i = 0, inc = 1; i < word.length; i++) {
+        pushes += inc;
+        if ((i + 1) % 8 === 0) inc++;
+    }
+    return pushes;
+}
