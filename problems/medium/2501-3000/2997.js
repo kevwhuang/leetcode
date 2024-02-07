28d5c6099b7bfa843d55f8cba45d6b74819c3c4e
Kevin Huang <kevwhuang@gmail.com>
2024-02-06 19:56:38 -0600

    LC 2997/3016

--- problems/medium/2501-3000/2997.js
+++ problems/medium/2501-3000/2997.js
@@ -1,3 +1,13 @@
 // 2997 - Minimum Number of Operations to Make Array XOR Equal to K
 
-
+function minOperations(nums, k) {
+    for (let i = 0; i < nums.length; i++) {
+        k ^= nums[i];
+    }
+    let flips = 0;
+    while (k) {
+        if (k & 1) flips++;
+        k >>= 1;
+    }
+    return flips;
+}
--- problems/medium/3001-3500/3016.js
+++ problems/medium/3001-3500/3016.js
@@ -1,3 +1,19 @@
 // 3016 - Minimum Number of Pushes to Type Word II
 
-
+function minimumPushes(word) {
+    const freqs = Array.from({ length: 26 }, (_, i) => [i, 0]);
+    for (let i = 0; i < word.length; i++) {
+        freqs[word.charCodeAt(i) - 97][1]++;
+    }
+    freqs.sort((a, b) => b[1] - a[1]);
+    const arr = new Array(26);
+    for (let i = 0, inc = 1; i < 26; i++) {
+        arr[freqs[i][0]] = inc;
+        if ((i + 1) % 8 === 0) inc++;
+    }
+    let pushes = 0;
+    for (let i = 0; i < word.length; i++) {
+        pushes += arr[word.charCodeAt(i) - 97];
+    }
+    return pushes;
+}
