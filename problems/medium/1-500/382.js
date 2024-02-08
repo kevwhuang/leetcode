35c9c757a4be7e67144f3c703825bf38b2d46e3f
Kevin Huang <kevwhuang@gmail.com>
2024-02-07 19:57:51 -0600

    LC 93/382

--- problems/medium/1-500/382.js
+++ problems/medium/1-500/382.js
@@ -1,3 +1,14 @@
 // 382 - Linked List Random Node
 
-
+class Solution {
+    constructor(head) {
+        this.vals = [];
+        while (head) {
+            this.vals.push(head.val);
+            head = head.next;
+        }
+    }
+    getRandom() {
+        return this.vals[this.vals.length * Math.random() >> 0];
+    }
+}
--- problems/medium/1-500/93.js
+++ problems/medium/1-500/93.js
@@ -1,3 +1,35 @@
 // 93 - Restore IP Addresses
 
-
+function restoreIpAddresses(s) {
+    function partition(str) {
+        if (str.length === 0) return [];
+        if (str[0] === '0') return ['0'];
+        if (str.length === 1) return [str];
+        if (str.length === 2) return [str[0], str];
+        return [str[0], str[0] + str[1], str];
+    }
+    if (s.length < 4 || s.length > 12) return [];
+    const ips = [];
+    const a = partition(s.slice(0, 3));
+    for (let i = 0; i < a.length; i++) {
+        if (+a[i] > 255) continue;
+        const aa = a[i].length;
+        const b = partition(s.slice(aa, aa + 3));
+        for (let j = 0; j < b.length; j++) {
+            if (+b[j] > 255) continue;
+            const bb = aa + b[j].length;
+            const c = partition(s.slice(bb, bb + 3));
+            for (let k = 0; k < c.length; k++) {
+                if (+c[k] > 255) continue;
+                const cc = bb + c[k].length;
+                const d = partition(s.slice(cc, cc + 3));
+                for (let l = 0; l < d.length; l++) {
+                    if (+d[l] > 255) continue;
+                    if (cc + d[l].length !== s.length) continue;
+                    ips.push(`${a[i]}.${b[j]}.${c[k]}.${d[l]}`);
+                }
+            }
+        }
+    }
+    return ips;
+}
