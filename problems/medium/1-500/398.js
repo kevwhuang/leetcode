52a63e396bc6cfb48787a290f713e127321bb708
Kevin Huang <kevwhuang@gmail.com>
2024-02-08 19:58:46 -0600

    LC 398/402

--- problems/medium/1-500/398.js
+++ problems/medium/1-500/398.js
@@ -1,3 +1,16 @@
 // 398 - Random Pick Index
 
-
+class Solution {
+    constructor(nums) {
+        this.map = new Map();
+        for (let i = 0; i < nums.length; i++) {
+            const num = nums[i];
+            if (this.map.has(num)) this.map.get(num).push(i);
+            else this.map.set(num, [i]);
+        }
+    }
+    pick(target) {
+        const arr = this.map.get(target);
+        return arr[arr.length * Math.random() >> 0];
+    }
+}
--- problems/medium/1-500/402.js
+++ problems/medium/1-500/402.js
@@ -1,3 +1,21 @@
 // 402 - Remove K Digits
 
-
+function removeKdigits(num, k) {
+    const stack = [];
+    for (let i = 0; i < num.length; i++) {
+        while (k && stack.length && num[i] < stack[stack.length - 1]) {
+            stack.pop();
+            k--;
+        }
+        stack.push(num[i]);
+    }
+    k++;
+    while (--k) stack.pop();
+    const index = stack.findIndex(e => e !== '0');
+    if (index === -1) return '0';
+    let res = '';
+    for (let i = index; i < stack.length; i++) {
+        res += stack[i];
+    }
+    return res;
+}
