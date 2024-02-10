994ba1171514559b07e841e9ef9d4e3db4f5aec7
Kevin Huang <kevwhuang@gmail.com>
2024-02-09 19:59:58 -0600

    LC 421/430

--- problems/medium/1-500/421.js
+++ problems/medium/1-500/421.js
@@ -1,3 +1,19 @@
 // 421 - Maximum XOR of Two Numbers in an Array
 
-
+function findMaximumXOR(nums) {
+    let res = 0, mask = 0;
+    for (let n = 31 - Math.clz32(Math.max(...nums)); n >= 0; n--) {
+        mask |= 1 << n;
+        const target = res | 1 << n;
+        const seen = new Set();
+        for (let i = 0; i < nums.length; i++) {
+            const and = nums[i] & mask;
+            if (!seen.has(and ^ target)) seen.add(and);
+            else {
+                res = target;
+                break;
+            }
+        }
+    }
+    return res;
+}
--- problems/medium/1-500/430.js
+++ problems/medium/1-500/430.js
@@ -1,3 +1,20 @@
 // 430 - Flatten a Multilevel Doubly Linked List
 
-
+function flatten(head) {
+    const stack = [];
+    let cur = head;
+    while (cur) {
+        if (!cur.next && stack.length) {
+            cur.next = stack.pop();
+            if (cur.next) cur.next.prev = cur;
+        }
+        if (cur.child) {
+            stack.push(cur.next);
+            cur.next = cur.child;
+            cur.child.prev = cur;
+            cur.child = null;
+        }
+        cur = cur.next;
+    }
+    return head;
+}
