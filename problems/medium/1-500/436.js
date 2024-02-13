8a93d2da7f1e641f7661036e28253fa685683cd9
Kevin Huang <kevwhuang@gmail.com>
2024-02-12 20:01:17 -0600

    LC 436/470

--- problems/medium/1-500/436.js
+++ problems/medium/1-500/436.js
@@ -1,3 +1,25 @@
 // 436 - Find Right Interval
 
-
+function findRightInterval(intervals) {
+    function binarySearch(target) {
+        let l = 0, r = len - 1;
+        while (l <= r) {
+            const m = (l + r) / 2 >> 0;
+            if (intervals[m][0] < target) l = m + 1;
+            else if (intervals[m][0] > target) r = m - 1;
+            else return m;
+        }
+        return r + 1;
+    }
+    const len = intervals.length;
+    for (let i = 0; i < len; i++) {
+        intervals[i].push(i);
+    }
+    intervals.sort((a, b) => a[0] - b[0]);
+    const res = new Array(len);
+    for (let i = 0; i < res.length; i++) {
+        const index = binarySearch(intervals[i][1]);
+        res[intervals[i][2]] = index === len ? -1 : intervals[index][2];
+    }
+    return res;
+}
--- problems/medium/1-500/470.js
+++ problems/medium/1-500/470.js
@@ -1,3 +1,5 @@
 // 470 - Implement Rand10 Using Rand7
 
-
+function rand10() {
+    return (10 * Math.random() + 1) >> 0;
+}
