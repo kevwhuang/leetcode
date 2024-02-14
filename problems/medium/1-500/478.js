c5bd12ffec9f8b6d014a4f47f0b4c26151de34c5
Kevin Huang <kevwhuang@gmail.com>
2024-02-13 20:02:13 -0600

    LC 478/497

--- problems/medium/1-500/478.js
+++ problems/medium/1-500/478.js
@@ -1,3 +1,16 @@
 // 478 - Generate Random Point in a Circle
 
-
+class Solution {
+    constructor(radius, x_center, y_center) {
+        this.x = x_center;
+        this.y = y_center;
+        this.radius = radius;
+    }
+    randPoint() {
+        const dist = this.radius * Math.sqrt(Math.random());
+        const angle = 2 * Math.PI * Math.random();
+        const x = this.x + dist * Math.cos(angle);
+        const y = this.y + dist * Math.sin(angle);
+        return [x, y];
+    }
+}
--- problems/medium/1-500/497.js
+++ problems/medium/1-500/497.js
@@ -1,3 +1,32 @@
 // 497 - Random Point in Non-Overlapping Rectangles
 
-
+class Solution {
+    constructor(rects) {
+        this.rects = rects;
+        this.sums = this.#initialize(rects);
+        this.total = this.sums[rects.length - 1][0];
+    }
+    pick() {
+        const target = this.total * Math.random() >> 0;
+        let l = 0, r = this.sums.length - 1;
+        while (l <= r) {
+            const m = (l + r) / 2 >> 0;
+            if (this.sums[m][0] > target) r = m - 1;
+            else l = m + 1;
+        }
+        const rect = this.rects[this.sums[l][1]];
+        const x = rect[0] + ((rect[2] - rect[0] + 1) * Math.random() >> 0);
+        const y = rect[1] + ((rect[3] - rect[1] + 1) * Math.random() >> 0);
+        return [x, y];
+    }
+    #initialize(rects) {
+        const sums = new Array(rects.length);
+        let running = 0;
+        for (let i = 0; i < rects.length; i++) {
+            const rect = rects[i];
+            running += (rect[2] - rect[0] + 1) * (rect[3] - rect[1] + 1);
+            sums[i] = [running, i];
+        }
+        return sums;
+    }
+}
