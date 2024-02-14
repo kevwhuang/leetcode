8c365958b7dc3b35d657c63cb0333d0a67d26537
Kevin Huang <kevwhuang@gmail.com>
2024-02-14 01:57:02 -0600

    LC 1706

--- problems/medium/1501-2000/1706.js
+++ problems/medium/1501-2000/1706.js
@@ -1,3 +1,17 @@
 // 1706 - Where Will the Ball Fall
 
-
+function findBall(grid) {
+    const m = grid.length, n = grid[0].length;
+    const res = new Array(n);
+    for (let c = 0; c < n; c++) {
+        let col = c;
+        for (let r = 0; r < m; r++) {
+            const dir = grid[r][col];
+            col += dir;
+            if (col === n || grid[r][col] === -dir) col = -1;
+            if (col === -1) break;
+        }
+        res[c] = col;
+    }
+    return res;
+}
