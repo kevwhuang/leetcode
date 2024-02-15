94e9f8d3c76fe668a6135aeea3b9a7546bbffa07
Kevin Huang <kevwhuang@gmail.com>
2024-02-14 20:03:43 -0600

    LC 528/554

--- problems/medium/501-1000/528.js
+++ problems/medium/501-1000/528.js
@@ -1,3 +1,22 @@
 // 528 - Random Pick With Weight
 
-
+class Solution {
+    constructor(w) {
+        this.sums = new Array(w);
+        this.sums[0] = w[0];
+        for (let i = 1; i < w.length; i++) {
+            this.sums[i] = this.sums[i - 1] + w[i];
+        }
+        this.total = this.sums[this.sums.length - 1];
+    }
+    pickIndex() {
+        const target = this.total * Math.random() >> 0;
+        let l = 0, r = this.sums.length - 1;
+        while (l <= r) {
+            const m = (l + r) / 2 >> 0;
+            if (this.sums[m] > target) r = m - 1;
+            else l = m + 1;
+        }
+        return l;
+    }
+}
--- problems/medium/501-1000/554.js
+++ problems/medium/501-1000/554.js
@@ -1,3 +1,18 @@
 // 554 - Brick Wall
 
-
+function leastBricks(wall) {
+    const map = new Map();
+    for (let i = 0; i < wall.length; i++) {
+        const row = wall[i], len = row.length - 1;
+        let index = 0;
+        for (let j = 0; j < len; j++) {
+            index += row[j];
+            map.set(index, map.get(index) + 1 || 1);
+        }
+    }
+    let max = 0;
+    for (const freq of map.values()) {
+        max = Math.max(freq, max);
+    }
+    return wall.length - max;
+}
