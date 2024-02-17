fb7dd1351faf96ecfad67fde94a9fe8e7cd7dbda
Kevin Huang <kevwhuang@gmail.com>
2024-02-16 20:06:11 -0600

    LC 752/809

--- problems/medium/501-1000/752.js
+++ problems/medium/501-1000/752.js
@@ -1,3 +1,30 @@
 // 752 - Open the Lock
 
-
+function openLock(deadends, target) {
+    function getNext(cur, pos, inc) {
+        const digit = (Number(cur[pos]) + inc + 10) % 10;
+        return `${cur.slice(0, pos)}${digit}${cur.slice(pos + 1)}`;
+    }
+    if (target === '0000') return 0;
+    deadends = new Set(deadends);
+    if (deadends.has('0000')) return -1;
+    let queue = ['0000'], turns = 1;
+    while (queue.length) {
+        const nextQueue = [];
+        for (let i = 0; i < queue.length; i++) {
+            const cur = queue[i];
+            for (let pos = 0; pos <= 3; pos++) {
+                for (let inc = -1; inc <= 1; inc += 2) {
+                    const next = getNext(cur, pos, inc);
+                    if (next === target) return turns;
+                    if (deadends.has(next)) continue;
+                    deadends.add(next);
+                    nextQueue.push(next);
+                }
+            }
+        }
+        queue = nextQueue;
+        turns++;
+    }
+    return -1;
+}
--- problems/medium/501-1000/809.js
+++ problems/medium/501-1000/809.js
@@ -1,3 +1,28 @@
 // 809 - Expressive Words
 
-
+function expressiveWords(s, words) {
+    function analyze(word) {
+        const arr = [];
+        for (let i = 0; i < word.length; i++) {
+            const char = word[i], start = i;
+            while (word[i] === word[i + 1]) i++;
+            arr.push([char, i - start + 1]);
+        }
+        return arr;
+    }
+    s = analyze(s);
+    let count = 0;
+    for (let i = 0; i < words.length; i++) {
+        const word = analyze(words[i]);
+        if (s.length !== word.length) continue;
+        let j = 0;
+        while (j < s.length) {
+            if (s[j][0] !== word[j][0]) break;
+            if (s[j][1] < word[j][1]) break;
+            if (s[j][1] <= 2 && s[j][1] !== word[j][1]) break;
+            j++;
+        }
+        if (j === s.length) count++;
+    }
+    return count;
+}
