ce50ba3af686e6dcc63cd089454aa97aabbc8807
Kevin Huang <kevwhuang@gmail.com>
2024-02-19 20:07:50 -0600

    LC 838/874

--- problems/medium/501-1000/838.js
+++ problems/medium/501-1000/838.js
@@ -1,3 +1,34 @@
 // 838 - Push Dominoes
 
-
+function pushDominoes(dominoes) {
+    dominoes = dominoes.split('');
+    let left, right;
+    for (let i = 0; i < dominoes.length; i++) {
+        if (dominoes[i] === 'L') {
+            if (Number.isInteger(left)) {
+                while (++left < i) dominoes[left] = 'L';
+            } else if (Number.isInteger(right)) {
+                left = i;
+                while (++right < --left) {
+                    dominoes[right] = 'R';
+                    dominoes[left] = 'L';
+                }
+            } else {
+                left = i;
+                while (0 <= --left) dominoes[left] = 'L';
+            }
+            left = i;
+            right = null;
+        } else if (dominoes[i] === 'R') {
+            if (Number.isInteger(right)) {
+                while (++right < i) dominoes[right] = 'R';
+            }
+            left = null;
+            right = i;
+        }
+    }
+    if (Number.isInteger(right)) {
+        while (++right < dominoes.length) dominoes[right] = 'R';
+    }
+    return dominoes.join('');
+}
--- problems/medium/501-1000/874.js
+++ problems/medium/501-1000/874.js
@@ -1,3 +1,29 @@
 // 874 - Walking Robot Simulation
 
-
+function robotSim(commands, obstacles) {
+    const map = new Map();
+    for (let i = 0; i < obstacles.length; i++) {
+        const x = obstacles[i][0], y = obstacles[i][1];
+        if (map.has(x)) map.get(x).add(y);
+        else map.set(x, new Set([y]));
+    }
+    let max = 0, x = 0, y = 0;
+    const dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    for (let i = 0, j = 0; i < commands.length; i++) {
+        const command = commands[i];
+        if (command === -2) {
+            if (--j === -1) j = 3;
+        } else if (command === -1) {
+            if (++j === 4) j = 0;
+        } else {
+            const dx = dirs[j][0], dy = dirs[j][1];
+            for (let n = 1; n <= command; n++) {
+                const xx = x + dx, yy = y + dy;
+                if (map.has(xx) && map.get(xx).has(yy)) break;
+                x = xx, y = yy;
+            }
+        }
+        max = Math.max(x * x + y * y, max);
+    }
+    return max;
+}
