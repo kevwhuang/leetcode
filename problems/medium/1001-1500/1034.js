05523e0c15bba282948211d245c897adee6137fc
Kevin Huang <kevwhuang@gmail.com>
2024-02-21 20:10:09 -0600

    LC 1034/1052

--- problems/medium/1001-1500/1034.js
+++ problems/medium/1001-1500/1034.js
@@ -1,3 +1,35 @@
 // 1034 - Coloring a Border
 
-
+function colorBorder(grid, row, col, color) {
+    function dfs(r, c) {
+        if (!validate(r, c)) return;
+        if (grid[r][c] !== target) return;
+        if (component.has(r) && component.get(r).has(c)) return;
+        if (!component.has(r)) component.set(r, new Set());
+        component.get(r).add(c);
+        if (isBorder(r, c)) border.push([r, c]);
+        dfs(r - 1, c);
+        dfs(r + 1, c);
+        dfs(r, c - 1);
+        dfs(r, c + 1);
+    }
+    function isBorder(r, c) {
+        if (r === 0 || r === m - 1 || c === 0 || c === n - 1) return true;
+        if (validate(r - 1, c) && grid[r - 1][c] !== target) return true;
+        if (validate(r + 1, c) && grid[r + 1][c] !== target) return true;
+        if (validate(r, c - 1) && grid[r][c - 1] !== target) return true;
+        if (validate(r, c + 1) && grid[r][c + 1] !== target) return true;
+        return false;
+    }
+    const validate = (r, c) => r >= 0 && r < m && c >= 0 && c < n;
+    const m = grid.length, n = grid[0].length;
+    const target = grid[row][col];
+    const component = new Map();
+    const border = [];
+    if (target === color) return grid;
+    dfs(row, col);
+    for (let i = 0; i < border.length; i++) {
+        grid[border[i][0]][border[i][1]] = color;
+    }
+    return grid;
+}
--- problems/medium/1001-1500/1052.js
+++ problems/medium/1001-1500/1052.js
@@ -1,3 +1,20 @@
 // 1052 - Grumpy Bookstore Owner
 
-
+function maxSatisfied(customers, grumpy, minutes) {
+    let satisfied = 0;
+    for (let i = 0; i < customers.length; i++) {
+        if (grumpy[i] === 1) continue;
+        satisfied += customers[i];
+        customers[i] = 0;
+    }
+    let window = 0;
+    for (let i = 0; i < minutes; i++) {
+        window += customers[i];
+    }
+    let convert = window;
+    for (let i = minutes; i < customers.length; i++) {
+        window += customers[i] - customers[i - minutes];
+        convert = Math.max(window, convert);
+    }
+    return satisfied + convert;
+}
