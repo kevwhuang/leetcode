ac2f6ce6b4460328cae6a8eed0045169d8478bd1
Kevin Huang <kevwhuang@gmail.com>
2024-02-22 20:11:22 -0600

    LC 1093/1110

--- problems/medium/1001-1500/1093.js
+++ problems/medium/1001-1500/1093.js
@@ -1,3 +1,25 @@
 // 1093 - Statistics From a Large Sample
 
-
+function sampleStats(count) {
+    let min, max, mode = 0, samples = 0, sum = 0;
+    for (let i = 0; i < count.length; i++) {
+        if (count[i] === 0) continue;
+        if (min === undefined) min = i;
+        max = i;
+        if (count[i] > count[mode]) mode = i;
+        samples += count[i];
+        sum += i * count[i];
+    }
+    let mid1, mid2, running1 = 0, running2 = 0;
+    const target1 = Math.round(samples / 2);
+    const target2 = samples % 2 ? target1 : target1 + 1;
+    for (let i = 0; mid1 === undefined || mid2 === undefined; i++) {
+        running1 += count[i];
+        running2 += count[i];
+        if (mid1 === undefined && running1 >= target1) mid1 = i;
+        if (mid2 === undefined && running2 >= target2) mid2 = i;
+    }
+    const mean = sum / samples;
+    const median = (mid1 + mid2) / 2;
+    return [min, max, mean, median, mode];
+}
--- problems/medium/1001-1500/1110.js
+++ problems/medium/1001-1500/1110.js
@@ -1,3 +1,23 @@
 // 1110 - Delete Nodes and Return Forest
 
-
+function delNodes(root, to_delete) {
+    function dfs(node, isRoot) {
+        if (!node) return;
+        if (isRoot && !set.has(node.val)) forest.push(node);
+        for (const child of children) {
+            if (!node[child]) continue;
+            if (set.has(node[child].val)) {
+                dfs(node[child].left, true);
+                dfs(node[child].right, true);
+                node[child] = null;
+            } else {
+                dfs(node[child], set.has(node.val));
+            }
+        }
+    }
+    const forest = [];
+    const children = ['left', 'right'];
+    const set = new Set(to_delete);
+    dfs(root, true);
+    return forest;
+}
