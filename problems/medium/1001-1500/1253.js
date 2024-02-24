bd0bdcc8c14ce65f97c2289e277d7ffe2eb4b3ae
Kevin Huang <kevwhuang@gmail.com>
2024-02-23 20:14:59 -0600

    LC 1253/1367

--- problems/medium/1001-1500/1253.js
+++ problems/medium/1001-1500/1253.js
@@ -1,3 +1,28 @@
 // 1253 - Reconstruct a 2-Row Binary Matrix
 
-
+function reconstructMatrix(upper, lower, colsum) {
+    const row1 = new Array(colsum.length);
+    const row2 = new Array(colsum.length);
+    let ones = 0;
+    for (let i = 0; i < colsum.length; i++) {
+        if (colsum[i] === 0) row1[i] = row2[i] = 0;
+        else if (colsum[i] === 1) ones++;
+        else {
+            row1[i] = row2[i] = 1;
+            upper--; lower--;
+        }
+    }
+    if (upper < 0 || lower < 0 || ones !== (upper + lower)) return [];
+    for (let i = 0; i < colsum.length; i++) {
+        if (colsum[i] !== 1) continue;
+        if (upper) {
+            row1[i] = 1;
+            row2[i] = 0;
+            upper--;
+        } else {
+            row1[i] = 0;
+            row2[i] = 1;
+        }
+    }
+    return [row1, row2];
+}
--- problems/medium/1001-1500/1367.js
+++ problems/medium/1001-1500/1367.js
@@ -1,3 +1,16 @@
 // 1367 - Linked List in Binary Tree
 
-
+function isSubPath(head, root) {
+    function dfs(node, cur) {
+        if (!node) return false;
+        if (match(node, cur)) return true;
+        return dfs(node.left, cur) || dfs(node.right, cur);
+    }
+    function match(node, cur) {
+        if (!cur) return true;
+        if (!node) return false;
+        if (node.val !== cur.val) return false;
+        return match(node.left, cur.next) || match(node.right, cur.next);
+    }
+    return dfs(root, head);
+}
