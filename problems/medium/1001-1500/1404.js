1f8c387995d03dcd94a1951da11d1ed0f345dadd
Kevin Huang <kevwhuang@gmail.com>
2024-02-26 20:21:26 -0600

    LC 1404/1432

--- problems/medium/1001-1500/1404.js
+++ problems/medium/1001-1500/1404.js
@@ -1,3 +1,10 @@
 // 1404 - Number of Steps to Reduce a Number in Binary Representation to One
 
-
+function numSteps(s) {
+    let steps = 0, n = BigInt('0b' + s);
+    while (n !== 1n) {
+        n = n % 2n ? n + 1n : n / 2n;
+        steps++;
+    }
+    return steps;
+}
--- problems/medium/1001-1500/1432.js
+++ problems/medium/1001-1500/1432.js
@@ -1,3 +1,10 @@
 // 1432 - Max Difference You Can Get From Changing an Integer
 
-
+function maxDiff(num) {
+    num = String(num);
+    const a = num.replaceAll(num[num.search(/[0-8]/)], 9);
+    const b = num[0] === '1'
+        ? num.replaceAll(num[num.search(/[2-9]/)], 0)
+        : num.replaceAll(num[0], 1);
+    return a - b;
+}
