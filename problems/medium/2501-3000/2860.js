3a1cece64fc555ec84541e40ca801700ee7fbdb0
Kevin Huang <kevwhuang@gmail.com>
2024-02-28 00:18:55 -0600

    LC 2860/3044

--- problems/medium/2501-3000/2860.js
+++ problems/medium/2501-3000/2860.js
@@ -1,3 +1,11 @@
 // 2860 - Happy Students
 
-
+function countWays(nums) {
+    nums.sort((a, b) => a - b);
+    let ways = nums[0] ? 1 : 0;
+    if (nums[nums.length - 1] < nums.length) ways++;
+    for (let i = 1; i <= nums.length; i++) {
+        if (nums[i - 1] < i && nums[i] > i) ways++;
+    }
+    return ways;
+}
---
+++ problems/medium/3001-3500/3044.js
@@ -0,0 +1,39 @@
+// 3044 - Most Frequent Prime
+
+function mostFrequentPrime(mat) {
+    function isPrime(num) {
+        const sqrt = Math.sqrt(num);
+        for (let i = 2; i <= sqrt; i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+    const m = mat.length, n = mat[0].length;
+    const dirs = [
+        [0, 1], [1, 1], [1, 0], [1, -1],
+        [0, -1], [-1, -1], [-1, 0], [-1, 1],
+    ];
+    const map = new Map([[-1, 1]]);
+    for (let r = 0; r < m; r++) {
+        for (let c = 0; c < n; c++) {
+            for (let i = 0; i < 8; i++) {
+                let num = mat[r][c];
+                let rr = r + dirs[i][0], cc = c + dirs[i][1];
+                while (rr >= 0 && rr < m && cc >= 0 && cc < n) {
+                    num = 10 * num + mat[rr][cc];
+                    map.set(num, map.get(num) + 1 || 1);
+                    rr += dirs[i][0];
+                    cc += dirs[i][1];
+                }
+            }
+        }
+    }
+    let mostFrequent = -1;
+    for (const num of map.keys()) {
+        if (!isPrime(num)) continue;
+        if (map.get(num) < map.get(mostFrequent)) continue;
+        if (map.get(num) > map.get(mostFrequent)) mostFrequent = num;
+        else mostFrequent = Math.max(num, mostFrequent);
+    }
+    return mostFrequent;
+}
