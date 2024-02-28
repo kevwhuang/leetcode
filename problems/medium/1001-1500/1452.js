6dcafa333f5b0975dc18ebacc5630a769de53804
Kevin Huang <kevwhuang@gmail.com>
2024-02-27 20:22:26 -0600

    LC 1452/1514

--- problems/medium/1001-1500/1452.js
+++ problems/medium/1001-1500/1452.js
@@ -1,3 +1,26 @@
 // 1452 - People Whose List of Favorite Companies Is Not a Subset of Another List
 
-
+function peopleIndexes(favoriteCompanies) {
+    for (let i = 0; i < favoriteCompanies.length; i++) {
+        favoriteCompanies[i].sort();
+    }
+    const res = [];
+    for (let i = 0; i < favoriteCompanies.length; i++) {
+        let unique = true;
+        const company1 = favoriteCompanies[i];
+        for (let j = 0; j < favoriteCompanies.length; j++) {
+            if (i === j) continue;
+            const company2 = favoriteCompanies[j];
+            if (company1.length > company2.length) continue;
+            let p1 = 0, p2 = 0;
+            while (p1 < company1.length && p2 < company2.length) {
+                if (company1[p1] === company2[p2++]) p1++;
+            }
+            if (p1 < company1.length) continue;
+            unique = false;
+            break;
+        }
+        if (unique) res.push(i);
+    }
+    return res;
+}
--- problems/medium/1501-2000/1514.js
+++ problems/medium/1501-2000/1514.js
@@ -1,3 +1,24 @@
 // 1514 - Path With Maximum Probability
 
-
+function maxProbability(n, edges, succProb, start_node, end_node) {
+    const bf = new Array(n).fill(0);
+    bf[start_node] = 1;
+    while (--n) {
+        let updated;
+        for (let i = 0; i < edges.length; i++) {
+            const v1 = edges[i][0], v2 = edges[i][1];
+            let next = bf[v1] * succProb[i];
+            if (next > bf[v2]) {
+                bf[v2] = next;
+                updated = true;
+            }
+            next = bf[v2] * succProb[i];
+            if (next > bf[v1]) {
+                bf[v1] = next;
+                updated = true;
+            }
+        }
+        if (!updated) break;
+    }
+    return bf[end_node];
+}
