6c39e42b05daeffba1a5681dfbb4243685f7b411
Kevin Huang <kevwhuang@gmail.com>
2024-02-28 20:27:14 -0600

    LC 1536/1559

--- problems/medium/1501-2000/1536.js
+++ problems/medium/1501-2000/1536.js
@@ -1,3 +1,20 @@
 // 1536 - Minimum Swaps to Arrange a Binary Grid
 
-
+function minSwaps(grid) {
+    const n = grid.length;
+    const arr = Array.from({ length: n }, (_, r) => grid[r].lastIndexOf(1));
+    let steps = 0;
+    for (let i = 0; i < n; i++) {
+        if (arr[i] <= i) continue;
+        let j = i;
+        while (arr[j] > i) j++;
+        if (j === n) return -1;
+        steps += j - i;
+        while (i < j) {
+            const temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j-- - 1] = temp;
+        }
+    }
+    return steps;
+}
--- problems/medium/1501-2000/1559.js
+++ problems/medium/1501-2000/1559.js
@@ -1,3 +1,30 @@
 // 1559 - Detect Cycles in 2D Grid
 
-
+function containsCycle(grid) {
+    function dfs(r, c, dir) {
+        if (isCycle) return;
+        if (r === -1 || r === m || c === -1 || c === n) return;
+        if (seen.has(r) && seen.get(r).has(c)) return isCycle = true;
+        if (grid[r][c] !== cur) return;
+        if (!seen.has(r)) seen.set(r, new Set());
+        seen.get(r).add(c);
+        grid[r][c] = null;
+        if (dir !== 'D') dfs(r - 1, c, 'U');
+        if (dir !== 'U') dfs(r + 1, c, 'D');
+        if (dir !== 'R') dfs(r, c - 1, 'L');
+        if (dir !== 'L') dfs(r, c + 1, 'R');
+    }
+    const m = grid.length, n = grid[0].length;
+    let isCycle, cur, seen;
+    for (let r = 0; r < m; r++) {
+        for (let c = 0; c < n; c++) {
+            if (grid[r][c] === null) continue;
+            isCycle = false;
+            cur = grid[r][c];
+            seen = new Map();
+            dfs(r, c);
+            if (isCycle) return true;
+        }
+    }
+    return false;
+}
