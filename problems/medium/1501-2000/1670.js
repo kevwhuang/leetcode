7dcca39f88e7546664d28de4f6752465dfd2ec3f
Kevin Huang <kevwhuang@gmail.com>
2024-02-29 20:49:09 -0600

    LC 1670/1785

--- problems/medium/1501-2000/1670.js
+++ problems/medium/1501-2000/1670.js
@@ -1,3 +1,30 @@
 // 1670 - Design Front Middle Back Queue
 
-
+class FrontMiddleBackQueue {
+    constructor() {
+        this.queue = [];
+    }
+    popBack() {
+        if (this.queue.length === 0) return -1;
+        return this.queue.pop();
+    }
+    popFront() {
+        if (this.queue.length === 0) return -1;
+        return this.queue.shift();
+    }
+    popMiddle() {
+        if (this.queue.length === 0) return -1;
+        let mid = this.queue.length / 2 >> 0;
+        if (this.queue.length % 2 === 0) mid--;
+        return this.queue.splice(mid, 1);
+    }
+    pushBack(val) {
+        this.queue.push(val);
+    }
+    pushFront(val) {
+        this.queue.unshift(val);
+    }
+    pushMiddle(val) {
+        this.queue.splice(this.queue.length / 2 >> 0, 0, val);
+    }
+}
--- problems/medium/1501-2000/1785.js
+++ problems/medium/1501-2000/1785.js
@@ -1,3 +1,6 @@
 // 1785 - Minimum Elements to Add to Form a Given Sum
 
-
+function minElements(nums, limit, goal) {
+    const sum = nums.reduce((s, e) => s + e);
+    return Math.ceil(Math.abs(sum - goal) / limit);
+}
