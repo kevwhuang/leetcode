e665ebb36f7446d2350186cd588e5e02f7ff8abb
Kevin Huang <kevwhuang@gmail.com>
2024-03-01 00:37:54 -0600

    LC 2038/2571/2962

--- problems/medium/2001-2500/2038.js
+++ problems/medium/2001-2500/2038.js
@@ -1,3 +1,14 @@
 // 2038 - Remove Colored Pieces If Both Neighbors Are the Same Color
 
-
+function winnerOfGame(colors) {
+    let alice = 0, bob = 0, i = 0;
+    while (i < colors.length) {
+        let count = 0;
+        const cur = colors[i];
+        while (colors[i] === cur) { count++; i++; }
+        if (count <= 2) continue;
+        if (cur === 'A') alice += count - 2;
+        else bob += count - 2;
+    }
+    return alice > bob;
+}
--- problems/medium/2501-3000/2571.js
+++ problems/medium/2501-3000/2571.js
@@ -1,3 +1,11 @@
 // 2571 - Minimum Operations to Reduce an Integer to 0
 
-
+function minOperations(n) {
+    let ops = 0;
+    while (n) {
+        const exp = Math.log2(n) >> 0;
+        n = Math.min(n - 2 ** exp, 2 ** (exp + 1) - n);
+        ++ops;
+    }
+    return ops;
+}
