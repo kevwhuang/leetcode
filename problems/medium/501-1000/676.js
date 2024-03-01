af727115636aabdd08f4e9a3e1b1948da09e9dfa
Kevin Huang <kevwhuang@gmail.com>
2024-03-01 01:35:43 -0600

    LC 676

--- problems/medium/501-1000/676.js
+++ problems/medium/501-1000/676.js
@@ -1,3 +1,22 @@
 // 676 - Implement Magic Dictionary
 
-
+class MagicDictionary {
+    constructor() {
+        this.dict;
+    }
+    buildDict(dictionary) {
+        this.dict = new Set(dictionary);
+    }
+    search(searchWord) {
+        for (const compareWord of this.dict) {
+            if (searchWord.length !== compareWord.length) continue;
+            let diff = 0;
+            for (let i = 0; i < searchWord.length; i++) {
+                if (searchWord[i] !== compareWord[i]) diff++;
+                if (diff > 1) break;
+            }
+            if (diff === 1) return true;
+        }
+        return false;
+    }
+}
