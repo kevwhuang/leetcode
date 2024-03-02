e410cb70121cf75699589f1eea6d8034e21080db
Kevin Huang <kevwhuang@gmail.com>
2024-03-01 20:50:12 -0600

    LC 1797/1834

--- problems/medium/1501-2000/1797.js
+++ problems/medium/1501-2000/1797.js
@@ -1,3 +1,24 @@
 // 1797 - Design Authentication Manager
 
-
+class AuthenticationManager {
+    constructor(timeToLive) {
+        this.tokens = new Map();
+        this.timeToLive = timeToLive;
+    }
+    countUnexpiredTokens(currentTime) {
+        for (const id of this.tokens.keys()) {
+            if (this.tokens.get(id) > currentTime) continue;
+            this.tokens.delete(id);
+        }
+        return this.tokens.size;
+    }
+    generate(tokenId, currentTime) {
+        this.tokens.set(tokenId, currentTime + this.timeToLive);
+    }
+    renew(tokenId, currentTime) {
+        if (!this.tokens.has(tokenId)) return;
+        const expiryTime = this.tokens.get(tokenId);
+        if (expiryTime <= currentTime) this.tokens.delete(tokenId);
+        else this.tokens.set(tokenId, currentTime + this.timeToLive);
+    }
+}
--- problems/medium/1501-2000/1834.js
+++ problems/medium/1501-2000/1834.js
@@ -1,3 +1,25 @@
 // 1834 - Single-Threaded CPU
 
-
+function getOrder(tasks) {
+    const compare = (a, b) => a[2] - b[2] || a[0] - b[0];
+    const order = new Array(tasks.length);
+    const pq = new MinPriorityQueue({ compare });
+    for (let i = 0; i < tasks.length; i++) {
+        tasks[i] = [i, tasks[i][0], tasks[i][1]];
+    }
+    tasks.sort((a, b) => a[1] - b[1]);
+    let time = 0, i = 0, j = 0;
+    while (i < tasks.length) {
+        while (j < tasks.length && tasks[j][1] <= time) {
+            pq.enqueue(tasks[j++]);
+        }
+        if (!pq.isEmpty()) {
+            const task = pq.dequeue();
+            order[i++] = task[0];
+            time += task[2];
+        } else if (j < tasks.length) {
+            time = tasks[j][1];
+        }
+    }
+    return order;
+}
