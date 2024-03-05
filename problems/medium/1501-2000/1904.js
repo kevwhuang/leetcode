fe433f156a4bafb43d18523bd52a0437bee3b48d
Kevin Huang <kevwhuang@gmail.com>
2024-03-04 20:54:26 -0600

    LC 1904/1921

--- problems/medium/1501-2000/1904.js
+++ problems/medium/1501-2000/1904.js
@@ -1,3 +1,10 @@
 // 1904 - The Number of Full Rounds You Have Played
 
-
+function numberOfRounds(loginTime, logoutTime) {
+    const convert = s => 60 * s.slice(0, 2) + +s.slice(3);
+    const first = convert(loginTime);
+    let last = convert(logoutTime);
+    if (first < last && last - first < 15) return 0;
+    if (first > last) last += 1440;
+    return Math.floor(last / 15) - Math.ceil(first / 15);
+}
--- problems/medium/1501-2000/1921.js
+++ problems/medium/1501-2000/1921.js
@@ -1,3 +1,16 @@
 // 1921 - Eliminate Maximum Number of Monsters
 
-
+function eliminateMaximum(dist, speed) {
+    const n = dist.length;
+    const time = new Array(n).fill(0);
+    for (let i = 0; i < n; i++) {
+        const arrival = Math.ceil(dist[i] / speed[i]);
+        if (arrival < n) time[arrival]++;
+    }
+    let running = 0;
+    for (let i = 0; i < n; i++) {
+        if (time[i] + running > i) return i;
+        running += time[i];
+    }
+    return n;
+}
