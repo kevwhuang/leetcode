f612a9b72053f436b5f353a1afbdee9083d11387
Kevin Huang <kevwhuang@gmail.com>
2024-03-05 20:55:45 -0600

    LC 1936/1968

--- problems/medium/1501-2000/1936.js
+++ problems/medium/1501-2000/1936.js
@@ -1,3 +1,10 @@
 // 1936 - Add Minimum Number of Rungs
 
-
+function addRungs(rungs, dist) {
+    let count = dist < rungs[0] ? (rungs[0] - 1) / dist >> 0 : 0;
+    for (let i = 1; i < rungs.length; i++) {
+        if (rungs[i - 1] + dist >= rungs[i]) continue;
+        count += (rungs[i] - rungs[i - 1] - 1) / dist >> 0;
+    }
+    return count;
+}
--- problems/medium/1501-2000/1968.js
+++ problems/medium/1501-2000/1968.js
@@ -1,3 +1,11 @@
 // 1968 - Array With Elements Not Equal to Average of Neighbors
 
-
+function rearrangeArray(nums) {
+    const swap = i => [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+    const bound = nums.length - 1;
+    for (let i = 1; i < bound; i++) {
+        if (nums[i - 1] < nums[i] && nums[i] < nums[i + 1]) swap(i);
+        else if (nums[i - 1] > nums[i] && nums[i] > nums[i + 1]) swap(i);
+    }
+    return nums;
+}
