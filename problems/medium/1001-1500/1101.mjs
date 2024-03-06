8319853d86aa4d0068718a16f1017a264178c5ac
Kevin Huang <kevwhuang@gmail.com>
2024-03-06 02:57:11 -0600

    LC 1101/1151

--- problems/medium/1001-1500/1101.mjs
+++ problems/medium/1001-1500/1101.mjs
@@ -1,3 +1,25 @@
 // 1101 - The Earliest Moment When Everyone Become Friends
 
-
+function earliestAcq(logs, n) {
+    function union(v1, v2) {
+        const p1 = find(v1);
+        const p2 = find(v2);
+        if (p1 < p2) uf[p2] = p1;
+        else uf[p1] = p2;
+        if (p1 !== p2) n--;
+    }
+    function find(v) {
+        while (v !== uf[v]) {
+            uf[v] = uf[uf[v]];
+            v = uf[v];
+        }
+        return v;
+    }
+    logs.sort((a, b) => a[0] - b[0]);
+    const uf = Array.from({ length: n }, (_, i) => i);
+    for (let i = 0; i < logs.length; i++) {
+        union(logs[i][1], logs[i][2]);
+        if (n === 1) return logs[i][0];
+    }
+    return -1;
+}
--- problems/medium/1001-1500/1151.mjs
+++ problems/medium/1001-1500/1151.mjs
@@ -1,3 +1,16 @@
 // 1151 - Minimum Swaps to Group All 1's Together
 
-
+function minSwaps(data) {
+    const ones = data.reduce((s, e) => s + e);
+    let window = 0;
+    for (let i = 0; i < ones; i++) {
+        if (data[i] === 0) window++;
+    }
+    let swaps = window;
+    for (let i = ones; i < data.length; i++) {
+        if (data[i] === 0) window++;
+        if (data[i - ones] === 0) window--;
+        swaps = Math.min(window, swaps);
+    }
+    return swaps;
+}
