c669278e6c4f5951d164147c3a57ced4839169f6
Kevin Huang <kevwhuang@gmail.com>
2024-03-06 20:57:21 -0600

    LC 2101/2126

--- problems/medium/2001-2500/2101.js
+++ problems/medium/2001-2500/2101.js
@@ -1,3 +1,38 @@
 // 2101 - Detonate the Maximum Bombs
 
-
+function maximumDetonation(bombs) {
+    const adj = new Map();
+    for (let i = 0; i < bombs.length; i++) {
+        const x = bombs[i][0], y = bombs[i][1];
+        const target = bombs[i][2] ** 2;
+        for (let j = 0; j < bombs.length; j++) {
+            if (i === j) continue;
+            const dx = x - bombs[j][0];
+            const dy = y - bombs[j][1];
+            if (dx ** 2 + dy ** 2 > target) continue;
+            if (!adj.has(i)) adj.set(i, []);
+            adj.get(i).push(j);
+        }
+    }
+    let max = 1;
+    for (const root of adj.keys()) {
+        const seen = new Set([root]);
+        let queue = [root];
+        while (queue.length) {
+            const nextQueue = [];
+            for (let i = 0; i < queue.length; i++) {
+                if (!adj.has(queue[i])) continue;
+                const neighbors = adj.get(queue[i]);
+                for (let j = 0; j < neighbors.length; j++) {
+                    if (seen.has(neighbors[j])) continue;
+                    seen.add(neighbors[j]);
+                    nextQueue.push(neighbors[j]);
+                }
+            }
+            queue = nextQueue;
+        }
+        max = Math.max(seen.size, max);
+        if (max === bombs.length) break;
+    }
+    return max;
+}
--- problems/medium/2001-2500/2126.js
+++ problems/medium/2001-2500/2126.js
@@ -1,3 +1,12 @@
 // 2126 - Destroying Asteroids
 
-
+function asteroidsDestroyed(mass, asteroids) {
+    const stack = [];
+    for (let i = 0; i < asteroids.length; i++) {
+        stack.push(asteroids[i]);
+        while (stack.length && stack[stack.length - 1] <= mass) {
+            mass += stack.pop();
+        }
+    }
+    return stack.length === 0;
+}
