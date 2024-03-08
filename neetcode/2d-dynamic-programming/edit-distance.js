733552b16fb8ec4eb569ad20de19d335f382efe6
Kevin Huang <kevwhuang@gmail.com>
2024-03-08 17:59:22 -0600

    NC minDistance

--- neetcode/2d-dynamic-programming/edit-distance.js
+++ neetcode/2d-dynamic-programming/edit-distance.js
@@ -7,7 +7,24 @@
  */
 
 function minDistance(word1, word2) {
-
+    const m = word1.length, n = word2.length;
+    const dp = Array.from({ length: m + 1 }, () => new Array(n + 1));
+    for (let r = m; r >= 0; r--) {
+        dp[r][n] = m - r;
+    }
+    for (let c = n; c >= 0; c--) {
+        dp[m][c] = n - c;
+    }
+    for (let r = m - 1; r >= 0; r--) {
+        for (let c = n - 1; c >= 0; c--) {
+            dp[r][c] = word1[r] === word2[c]
+                ? dp[r + 1][c + 1]
+                : 1 + Math.min(dp[r + 1][c], dp[r + 1][c + 1], dp[r][c + 1]);
+        }
+    }
+    return dp[0][0];
 }
+
+minDistance("intention", "execution");
 
 module.exports = minDistance;
