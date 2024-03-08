551b658c1a8d9436ba47f37181257001c34b659a
Kevin Huang <kevwhuang@gmail.com>
2024-03-07 20:59:15 -0600

    LC 2139/2201

--- problems/medium/2001-2500/2139.js
+++ problems/medium/2001-2500/2139.js
@@ -1,3 +1,17 @@
 // 2139 - Minimum Moves to Reach Target Score
 
-
+function minMoves(target, maxDoubles) {
+    let moves = 0;
+    while (target > 1) {
+        if (target % 2) {
+            moves++;
+            target--;
+        } else {
+            if (maxDoubles === 0) break;
+            maxDoubles--;
+            moves++;
+            target /= 2;
+        }
+    }
+    return moves + target - 1;
+}
