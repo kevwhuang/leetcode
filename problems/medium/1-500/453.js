68e2f69b0fa6ff649747e98873ecf169ecd1ce99
Kevin Huang <kevwhuang@gmail.com>
2024-03-08 15:37:09 -0600

    LC 453/816

--- problems/medium/1-500/453.js
+++ problems/medium/1-500/453.js
@@ -1,3 +1,5 @@
 // 453 - Minimum Moves to Equal Array Elements
 
-
+function minMoves(nums) {
+    return nums.reduce((s, e) => s + e) - Math.min(...nums) * nums.length;
+}
--- problems/medium/501-1000/816.js
+++ problems/medium/501-1000/816.js
@@ -1,3 +1,28 @@
 // 816 - Ambiguous Coordinates
 
-
+function ambiguousCoordinates(s) {
+    function isValid(str) {
+        if (str.length > 1 && str[0] === '0' && str[1] !== '.') return false;
+        if (str.includes('.') && str[str.length - 1] === '0') return false;
+        return true;
+    }
+    const coordinates = [];
+    for (let i = 2; i < s.length - 1; i++) {
+        const left = s.slice(1, i);
+        const right = s.slice(i, s.length - 1);
+        for (let j = 1; j <= left.length; j++) {
+            const leftCoord = left.slice(0, j)
+                + (j === left.length ? '' : '.')
+                + left.slice(j);
+            if (isValid(leftCoord) === false) continue;
+            for (let k = 1; k <= right.length; k++) {
+                const rightCoord = right.slice(0, k)
+                    + (k === right.length ? '' : '.')
+                    + right.slice(k);
+                if (isValid(rightCoord) === false) continue;
+                coordinates.push(`(${leftCoord}, ${rightCoord})`);
+            }
+        }
+    }
+    return coordinates;
+}
