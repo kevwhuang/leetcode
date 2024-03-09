48a2387bc3763b4a55cc6e97b7e5af0b261f2083
Kevin Huang <kevwhuang@gmail.com>
2024-03-09 03:31:08 -0600

    LC 1072

--- problems/medium/1001-1500/1072.js
+++ problems/medium/1001-1500/1072.js
@@ -1,3 +1,16 @@
 // 1072 - Flip Columns for Maximum Number of Equal Rows
 
-
+function maxEqualRowsAfterFlips(matrix) {
+    const map = new Map();
+    const m = matrix.length, n = matrix[0].length;
+    for (let r = 0; r < m; r++) {
+        if (matrix[r][0]) {
+            for (let c = 0; c < n; c++) {
+                matrix[r][c] = matrix[r][c] ? 0 : 1;
+            }
+        }
+        const row = matrix[r].join('');
+        map.set(row, map.get(row) + 1 || 1);
+    }
+    return Math.max(...map.values());
+}
