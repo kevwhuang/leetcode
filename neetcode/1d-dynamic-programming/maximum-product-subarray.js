367d59553bed2f9593e7fc99152117c5500111b3
Kevin Huang <kevwhuang@gmail.com>
2024-03-08 20:56:22 -0600

    NC maxProduct

--- neetcode/1d-dynamic-programming/maximum-product-subarray.js
+++ neetcode/1d-dynamic-programming/maximum-product-subarray.js
@@ -6,7 +6,18 @@
  */
 
 function maxProduct(nums) {
-
+    let res = nums[0], max = nums[0], min = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] < 0) {
+            const temp = max;
+            max = min;
+            min = temp;
+        }
+        max = Math.max(nums[i], nums[i] * max);
+        min = Math.min(nums[i], nums[i] * min);
+        res = Math.max(max, res);
+    }
+    return res;
 }
 
 module.exports = maxProduct;
