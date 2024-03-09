e56815cdad9dd303ece5ef79c68e4f289bb026a1
Kevin Huang <kevwhuang@gmail.com>
2024-03-08 21:31:08 -0600

    NC maxProfit (2)

--- neetcode/2d-dynamic-programming/best-time-to-buy-and-sell-stock-with-cooldown.js
+++ neetcode/2d-dynamic-programming/best-time-to-buy-and-sell-stock-with-cooldown.js
@@ -6,7 +6,14 @@
  */
 
 function maxProfit(prices) {
-
+    let sold = 0, rest = 0, hold = -Infinity;
+    for (let i = 0; i < prices.length; i++) {
+        const prev = sold;
+        sold = hold + prices[i];
+        hold = Math.max(rest - prices[i], hold);
+        rest = Math.max(prev, rest);
+    }
+    return Math.max(sold, rest);
 }
 
 module.exports = maxProfit;
