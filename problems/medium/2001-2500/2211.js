1e6c00f7a56f561f38a4173b769ccbf25c2f76db
Kevin Huang <kevwhuang@gmail.com>
2024-03-08 21:00:30 -0600

    LC 2211/2241

--- problems/medium/2001-2500/2211.js
+++ problems/medium/2001-2500/2211.js
@@ -1,3 +1,11 @@
 // 2211 - Count Collisions on a Road
 
-
+function countCollisions(directions) {
+    let l = 0;
+    while (directions[l] === 'L') l++;
+    let r = directions.length - 1;
+    while (directions[r] === 'R') r--;
+    let collisions = 0;
+    while (l <= r) directions[l++] !== 'S' && collisions++;
+    return collisions;
+}
--- problems/medium/2001-2500/2241.js
+++ problems/medium/2001-2500/2241.js
@@ -1,3 +1,28 @@
 // 2241 - Design an ATM Machine
 
-
+class ATM {
+    constructor() {
+        this.atm = [0, 0, 0, 0, 0];
+        this.denominations = [20, 50, 100, 200, 500];
+    }
+    deposit(banknotesCount) {
+        for (let i = 0; i < 5; i++) {
+            this.atm[i] += banknotesCount[i];
+        }
+    }
+    withdraw(amount) {
+        const notes = [0, 0, 0, 0, 0];
+        for (let i = 4; i >= 0; i--) {
+            const cur = this.denominations[i];
+            if (amount < cur || this.atm[i] === 0) continue;
+            const count = Math.min(this.atm[i], amount / cur >> 0);
+            notes[i] += count;
+            amount -= cur * count;
+        }
+        if (amount) return [-1];
+        for (let i = 0; i < 5; i++) {
+            this.atm[i] -= notes[i];
+        }
+        return notes;
+    }
+}
