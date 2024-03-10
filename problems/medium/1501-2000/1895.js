f796c54172dc320c09466b2e03d67c2c5f87e680
Kevin Huang <kevwhuang@gmail.com>
2024-03-10 18:20:56 -0500

    LC 1895/3030

--- problems/medium/1501-2000/1895.js
+++ problems/medium/1501-2000/1895.js
@@ -1,3 +1,53 @@
 // 1895 - Largest Magic Square
 
-
+function largestMagicSquare(grid) {
+    function getPrefixSum(r, c, matrix) {
+        return r < 0 || r >= m || c < 0 || c >= n ? 0 : matrix[r][c];
+    }
+    const validate = (r, c) => r >= 0 && r < m && c >= 0 && c < n;
+    const m = grid.length, n = grid[0].length;
+    const prefixSumRow = Array.from({ length: m }, () => new Uint32Array(n));
+    const prefixSumCol = Array.from({ length: m }, () => new Uint32Array(n));
+    const prefixSumMainDiag = Array.from({ length: m }, () => new Uint32Array(n));
+    const prefixSumAntiDiag = Array.from({ length: m }, () => new Uint32Array(n));
+    for (let r = 0; r < m; r++) {
+        for (let c = 0; c < n; c++) {
+            prefixSumRow[r][c] = prefixSumCol[r][c] = grid[r][c];
+            if (c) prefixSumRow[r][c] += prefixSumRow[r][c - 1];
+            if (r) prefixSumCol[r][c] += prefixSumCol[r - 1][c];
+            prefixSumMainDiag[r][c] = prefixSumAntiDiag[r][c] = grid[r][c];
+            if (validate(r - 1, c - 1)) {
+                prefixSumMainDiag[r][c] += prefixSumMainDiag[r - 1][c - 1];
+            }
+            if (validate(r - 1, c + 1)) {
+                prefixSumAntiDiag[r][c] += prefixSumAntiDiag[r - 1][c + 1];
+            }
+        }
+    }
+    for (let k = Math.min(m, n); k > 1; k--) {
+        const bound1 = m - k + 1, bound2 = n - k + 1;
+        for (let r = 0; r < bound1; r++) {
+            L: for (let c = 0; c < bound2; c++) {
+                const mainDiag = prefixSumMainDiag[r + k - 1][c + k - 1]
+                    - getPrefixSum(r - 1, c - 1, prefixSumMainDiag);
+                const antiDiag = prefixSumAntiDiag[r + k - 1][c]
+                    - getPrefixSum(r - 1, c + k, prefixSumAntiDiag);
+                if (mainDiag !== antiDiag) continue;
+                const bound3 = r + k;
+                for (let rr = r; rr < bound3; rr++) {
+                    const row = prefixSumRow[rr][c + k - 1]
+                        - getPrefixSum(rr, c - 1, prefixSumRow);
+                    if (row !== mainDiag) continue L;
+                }
+                const bound4 = c + k;
+                for (let cc = c; cc < bound4; cc++) {
+                    const col = prefixSumCol[r + k - 1][cc]
+                        - getPrefixSum(r - 1, cc, prefixSumCol);
+                    if (col !== mainDiag) continue L;
+                }
+                return k;
+            }
+        }
+    }
+    return 1;
+}
---
+++ problems/medium/3001-3500/3030.js
@@ -0,0 +1,65 @@
+// 3030 - Find the Grid of Region Average
+
+function resultGrid(image, threshold) {
+    function isRegion(r, c) {
+        for (let i = 0; i < 12; i++) {
+            const pixel1 = image[r + dirs1[i][0]][c + dirs1[i][1]];
+            const pixel2 = image[r + dirs1[i][2]][c + dirs1[i][3]];
+            if (Math.abs(pixel1 - pixel2) > threshold) return false;
+        }
+        return true;
+    }
+    function getPrefixSum(r, c) {
+        return r < 0 || r >= m || c < 0 || c >= n ? 0 : prefixSum[r][c];
+    }
+    const m = image.length, n = image[0].length;
+    const prefixSum = Array.from({ length: m }, () => new Uint32Array(n));
+    for (let r = 0; r < m; r++) {
+        prefixSum[r][0] = image[r][0];
+    }
+    for (let r = 0; r < m; r++) {
+        for (let c = 1; c < n; c++) {
+            prefixSum[r][c] = prefixSum[r][c - 1] + image[r][c];
+        }
+    }
+    for (let r = 1; r < m; r++) {
+        for (let c = 0; c < n; c++) {
+            prefixSum[r][c] += prefixSum[r - 1][c];
+        }
+    }
+    const regions = Array.from({ length: m }, () => new Uint8Array(n));
+    const cumulative = Array.from({ length: m }, () => new Uint16Array(n));
+    const dirs1 = [
+        [0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 2], [0, 1, 1, 1],
+        [0, 2, 1, 2], [1, 0, 1, 1], [1, 0, 2, 0], [1, 1, 1, 2],
+        [1, 1, 2, 1], [1, 2, 2, 2], [2, 0, 2, 1], [2, 1, 2, 2],
+    ];
+    const dirs2 = [
+        [0, 0], [0, 1], [0, 2],
+        [1, 0], [1, 1], [1, 2],
+        [2, 0], [2, 1], [2, 2],
+    ];
+    const bound1 = m - 2, bound2 = n - 2;
+    for (let r = 0; r < bound1; r++) {
+        for (let c = 0; c < bound2; c++) {
+            if (!isRegion(r, c)) continue;
+            const regionSum = prefixSum[r + 2][c + 2]
+                + getPrefixSum(r - 1, c - 1)
+                - getPrefixSum(r - 1, c + 2)
+                - getPrefixSum(r + 2, c - 1);
+            const regionAvg = regionSum / 9 >> 0;
+            for (let i = 0; i < 9; i++) {
+                const rr = r + dirs2[i][0], cc = c + dirs2[i][1];
+                regions[rr][cc]++;
+                cumulative[rr][cc] += regionAvg;
+            }
+        }
+    }
+    for (let r = 0; r < m; r++) {
+        for (let c = 0; c < n; c++) {
+            if (!regions[r][c]) continue;
+            image[r][c] = cumulative[r][c] / regions[r][c] >> 0;
+        }
+    }
+    return image;
+}
