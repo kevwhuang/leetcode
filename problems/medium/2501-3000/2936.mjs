46a9e5387bf1411156949dbc965c2592a6fd5a0c
Kevin Huang <kevwhuang@gmail.com>
2024-03-12 17:03:12 -0500

    LC 2936/3020

--- problems/medium/2501-3000/2936.mjs
+++ problems/medium/2501-3000/2936.mjs
@@ -1,3 +1,18 @@
 // 2936 - Number of Equal Numbers Blocks
 
-
+function countBlocks(nums) {
+    let blocks = 0, first = 0;
+    const last = nums.size() - 1;
+    while (first <= last) {
+        const target = nums.at(first);
+        let l = first, r = last;
+        while (l <= r) {
+            const m = Math.floor((l + r) / 2);
+            if (nums.at(m) === target) l = m + 1;
+            else r = m - 1;
+        }
+        blocks++;
+        first = r + 1;
+    }
+    return blocks;
+}
---
+++ problems/medium/3001-3500/3020.js
@@ -0,0 +1,31 @@
+// 3020 - Find the Maximum Number of Elements in Subset
+
+function maximumLength(nums) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        map.set(nums[i], map.get(nums[i]) + 1 || 1);
+    }
+    let max = 1;
+    if (map.has(1)) {
+        const count = map.get(1);
+        map.delete(1);
+        max = Math.max(count % 2 ? count : count - 1, max);
+    }
+    for (let num of map.keys()) {
+        if (map.get(num) === 1) continue;
+        num *= num;
+        let count = 2, flag = false;
+        while (map.has(num)) {
+            if (map.get(num) === 1) {
+                count++;
+                flag = true;
+                break;
+            } else {
+                count += 2;
+                num *= num;
+            }
+        }
+        max = Math.max(count - !flag, max);
+    }
+    return max;
+}
