4b6e5de5e41bbdfa0307df0f9ab5a3e6e928a68c
Kevin Huang <kevwhuang@gmail.com>
2024-03-11 22:02:15 -0500

    LC 2401/2526

--- problems/medium/2001-2500/2401.js
+++ problems/medium/2001-2500/2401.js
@@ -1,3 +1,11 @@
 // 2401 - Longest Nice Subarray
 
-
+function longestNiceSubarray(nums) {
+    let max = 1, window = 0;
+    for (let i = 0, j = 0; i < nums.length; i++) {
+        while (window & nums[i]) window ^= nums[j++];
+        window |= nums[i];
+        max = Math.max(i - j + 1, max);
+    }
+    return max;
+}
--- problems/medium/2501-3000/2526.js
+++ problems/medium/2501-3000/2526.js
@@ -1,3 +1,14 @@
 // 2526 - Find Consecutive Integers From a Data Stream
 
-
+class DataStream {
+    constructor(value, k) {
+        this.value = value;
+        this.k = k;
+        this.matches = 0;
+    }
+    consec(num) {
+        if (num === this.value) this.matches++;
+        else this.matches = 0;
+        return this.matches >= this.k;
+    }
+}
