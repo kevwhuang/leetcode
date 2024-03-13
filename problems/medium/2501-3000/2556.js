8e0b010647e6add598c371fb52d3b04d1b9a23c8
Kevin Huang <kevwhuang@gmail.com>
2024-03-12 22:03:47 -0500

    LC 2556/2611

--- problems/medium/2501-3000/2556.js
+++ problems/medium/2501-3000/2556.js
@@ -1,3 +1,14 @@
 // 2556 - Disconnect Path in a Binary Matrix by at Most One Flip
 
-
+function isPossibleToCutPath(grid) {
+    function dfs(r, c) {
+        if (r === m || c === n || grid[r][c] === 0) return;
+        if (r === m - 1 && c === n - 1) return true;
+        grid[r][c] = 0;
+        return dfs(r + 1, c) || dfs(r, c + 1);
+    }
+    const m = grid.length, n = grid[0].length;
+    if (!dfs(0, 0)) return true;
+    grid[0][0] = 1;
+    return !dfs(0, 0);
+}
--- problems/medium/2501-3000/2611.js
+++ problems/medium/2501-3000/2611.js
@@ -1,3 +1,15 @@
 // 2611 - Mice and Cheese
 
-
+function miceAndCheese(reward1, reward2, k) {
+    let points = 0;
+    const arr = new Array(reward1.length);
+    for (let i = 0; i < reward1.length; i++) {
+        points += reward2[i];
+        arr[i] = reward1[i] - reward2[i];
+    }
+    arr.sort((a, b) => b - a);
+    for (let i = 0; i < k; i++) {
+        points += arr[i];
+    }
+    return points;
+}
