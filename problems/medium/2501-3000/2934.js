bf0344c39e321f1fb82def66aad607b4744c9e9f
Kevin Huang <kevwhuang@gmail.com>
2024-03-18 16:20:22 -0500

    LC 2934/3085

--- problems/medium/2501-3000/2934.js
+++ problems/medium/2501-3000/2934.js
@@ -1,3 +1,16 @@
 // 2934 - Minimum Operations to Maximize Last Elements in Arrays
 
-
+function minOperations(nums1, nums2) {
+    let swaps1 = 0, swaps2 = 0;
+    const len = nums1.length;
+    const max = Math.max(nums1[len - 1], nums2[len - 1]);
+    const min = Math.min(nums1[len - 1], nums2[len - 1]);
+    for (let i = 0; i < len; i++) {
+        const a = nums1[i], b = nums2[i];
+        if (a > max || b > max) return -1;
+        if (a > min && b > min) return -1;
+        if (a > min) swaps1++;
+        else if (b > min) swaps2++;
+    }
+    return Math.min(swaps1, swaps2);
+}
--- problems/medium/3001-3500/3085.js
+++ problems/medium/3001-3500/3085.js
@@ -1,3 +1,19 @@
 // 3085 - Minimum Deletions to Make String K-Special
 
-
+function minimumDeletions(word, k) {
+    const bucket = new Uint32Array(26);
+    for (let i = 0; i < word.length; i++) {
+        bucket[word.charCodeAt(i) - 97]++;
+    }
+    let min = Infinity;
+    for (let i = 0; i < 26; i++) {
+        const a = bucket[i];
+        let deletions = 0;
+        for (let j = 0; j < 26; j++) {
+            const b = bucket[j];
+            deletions += a <= b ? Math.max(0, b - a - k) : b;
+        }
+        min = Math.min(deletions, min);
+    }
+    return min;
+}
