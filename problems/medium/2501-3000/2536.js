16f9d148667653561a33a0cb50c9ac3fd4e5d773
Kevin Huang <kevwhuang@gmail.com>
2024-03-19 15:57:45 -0500

    LC 2536/2599

--- problems/medium/2501-3000/2536.js
+++ problems/medium/2501-3000/2536.js
@@ -1,3 +1,24 @@
 // 2536 - Increment Submatrices by One
 
-
+function rangeAddQueries(n, queries) {
+    const mat = Array.from({ length: n }, () => new Uint16Array(n));
+    for (let i = 0; i < queries.length; i++) {
+        const r1 = queries[i][0], c1 = queries[i][1];
+        const r2 = queries[i][2] + 1, c2 = queries[i][3] + 1;
+        mat[r1][c1]++;
+        if (r2 < n) mat[r2][c1]--;
+        if (c2 < n) mat[r1][c2]--;
+        if (r2 < n && c2 < n) mat[r2][c2]++;
+    }
+    for (let r = 0; r < n; r++) {
+        for (let c = 1; c < n; c++) {
+            mat[r][c] += mat[r][c - 1];
+        }
+    }
+    for (let c = 0; c < n; c++) {
+        for (let r = 1; r < n; r++) {
+            mat[r][c] += mat[r - 1][c];
+        }
+    }
+    return mat;
+}
--- problems/medium/2501-3000/2599.mjs
+++ problems/medium/2501-3000/2599.mjs
@@ -1,3 +1,15 @@
 // 2599 - Make the Prefix Sum Non-Negative
 
-
+function makePrefSumNonNegative(nums) {
+    let ops = 0, sum = 0;
+    const heap = new PriorityQueue({ compare: (a, b) => a - b });
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        if (nums[i] >= 0) continue;
+        heap.enqueue(nums[i]);
+        if (sum >= 0) continue;
+        sum -= heap.dequeue();
+        ops++;
+    }
+    return ops;
+}
