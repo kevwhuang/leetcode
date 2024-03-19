5b63ed64dcaa00928d36e2846356df34313cd0f2
Kevin Huang <kevwhuang@gmail.com>
2024-03-19 02:49:52 -0500

    LC 1010

--- problems/medium/1001-1500/1010.js
+++ problems/medium/1001-1500/1010.js
@@ -1,3 +1,12 @@
 // 1010 - Pairs of Songs With Total Durations Divisible by 60
 
-
+function numPairsDivisibleBy60(time) {
+    let pairs = 0;
+    const mods = new Uint16Array(60);
+    for (let i = 0; i < time.length; i++) {
+        const mod = time[i] % 60;
+        pairs += mods[(60 - mod) % 60];
+        mods[mod]++;
+    }
+    return pairs;
+}
