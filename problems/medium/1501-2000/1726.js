cebf10c0513fb3aa7b1acf38b87062af43504f5f
Kevin Huang <kevwhuang@gmail.com>
2024-03-20 03:24:28 -0500

    LC 1726

--- problems/medium/1501-2000/1726.js
+++ problems/medium/1501-2000/1726.js
@@ -1,3 +1,19 @@
 // 1726 - Tuple With Same Product
 
-
+function tupleSameProduct(nums) {
+    let tuples = 0;
+    const len = nums.length, map = new Map();
+    for (let i = 0; i < len; i++) {
+        for (let j = i + 1; j < len; j++) {
+            const product = nums[i] * nums[j];
+            if (map.has(product)) {
+                const count = map.get(product);
+                tuples += 8 * count;
+                map.set(product, count + 1);
+            } else {
+                map.set(product, 1);
+            }
+        }
+    }
+    return tuples;
+}
