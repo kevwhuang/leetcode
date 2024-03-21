c79beb91bba5c81e8f4a43d7cf64bd9e5ae034e9
Kevin Huang <kevwhuang@gmail.com>
2024-03-21 03:42:50 -0500

    LC 2275

--- problems/medium/2001-2500/2275.js
+++ problems/medium/2001-2500/2275.js
@@ -1,3 +1,14 @@
 // 2275 - Largest Combination With Bitwise AND Greater Than Zero
 
-
+function largestCombination(candidates) {
+    const bits = new Uint32Array(24);
+    for (let i = 0; i < candidates.length; i++) {
+        let num = candidates[i], j = 0;
+        while (num) {
+            if (num & 1) bits[j]++;
+            num >>= 1;
+            j++;
+        }
+    }
+    return Math.max(...bits);
+}
