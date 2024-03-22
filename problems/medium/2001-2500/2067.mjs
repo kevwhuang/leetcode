71f43bd7f11dfd854f4d5f57b1002e0fa474b217
Kevin Huang <kevwhuang@gmail.com>
2024-03-22 14:55:28 -0500

    LC 2067

--- problems/medium/2001-2500/2067.mjs
+++ problems/medium/2001-2500/2067.mjs
@@ -1,3 +1,20 @@
 // 2067 - Number of Equal Count Substrings
 
-
+function equalCountSubstrings(s, count) {
+    let substrings = 0;
+    for (let k = 1; k <= 26; k++) {
+        let chars = 0;
+        const freqs = new Array(26).fill(0);
+        const size = Math.min(k * count, s.length);
+        for (let i = 0; i < size; i++) {
+            if (++freqs[s.charCodeAt(i) - 97] === count) chars++;
+            if (chars === k) substrings++;
+        }
+        for (let i = size; i < s.length; i++) {
+            if (++freqs[s.charCodeAt(i) - 97] === count) chars++;
+            if (freqs[s.charCodeAt(i - size) - 97]-- === count) chars--;
+            if (chars === k) substrings++;
+        }
+    }
+    return substrings;
+}
