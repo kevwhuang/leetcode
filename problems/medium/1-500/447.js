7d7847959236c2ca516dde1cc222664402e8fcdf
Kevin Huang <kevwhuang@gmail.com>
2024-03-29 07:55:08 -0500

    LC 447

--- problems/medium/1-500/447.js
+++ problems/medium/1-500/447.js
@@ -1,3 +1,18 @@
 // 447 - Number of Boomerangs
 
-
+function numberOfBoomerangs(points) {
+    let boomerangs = 0;
+    for (let i = 0; i < points.length; i++) {
+        const map = new Map();
+        const x = points[i][0], y = points[i][1];
+        for (let j = 0; j < points.length; j++) {
+            let dist = (x - points[j][0]) ** 2;
+            dist += (y - points[j][1]) ** 2;
+            map.set(dist, (map.get(dist) ?? 0) + 1);
+        }
+        for (const freq of map.values()) {
+            boomerangs += freq * (freq - 1);
+        }
+    }
+    return boomerangs;
+}
