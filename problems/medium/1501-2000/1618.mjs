35d91967288b553393624a67e692484d1bc68ae0
Kevin Huang <kevwhuang@gmail.com>
2024-04-01 00:47:48 -0500

    LC 1618/1858

--- problems/medium/1501-2000/1618.mjs
+++ problems/medium/1501-2000/1618.mjs
@@ -1,3 +1,24 @@
 // 1618 - Maximum Font to Fit a Sentence in a Screen
 
-
+function maxFont(text, w, h, fonts, fontInfo) {
+    function isValid(font) {
+        if (fontInfo.getHeight(font) > h) return false;
+        let width = 0;
+        for (let i = 0; i < text.length; i++) {
+            width += fontInfo.getWidth(font, text[i]);
+            if (width > w) return false;
+        }
+        return true;
+    }
+    let res = -1, l = 0, r = fonts.length - 1;
+    while (l <= r) {
+        const m = l + r >> 1;
+        if (isValid(fonts[m])) {
+            res = fonts[m];
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+    return res;
+}
--- problems/medium/1501-2000/1858.mjs
+++ problems/medium/1501-2000/1858.mjs
@@ -1,3 +1,14 @@
 // 1858 - Longest Word With All Prefixes
 
-
+function longestWord(words) {
+    words.sort((a, b) => b.length - a.length || (a < b ? -1 : 1));
+    const set = new Set(words);
+    L: for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        for (let j = 1; j < word.length; j++) {
+            if (!set.has(word.slice(0, j))) continue L;
+        }
+        return word;
+    }
+    return '';
+}
