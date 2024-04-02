984fa1bbb21b2b10d757d7a146024ce516da4a1f
Kevin Huang <kevwhuang@gmail.com>
2024-04-02 17:00:47 -0500

    LC 1642/2116

--- problems/medium/2001-2500/2116.js
+++ problems/medium/2001-2500/2116.js
@@ -1,3 +1,18 @@
 // 2116 - Check If a Parentheses String Can Be Valid
 
-
+function canBeValid(s, locked) {
+    if (s.length % 2) return false;
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || locked[i] === '0') count++;
+        else count--;
+        if (count < 0) return false;
+    }
+    count = 0;
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] === ')' || locked[i] === '0') count++;
+        else count--;
+        if (count < 0) return false;
+    }
+    return true;
+}
