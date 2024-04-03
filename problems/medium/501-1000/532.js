76f1f186acbd39f50d67d964b2f2bf7e603f7a10
Kevin Huang <kevwhuang@gmail.com>
2024-04-03 15:19:31 -0500

    L 532/556

--- problems/medium/501-1000/532.js
+++ problems/medium/501-1000/532.js
@@ -1,3 +1,19 @@
 // 532 - K-Diff Pairs in an Array
 
-
+function findPairs(nums, k) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        map.set(nums[i], (map.get(nums[i]) ?? 0) + 1);
+    }
+    let pairs = 0;
+    if (k) {
+        for (const num of map.keys()) {
+            if (map.has(num + k)) pairs++;
+        }
+    } else {
+        for (const num of map.keys()) {
+            if (map.get(num) >= 2) pairs++;
+        }
+    }
+    return pairs;
+}
--- problems/medium/501-1000/556.js
+++ problems/medium/501-1000/556.js
@@ -1,3 +1,21 @@
 // 556 - Next Greater Element III
 
-
+function nextGreaterElement(n) {
+    function swap(i, j) {
+        const temp = n[i];
+        n[i] = n[j];
+        n[j] = temp;
+    }
+    n = n.toString().split('');
+    for (let i = n.length - 2; i >= 0; i--) {
+        if (n[i] >= n[i + 1]) continue;
+        let j = n.length - 1;
+        while (n[i] >= n[j]) j--;
+        swap(i, j);
+        j = n.length - 1;
+        while (i < j) swap(++i, j--);
+        n = Number(n.join(''));
+        return n <= 2147483647 ? n : -1;
+    }
+    return -1;
+}
