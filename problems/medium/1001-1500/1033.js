b767554126dc72059e0307392ebad504fc13fef1
Kevin Huang <kevwhuang@gmail.com>
2024-04-04 18:05:54 -0500

    LC 1033/1625

--- problems/medium/1001-1500/1033.js
+++ problems/medium/1001-1500/1033.js
@@ -1,3 +1,9 @@
 // 1033 - Moving Stones Until Consecutive
 
-
+function numMovesStones(a, b, c) {
+    [a, b, c] = [a, b, c].sort((a, b) => a - b);
+    let min = 2;
+    if (c - a === 2) min = 0;
+    else if (b - a <= 2 || c - b <= 2) min = 1;
+    return [min, c - a - 2];
+}
--- problems/medium/1501-2000/1625.js
+++ problems/medium/1501-2000/1625.js
@@ -1,3 +1,28 @@
 // 1625 - Lexicographically Smallest String After Applying Operations
 
-
+function findLexSmallestString(s, a, b) {
+    let res = s, queue = [s];
+    const seen = new Set(queue), len = s.length;
+    while (queue.length) {
+        const nextQueue = [];
+        for (let i = 0; i < queue.length; i++) {
+            let cur = queue[i];
+            if (cur < res) res = cur;
+            cur = cur.slice(len - b) + cur.slice(0, len - b);
+            if (!seen.has(cur)) {
+                seen.add(cur);
+                nextQueue.push(cur);
+            }
+            cur = queue[i].split('');
+            for (let j = 1; j < len; j += 2) {
+                cur[j] = (Number(cur[j]) + a) % 10;
+            }
+            cur = cur.join('');
+            if (seen.has(cur)) continue;
+            seen.add(cur);
+            nextQueue.push(cur);
+        }
+        queue = nextQueue;
+    }
+    return res;
+}
