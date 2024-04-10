b63326a7396970916e3be45498aed50a75b3c3d3
Kevin Huang <kevwhuang@gmail.com>
2024-04-10 01:54:05 -0500

    LC 1792/2162

--- problems/medium/1501-2000/1792.js
+++ problems/medium/1501-2000/1792.js
@@ -1,3 +1,21 @@
 // 1792 - Maximum Average Pass Ratio
 
-
+function maxAverageRatio(classes, extraStudents) {
+    function comparator(a, b) {
+        const diffA = (a[1] - a[0]) / (a[1] * a[1] + a[1]);
+        const diffB = (b[1] - b[0]) / (b[1] * b[1] + b[1]);
+        return diffB - diffA;
+    }
+    let sum = 0;
+    const pq = new PriorityQueue({ compare: comparator });
+    for (let i = 0; i < classes.length; i++) {
+        sum += classes[i][0] / classes[i][1];
+        pq.enqueue(classes[i]);
+    }
+    while (extraStudents--) {
+        const cur = pq.dequeue();
+        sum = sum - cur[0] / cur[1] + ++cur[0] / ++cur[1];
+        pq.enqueue(cur);
+    }
+    return sum / classes.length;
+}
--- problems/medium/2001-2500/2162.js
+++ problems/medium/2001-2500/2162.js
@@ -1,3 +1,20 @@
 // 2162 - Minimum Cost to Set Cooking Time
 
-
+function minCostSetTime(startAt, moveCost, pushCost, targetSeconds) {
+    function calculate(str) {
+        let i = 0;
+        while (str[i] === '0') i++;
+        let cost = (str.length - i) * pushCost;
+        if (str[i++] !== String(startAt)) cost += moveCost;
+        while (i < str.length) {
+            if (str[i - 1] !== str[i++]) cost += moveCost;
+        }
+        return cost;
+    }
+    let min = Math.floor(targetSeconds / 60);
+    let sec = targetSeconds - 60 * min;
+    if (min > 99) return calculate(String(min - 1) + String(sec + 60));
+    const cost = calculate(String(min) + String(sec).padStart(2, '0'));
+    if (min === 0 || sec > 39) return cost;
+    return Math.min(cost, calculate(String(min - 1) + String(sec + 60)));
+}
