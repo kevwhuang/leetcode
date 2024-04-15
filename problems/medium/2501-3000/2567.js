25f8d2735a434affef1884cb1af0c5e1aef10bef
Kevin Huang <kevwhuang@gmail.com>
2024-04-14 22:42:38 -0500

    LC 2567/2568

--- problems/medium/2501-3000/2567.js
+++ problems/medium/2501-3000/2567.js
@@ -1,3 +1,16 @@
 // 2567 - Minimum Score by Changing Two Elements
 
-
+function minimizeSum(nums) {
+    let min1 = 1e9, min2 = 1e9, min3 = 1e9;
+    let max1 = 1, max2 = 1, max3 = 1;
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (num < min1) [min1, min2, min3] = [num, min1, min2];
+        else if (num < min2) [min2, min3] = [num, min2];
+        else if (num < min3) min3 = num;
+        if (num > max1) [max3, max2, max1] = [max2, max1, num];
+        else if (num > max2) [max3, max2] = [max2, num];
+        else if (num > max3) max3 = num;
+    }
+    return Math.min(max1 - min3, max2 - min2, max3 - min1);
+}
--- problems/medium/2501-3000/2568.js
+++ problems/medium/2501-3000/2568.js
@@ -1,3 +1,13 @@
 // 2568 - Minimum Impossible OR
 
-
+function minImpossibleOR(nums) {
+    const set = new Set();
+    const bound = 2 ** 30;
+    for (let n = 1; n <= bound; n *= 2) {
+        set.add(n);
+    }
+    for (let i = 0; i < nums.length; i++) {
+        set.delete(nums[i]);
+    }
+    return set.keys().next().value;
+}
