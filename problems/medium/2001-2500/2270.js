5f3079f86fde8b520d1ea6e11a868edb0a95243d
Kevin Huang <kevwhuang@gmail.com>
2024-04-17 20:34:35 -0500

    LC 2270/2425

--- problems/medium/2001-2500/2270.js
+++ problems/medium/2001-2500/2270.js
@@ -1,3 +1,13 @@
 // 2270 - Number of Ways to Split Array
 
-
+function waysToSplitArray(nums) {
+    let splits = 0, leftSum = 0;
+    let rightSum = nums.reduce((s, e) => s + e);
+    const bound = nums.length - 1;
+    for (let i = 0; i < bound; i++) {
+        leftSum += nums[i];
+        rightSum -= nums[i];
+        if (leftSum >= rightSum) splits++;
+    }
+    return splits;
+}
--- problems/medium/2001-2500/2425.js
+++ problems/medium/2001-2500/2425.js
@@ -1,3 +1,12 @@
 // 2425 - Bitwise XOR of All Pairings
 
-
+function xorAllNums(nums1, nums2) {
+    const reducer = (xor, num) => xor ^= num;
+    const m = nums1.length, n = nums2.length;
+    if (m % 2) {
+        const xor = nums2.reduce(reducer);
+        return n % 2 ? xor ^ nums1.reduce(reducer) : xor;
+    } else {
+        return n % 2 ? nums1.reduce(reducer) : 0;
+    }
+}
