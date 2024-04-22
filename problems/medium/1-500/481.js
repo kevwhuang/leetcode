496e56e54fc39f56bbe83f0996a522ea7bee0e91
Kevin Huang <kevwhuang@gmail.com>
2024-04-22 14:22:56 -0500

    LC 481/522

--- problems/medium/1-500/481.js
+++ problems/medium/1-500/481.js
@@ -1,3 +1,17 @@
 // 481 - Magical String
 
-
+function magicalString(n) {
+    const arr = new Uint8Array(n);
+    arr[0] = 1, arr[1] = arr[2] = 2;
+    for (let i = 2, j = 2; j < n; i++) {
+        const num = 3 - arr[j];
+        arr[++j] = num;
+        if (j === n) break;
+        if (arr[i] === 2) arr[++j] = num;
+    }
+    let ones = 1;
+    for (let i = 3; i < n; i++) {
+        if (arr[i] === 1) ones++;
+    }
+    return ones;
+}
--- problems/medium/501-1000/522.js
+++ problems/medium/501-1000/522.js
@@ -1,3 +1,23 @@
 // 522 - Longest Uncommon Subsequence II
 
-
+function findLUSlength(strs) {
+    function checkSubsequence(s1, s2) {
+        let p1 = 0, p2 = 0;
+        while (p1 < s1.length && p2 < s2.length) {
+            if (s1[p1] === s2[p2++]) p1++;
+        }
+        return p1 === s1.length;
+    }
+    strs.sort((a, b) => b.length - a.length);
+    for (let i = 0; i < strs.length; i++) {
+        let hasSubsequence = false;
+        for (let j = 0; j < strs.length; j++) {
+            if (i === j) continue;
+            if (!checkSubsequence(strs[i], strs[j])) continue;
+            hasSubsequence = true;
+            break;
+        }
+        if (!hasSubsequence) return strs[i].length;
+    }
+    return -1;
+}
