59398c6056bd6b41d7cc079efc27b22c139575f7
Kevin Huang <kevwhuang@gmail.com>
2024-04-24 16:35:27 -0500

    LC 945/1043/1054

--- problems/medium/1001-1500/1043.js
+++ problems/medium/1001-1500/1043.js
@@ -1,3 +1,15 @@
 // 1043 - Partition Array for Maximum Sum
 
-
+function maxSumAfterPartitioning(arr, k) {
+    const dp = new Array(arr.length + 1);
+    dp[0] = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let max = 0, maxSum = 0;
+        for (let j = 0; j <= i && j < k; j++) {
+            max = Math.max(arr[i - j], max);
+            maxSum = Math.max(dp[i - j] + max * (j + 1), maxSum);
+        }
+        dp[i + 1] = maxSum;
+    }
+    return dp.at(-1);
+}
--- problems/medium/1001-1500/1054.js
+++ problems/medium/1001-1500/1054.js
@@ -1,3 +1,17 @@
 // 1054 - Distant Barcodes
 
-
+function rearrangeBarcodes(barcodes) {
+    const map = new Map();
+    for (let i = 0; i < barcodes.length; i++) {
+        map.set(barcodes[i], (map.get(barcodes[i]) ?? 0) + 1);
+    }
+    const arr = [...map].sort((a, b) => b[1] - a[1]);
+    for (let i = 0, j = 0; j < arr.length; j++) {
+        while (arr[j][1]--) {
+            barcodes[i] = arr[j][0];
+            i += 2;
+            if (i >= barcodes.length) i = 1;
+        }
+    }
+    return barcodes;
+}
