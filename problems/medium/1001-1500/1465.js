452fe6d4d06381747c0bc679e6e7346f4f64e62a
Kevin Huang <kevwhuang@gmail.com>
2024-04-25 02:16:23 -0500

    LC 659/1465

--- problems/medium/1001-1500/1465.js
+++ problems/medium/1001-1500/1465.js
@@ -1,3 +1,14 @@
 // 1465 - Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts
 
-
+function maxArea(h, w, horizontalCuts, verticalCuts) {
+    function len(arr, bound) {
+        arr.sort((a, b) => a - b);
+        let max = Math.max(arr[0], bound - arr[arr.length - 1]);
+        for (let i = 1; i < arr.length; i++) {
+            max = Math.max(arr[i] - arr[i - 1], max);
+        }
+        return BigInt(max);
+    }
+    const mod = BigInt(1e9 + 7);
+    return len(horizontalCuts, h) * len(verticalCuts, w) % mod;
+}
--- problems/medium/501-1000/659.js
+++ problems/medium/501-1000/659.js
@@ -1,3 +1,26 @@
 // 659 - Split Array Into Consecutive Subsequences
 
-
+function isPossible(nums) {
+    const map = {};
+    for (let i = 0; i < nums.length; i++) {
+        map[nums[i]] ??= 0;
+        map[nums[i]]++;
+    }
+    const next = {};
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (map[num] === 0) continue;
+        map[num]--;
+        if (next[num]) {
+            next[num]--;
+            next[num + 1] ??= 0;
+            next[num + 1]++;
+        } else if (map[num + 1] && map[num + 2]) {
+            map[num + 1]--;
+            map[num + 2]--;
+            next[num + 3] ??= 0;
+            next[num + 3]++;
+        } else return false;
+    }
+    return true;
+}
