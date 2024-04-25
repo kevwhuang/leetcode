918161a014c0a5c15cd4803ff215152d5b7db70d
Kevin Huang <kevwhuang@gmail.com>
2024-04-25 14:07:27 -0500

    LC 1353

--- problems/medium/1001-1500/1353.js
+++ problems/medium/1001-1500/1353.js
@@ -1,3 +1,15 @@
 // 1353 - Maximum Number of Events That Can Be Attended
 
-
+function maxEvents(events) {
+    events.sort((a, b) => a[0] - b[0]);
+    const pq = new MinPriorityQueue({ compare: (a, b) => a[1] - b[1] });
+    const len = events.length;
+    let count = 0, i = 0, n = 1;
+    while (i < len || !pq.isEmpty()) {
+        while (i < len && events[i][0] === n) pq.enqueue(events[i++]);
+        if (!pq.isEmpty() && pq.dequeue()) count++;
+        while (!pq.isEmpty() && pq.front()[1] === n) pq.dequeue();
+        n++;
+    }
+    return count;
+}
