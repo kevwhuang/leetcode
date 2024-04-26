7b6696d1f6d77885720094613c61ff4e1972f149
Kevin Huang <kevwhuang@gmail.com>
2024-04-26 16:14:40 -0500

    LC 784/2044

--- problems/medium/2001-2500/2044.js
+++ problems/medium/2001-2500/2044.js
@@ -1,3 +1,13 @@
 // 2044 - Count Number of Maximum Bitwise OR Subsets
 
-
+function countMaxOrSubsets(nums) {
+    function backtrack(i, or) {
+        if (i === nums.length) return or === max && ++count;
+        backtrack(i + 1, or);
+        backtrack(i + 1, or | nums[i]);
+    }
+    let count = 0;
+    const max = nums.reduce((s, e) => s | e, 0);
+    backtrack(0, 0);
+    return count;
+}
--- problems/medium/501-1000/784.js
+++ problems/medium/501-1000/784.js
@@ -1,3 +1,22 @@
 // 784 - Letter Case Permutation
 
-
+function letterCasePermutation(s) {
+    function backtrack(i) {
+        if (i === len) return res.push(cur.join(''));
+        if (set.has(s[i])) {
+            cur[i] = s[i].toUpperCase();
+            backtrack(i + 1);
+        }
+        cur[i] = s[i];
+        backtrack(i + 1);
+    }
+    s = s.toLowerCase();
+    const set = new Set();
+    for (let code = 97; code <= 122; code++) {
+        set.add(String.fromCharCode(code));
+    }
+    const res = [], len = s.length;
+    const cur = new Array(len);
+    backtrack(0);
+    return res;
+}
