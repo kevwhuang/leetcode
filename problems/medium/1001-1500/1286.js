a2eab9b90ab528333ce7d0643b0aafeb0060352c
Kevin Huang <kevwhuang@gmail.com>
2024-04-26 21:58:53 -0500

    LC 1286/1415

--- problems/medium/1001-1500/1286.js
+++ problems/medium/1001-1500/1286.js
@@ -1,3 +1,25 @@
 // 1286 - Iterator for Combination
 
-
+class CombinationIterator {
+    constructor(characters, combinationLength) {
+        this.characters = characters;
+        this.combinationLength = combinationLength;
+        this.i = 0;
+        this.permutations = [];
+        this.#backtrack(0, '');
+    }
+    hasNext() {
+        return this.i < this.permutations.length;
+    }
+    next() {
+        return this.permutations[this.i++];
+    }
+    #backtrack(i, cur) {
+        if (cur.length === this.combinationLength) {
+            return this.permutations.push(cur);
+        }
+        if (i === this.characters.length) return;
+        this.#backtrack(i + 1, cur + this.characters[i]);
+        this.#backtrack(i + 1, cur);
+    }
+}
--- problems/medium/1001-1500/1415.js
+++ problems/medium/1001-1500/1415.js
@@ -1,3 +1,23 @@
 // 1415 - The Kth Lexicographical String of All Happy Strings of Length N
 
-
+function getHappyString(n, k) {
+    function backtrack(cur) {
+        if (count === k) return;
+        if (cur.length === n) return (res = cur) && count++;
+        if (cur[cur.length - 1] === 'a') {
+            backtrack(cur + 'b');
+            backtrack(cur + 'c');
+        } else if (cur[cur.length - 1] === 'b') {
+            backtrack(cur + 'a');
+            backtrack(cur + 'c');
+        } else {
+            backtrack(cur + 'a');
+            backtrack(cur + 'b');
+        }
+    }
+    let res, count = 0;
+    backtrack('a');
+    backtrack('b');
+    backtrack('c');
+    return count === k ? res : '';
+}
