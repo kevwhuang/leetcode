135153e8cf5915a9f4b71ee3ad6bfba076bd322a
Kevin Huang <kevwhuang@gmail.com>
2024-04-28 04:19:24 -0500

    LC 491

--- problems/medium/1-500/491.js
+++ problems/medium/1-500/491.js
@@ -1,3 +1,19 @@
 // 491 - Non-Decreasing Subsequences
 
-
+function findSubsequences(nums) {
+    function backtrack(i) {
+        if (cur.length >= 2) subsequences.push([...cur]);
+        const seen = new Set();
+        for (let j = i; j < nums.length; j++) {
+            if (cur.length && nums[j] < cur[cur.length - 1]) continue;
+            if (seen.has(nums[j])) continue;
+            seen.add(nums[j]);
+            cur.push(nums[j]);
+            backtrack(j + 1);
+            cur.pop();
+        }
+    }
+    const subsequences = [], cur = [];
+    backtrack(0);
+    return subsequences;
+}
