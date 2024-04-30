695e3b0d018deb0b6189eae0f4e08ff3603f4b24
Kevin Huang <kevwhuang@gmail.com>
2024-04-30 06:11:36 -0500

    LC 357/378

--- problems/medium/1-500/357.js
+++ problems/medium/1-500/357.js
@@ -1,3 +1,10 @@
 // 357 - Count Numbers With Unique Digits
 
-
+function countNumbersWithUniqueDigits(n) {
+    if (n-- === 0) return 1;
+    let unique = 9;
+    for (let d = 0; d < n; d++) {
+        unique *= 9 - d;
+    }
+    return unique + countNumbersWithUniqueDigits(n);
+}
--- problems/medium/1-500/378.js
+++ problems/medium/1-500/378.js
@@ -1,3 +1,12 @@
 // 378 - Kth Smallest Element in a Sorted Matrix
 
-
+function kthSmallest(matrix, k) {
+    const m = matrix.length, n = matrix[0].length;
+    const arr = new Array(m * n);
+    for (let r = 0; r < m; r++) {
+        for (let c = 0; c < n; c++) {
+            arr[r * m + c] = matrix[r][c];
+        }
+    }
+    return arr.sort((a, b) => a - b)[k - 1];
+}
