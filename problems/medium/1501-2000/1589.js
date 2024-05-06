fa0b9ab11b606fe7e21352d1f640b47e43b77180
Kevin Huang <kevwhuang@gmail.com>
2024-05-06 18:34:45 -0500

    LC 1589/2049

--- problems/medium/1501-2000/1589.js
+++ problems/medium/1501-2000/1589.js
@@ -1,3 +1,26 @@
 // 1589 - Maximum Sum Obtained of Any Permutation
 
-
+function maxSumRangeQuery(nums, requests) {
+    let end = -Infinity;
+    for (let i = 0; i < requests.length; i++) {
+        end = Math.max(requests[i][1], end);
+    }
+    const prefix = new Array(end + 2).fill(0);
+    for (let i = 0; i < requests.length; i++) {
+        prefix[requests[i][0]]++;
+        prefix[requests[i][1] + 1]--;
+    }
+    for (let i = 1; i < prefix.length; i++) {
+        prefix[i] += prefix[i - 1];
+    }
+    prefix.pop();
+    prefix.sort((a, b) => b - a);
+    nums.sort((a, b) => b - a);
+    let sum = 0;
+    const mod = 1e9 + 7;
+    for (let i = 0; i < prefix.length; i++) {
+        sum += prefix[i] * nums[i];
+        sum %= mod;
+    }
+    return sum;
+}
--- problems/medium/2001-2500/2049.js
+++ problems/medium/2001-2500/2049.js
@@ -1,3 +1,33 @@
 // 2049 - Count Nodes With the Highest Score
 
-
+function countHighestScoreNodes(parents) {
+    function dfs(node) {
+        let count = 1, score = 1;
+        if (left[node]) {
+            const subcount = dfs(left[node]);
+            count += subcount;
+            score *= subcount;
+        }
+        if (right[node]) {
+            const subcount = dfs(right[node]);
+            count += subcount;
+            score *= subcount;
+        }
+        score *= parents.length - count || 1;
+        if (score === max) res++;
+        else if (score > max) {
+            res = 1;
+            max = score;
+        }
+        return count;
+    }
+    const left = new Uint32Array(parents.length);
+    const right = new Uint32Array(parents.length);
+    for (let i = 1; i < parents.length; i++) {
+        if (left[parents[i]]) right[parents[i]] = i;
+        else left[parents[i]] = i;
+    }
+    let res = 0, max = 1;
+    dfs(0);
+    return res;
+}
