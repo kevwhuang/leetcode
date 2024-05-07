bd5e450e99a4350fee08e077badd1dac799493c5
Kevin Huang <kevwhuang@gmail.com>
2024-05-07 05:17:42 -0500

    LC 1530/1915

--- problems/medium/1501-2000/1530.js
+++ problems/medium/1501-2000/1530.js
@@ -1,3 +1,35 @@
 // 1530 - Number of Good Leaf Nodes Pairs
 
-
+function countPairs(root, distance) {
+    function dfs1(node) {
+        if (!node.left && !node.right) return leaves.add(node);
+        if (node.left) {
+            parents.set(node.left, node);
+            dfs1(node.left);
+        }
+        if (node.right) {
+            parents.set(node.right, node);
+            dfs1(node.right);
+        }
+    }
+    function dfs2(node, prev, dist) {
+        if (dist > distance) return;
+        if (leaves.has(node)) count++;
+        if (node.left && node.left !== prev) {
+            dfs2(node.left, node, dist + 1);
+        }
+        if (node.right && node.right !== prev) {
+            dfs2(node.right, node, dist + 1);
+        }
+        if (parents.has(node) && parents.get(node) !== prev) {
+            dfs2(parents.get(node), node, dist + 1);
+        }
+    }
+    const leaves = new Set(), parents = new Map();
+    dfs1(root);
+    let count = -leaves.size;
+    for (const leaf of leaves) {
+        dfs2(leaf, null, 0);
+    }
+    return count / 2;
+}
--- problems/medium/1501-2000/1915.js
+++ problems/medium/1501-2000/1915.js
@@ -1,3 +1,15 @@
 // 1915 - Number of Wonderful Substrings
 
-
+function wonderfulSubstrings(word) {
+    const arr = new Uint32Array(1024);
+    arr[0] = 1;
+    let substrings = 0, mask = 0;
+    for (let i = 0; i < word.length; i++) {
+        mask ^= 1 << word.charCodeAt(i) - 97;
+        substrings += arr[mask]++;
+        for (let n = 0; n < 10; n++) {
+            substrings += arr[mask ^ 1 << n];
+        }
+    }
+    return substrings;
+}
