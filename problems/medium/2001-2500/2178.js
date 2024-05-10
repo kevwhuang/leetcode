c167964bc1e542e259409ba9781c6cb6150f9372
Kevin Huang <kevwhuang@gmail.com>
2024-05-10 02:36:45 -0500

    LC 2178/2933

--- problems/medium/2001-2500/2178.js
+++ problems/medium/2001-2500/2178.js
@@ -1,3 +1,18 @@
 // 2178 - Maximum Split of Positive Even Integers
 
-
+function maximumEvenSplit(finalSum) {
+    if (finalSum & 1) return [];
+    const res = [];
+    let n = 0;
+    while (finalSum > 0) {
+        n += 2;
+        finalSum -= n;
+        res.push(n);
+    }
+    const last = res.length - 1;
+    res[last] -= Math.abs(finalSum);
+    if (res.length > 1 && res[last] <= res[last - 1]) {
+        res[last - 1] += res.pop();
+    }
+    return res;
+}
--- problems/medium/2501-3000/2933.js
+++ problems/medium/2501-3000/2933.js
@@ -1,3 +1,17 @@
 // 2933 - High-Access Employees
 
-
+function findHighAccessEmployees(access_times) {
+    function comparator(a, b) {
+        return a[0].localeCompare(b[0]) || a[1].localeCompare(b[1]);
+    }
+    access_times.sort(comparator);
+    const set = new Set(), bound = access_times.length - 2;
+    for (let i = 0; i < bound; i++) {
+        const t1 = access_times[i], t2 = access_times[i + 2];
+        if (t1[0] !== t2[0]) continue;
+        let min = 60 * (t2[1].slice(0, 2) - t1[1].slice(0, 2));
+        min += t2[1].slice(2) - t1[1].slice(2);
+        if (min < 60) set.add(t1[0]);
+    }
+    return [...set];
+}
