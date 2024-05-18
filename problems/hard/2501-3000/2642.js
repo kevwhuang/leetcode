c5d173c90a2781322e1c9d03e3167bea2c783672
Kevin Huang <kevwhuang@gmail.com>
2024-05-17 20:13:33 -0500

    LC 2856

--- problems/hard/2501-3000/2642.js
+++ problems/hard/2501-3000/2642.js
@@ -2,62 +2,33 @@
 
 class Graph {
     constructor(n, edges) {
+        this.comparator = (a, b) => a[1] - b[1];
         this.adj = {};
         for (let i = 0; i < edges.length; i++) {
             this.addEdge(edges[i]);
         }
     }
     addEdge(edge) {
-        const v = edge[0];
-        if (v in this.adj) this.adj[v].push(edge.slice(1));
-        else this.adj[v] = [[edge[1], edge[2]]];
+        if (!this.adj[edge[0]]) this.adj[edge[0]] = [];
+        this.adj[edge[0]].push(edge.slice(1));
     }
     shortestPath(node1, node2) {
         const costs = { [node1]: 0 };
-        const pq = new MinimumPriorityQueue();
-        pq.enqueue(node1, 0);
-        while (pq.data.length > 1) {
-            const [v, cost] = pq.dequeue();
-            if (v === node2) return costs[node2];
-            if (!(v in this.adj)) continue;
-            const neighbors = this.adj[v];
+        const pq = new PriorityQueue({ compare: this.comparator });
+        pq.enqueue([node1, 0]);
+        while (pq.size()) {
+            const [u, cost] = pq.dequeue();
+            if (u === node2) return costs[u];
+            const neighbors = this.adj[u];
+            if (!neighbors) continue;
             for (let i = 0; i < neighbors.length; i++) {
-                const neighbor = neighbors[i][0];
-                const newCost = neighbors[i][1] + cost;
-                if (newCost >= costs[neighbor]) continue;
-                costs[neighbor] = newCost;
-                pq.enqueue(neighbor, newCost);
+                const v = neighbors[i][0];
+                const nextCost = neighbors[i][1] + cost;
+                if (nextCost >= costs[v]) continue;
+                costs[v] = nextCost;
+                pq.enqueue([v, nextCost]);
             }
         }
         return -1;
-    }
-}
-
-class MinimumPriorityQueue {
-    constructor() {
-        this.data = [null];
-    }
-    dequeue() {
-        if (this.data.length === 1) return null;
-        if (this.data.length === 2) return this.data.pop();
-        const entry = this.data[1];
-        this.data[1] = this.data.pop();
-        let top = 1, l = 2, r = 3;
-        let next = !this.data[r] || this.data[l][1] < this.data[r][1] ? l : r;
-        while (this.data[top]?.[1] > this.data[next]?.[1]) {
-            [this.data[top], this.data[next]] = [this.data[next], this.data[top]];
-            [top, l, r] = [next, 2 * next, 2 * next + 1];
-            next = !this.data[r] || this.data[l][1] < this.data[r][1] ? l : r;
-        }
-        return entry;
-    }
-    enqueue(val, priority) {
-        this.data.push([val, priority]);
-        let index = this.data.length - 1, parent = ~~(index / 2);
-        while (parent && priority < this.data[parent][1]) {
-            [this.data[index], this.data[parent]] = [this.data[parent], this.data[index]];
-            [index, parent] = [parent, ~~(parent / 2)];
-        }
-        return val;
     }
 }
--- problems/medium/2501-3000/2856.js
+++ problems/medium/2501-3000/2856.js
@@ -1,3 +1,10 @@
 // 2856 - Minimum Array Length After Pair Removals
 
-
+function minLengthAfterRemovals(nums) {
+    const len = nums.length, half = len / 2 >> 0;
+    let l = 0, r = half;
+    while (l < half && r < len) {
+        if (nums[l] < nums[r++]) l++;
+    }
+    return nums.length - l - l;
+}
