fa6f3f656c5d83940a33d67ea2c38049e0364fbb
Kevin Huang <kevwhuang@gmail.com>
2024-05-20 23:56:02 -0500

    LC 2195

--- problems/medium/2001-2500/2195.js
+++ problems/medium/2001-2500/2195.js
@@ -1,3 +1,14 @@
 // 2195 - Append K Integers With Minimal Sum
 
-
+function minimalKSum(nums, k) {
+    nums.sort((a, b) => a - b);
+    let sum = 0;
+    for (let i = 0; i < nums.length && k; i++) {
+        const l = (nums[i - 1] ?? 0) + 1;
+        const r = Math.min(nums[i] - 1, l + k - 1);
+        if (l > r) continue;
+        sum += (l + r) * (r - l + 1) / 2;
+        k -= r - l + 1;
+    }
+    return sum + (2 * nums[nums.length - 1] + k + 1) * k / 2;
+}
