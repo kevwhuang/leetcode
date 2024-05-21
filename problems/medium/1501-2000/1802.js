def1592f7af9a345994e51a046a0b6f948a4d019
Kevin Huang <kevwhuang@gmail.com>
2024-05-21 16:14:28 -0500

    LC 1802/2761

--- problems/medium/1501-2000/1802.js
+++ problems/medium/1501-2000/1802.js
@@ -1,3 +1,21 @@
 // 1802 - Maximum Value at a Given Index in a Bounded Array
 
-
+function maxValue(n, index, maxSum) {
+    function sum(start, count) {
+        const terms = Math.min(start, count);
+        total += (2 * start - terms + 1) * terms / 2;
+        if (count > start) total += count - start;
+    }
+    if (n === 1) return maxSum;
+    let l = Math.floor(maxSum / n), r = maxSum, total;
+    while (l <= r) {
+        const m = l + r >> 1;
+        total = m;
+        sum(m - 1, index);
+        sum(m - 1, n - index - 1);
+        if (total < maxSum) l = m + 1;
+        else if (total > maxSum) r = m - 1;
+        else return m;
+    }
+    return l - 1;
+}
--- problems/medium/2501-3000/2761.js
+++ problems/medium/2501-3000/2761.js
@@ -1,3 +1,19 @@
 // 2761 - Prime Pairs With Target Sum
 
-
+function findPrimePairs(n) {
+    const sieve = new Uint8Array(n + 1);
+    const lim1 = Math.sqrt(n + 1);
+    for (let i = 2; i <= lim1; i++) {
+        if (sieve[i]) continue;
+        for (let j = i * i; j <= n; j += i) {
+            sieve[j] = 1;
+        }
+    }
+    const res = [];
+    const lim2 = n / 2;
+    for (let i = 2; i <= lim2; i++) {
+        if (sieve[i] || sieve[n - i]) continue;
+        res.push([i, n - i]);
+    }
+    return res;
+}
