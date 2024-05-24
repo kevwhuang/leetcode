17b43e95273f8339fc9ad0052613879dcc5ff9ce
Kevin Huang <kevwhuang@gmail.com>
2024-05-24 00:56:47 -0500

    LC 2929/2982

--- problems/medium/2501-3000/2929.js
+++ problems/medium/2501-3000/2929.js
@@ -1,3 +1,10 @@
 // 2929 - Distribute Candies Among Children II
 
-
+function distributeCandies(n, limit) {
+    const calculate = n => n < 2 ? 0 : n * (n - 1) / 2;
+    let ways = calculate(n + 2);
+    ways -= 3 * calculate(n - limit + 1);
+    ways += 3 * calculate(n - 2 * limit);
+    ways -= calculate(n - 3 * limit - 1);
+    return ways;
+}
--- problems/medium/2501-3000/2982.js
+++ problems/medium/2501-3000/2982.js
@@ -1,3 +1,31 @@
 // 2982 - Find Longest Special Substring That Occurs Thrice II
 
-
+function maximumLength(s) {
+    function insert(i) {
+        const code = s.charCodeAt(i - 1) - 97;
+        const freq = (arr[code].get(window) ?? 0) + 1;
+        arr[code].set(window, freq);
+        window = 1;
+    }
+    const update = len => max = Math.max(len, max);
+    const arr = Array.from({ length: 26 }, () => new Map());
+    let window = 1;
+    for (let i = 1; i < s.length; i++) {
+        s[i] === s[i - 1] ? window++ : insert(i);
+    }
+    insert(s.length);
+    let max = -1;
+    for (let i = 0; i < 26; i++) {
+        const map = arr[i];
+        for (const len of map.keys()) {
+            if (len <= max) continue;
+            const freq = map.get(len);
+            if (freq >= 3 && update(len) || len === 1) continue;
+            if (map.has(1)) update(1);
+            if (len === 2) continue;
+            if ((freq === 2 || map.has(len - 1)) && update(len - 1)) continue;
+            update(len - 2);
+        }
+    }
+    return max;
+}
