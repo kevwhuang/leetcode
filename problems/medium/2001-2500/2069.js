5c5baf68a8d805c4cb9d7daaf212af025fdda294
Kevin Huang <kevwhuang@gmail.com>
2024-05-29 14:35:54 -0500

    LC 2069/2598

--- problems/medium/2001-2500/2069.js
+++ problems/medium/2001-2500/2069.js
@@ -1,3 +1,51 @@
 // 2069 - Walking Robot Simulation II
 
-
+class Robot {
+    constructor(width, height) {
+        this.x = 0;
+        this.y = 0;
+        this.dir = 0;
+        this.width = width - 1;
+        this.height = height - 1;
+        this.mod = 2 * (this.width + this.height);
+        this.labels = ['East', 'North', 'West', 'South'];
+    }
+    getDir() {
+        return this.labels[this.dir];
+    }
+    getPos() {
+        return [this.x, this.y];
+    }
+    step(num) {
+        num %= this.mod;
+        if (!num && !this.x && !this.y) return this.dir = 3;
+        let dist;
+        while (num) {
+            switch (this.dir) {
+                case 0:
+                    dist = Math.min(this.width - this.x, num);
+                    this.x += dist;
+                    num -= dist;
+                    if (num && this.x === this.width) this.dir = 1;
+                    break;
+                case 1:
+                    dist = Math.min(this.height - this.y, num);
+                    this.y += dist;
+                    num -= dist;
+                    if (num && this.y === this.height) this.dir = 2;
+                    break;
+                case 2:
+                    dist = Math.min(this.x, num);
+                    this.x -= dist;
+                    num -= dist;
+                    if (num && !this.x) this.dir = 3;
+                    break;
+                default:
+                    dist = Math.min(this.y, num);
+                    this.y -= dist;
+                    num -= dist;
+                    if (num && !this.y) this.dir = 0;
+            }
+        }
+    }
+}
--- problems/medium/2501-3000/2598.js
+++ problems/medium/2501-3000/2598.js
@@ -1,3 +1,13 @@
 // 2598 - Smallest Missing Non-Negative Integer After Operations
 
-
+function findSmallestInteger(nums, value) {
+    const bucket = new Array(value).fill(0);
+    for (let i = 0; i < nums.length; i++) {
+        bucket[(nums[i] % value + value) % value]++;
+    }
+    let min = 0;
+    for (let n = 1; n < value; n++) {
+        if (bucket[n] < bucket[min]) min = n;
+    }
+    return min + bucket[min] * value;
+}
