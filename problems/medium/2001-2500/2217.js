778bb8ae9c97e0ae90d93603f4350d8a9c4b54e8
Kevin Huang <kevwhuang@gmail.com>
2024-05-31 15:03:20 -0500

    LC 2217

--- problems/medium/2001-2500/2217.js
+++ problems/medium/2001-2500/2217.js
@@ -1,3 +1,21 @@
 // 2217 - Find Palindrome With Fixed Length
 
-
+function kthPalindrome(queries, intLength) {
+    const digits = (intLength + 1) / 2 >> 0;
+    const base = 10 ** (digits - 1);
+    const bound = 10 ** digits - base;
+    for (let i = 0; i < queries.length; i++) {
+        if (queries[i] > bound) queries[i] = -1;
+        else {
+            let palindrome = base + queries[i] - 1;
+            let reflection = palindrome;
+            if (intLength & 1) reflection = reflection / 10 >> 0;
+            while (reflection) {
+                palindrome = 10 * palindrome + reflection % 10;
+                reflection = reflection / 10 >> 0;
+            }
+            queries[i] = palindrome;
+        }
+    }
+    return queries;
+}
