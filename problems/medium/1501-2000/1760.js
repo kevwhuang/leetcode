b647d9c6ec15c607a7fd72dbcafeddd513e83d2b
Kevin Huang <kevwhuang@gmail.com>
2024-05-31 02:59:14 -0500

    LC 1760

--- problems/medium/1501-2000/1760.js
+++ problems/medium/1501-2000/1760.js
@@ -1,3 +1,15 @@
 // 1760 - Minimum Limit of Balls in a Bag
 
-
+function minimumSize(nums, maxOperations) {
+    let l = 1, r = 1e9;
+    while (l <= r) {
+        const m = l + r >> 1;
+        let count = 0;
+        for (let i = 0; i < nums.length; i++) {
+            count += (nums[i] - 1) / m >> 0;
+        }
+        if (count > maxOperations) l = m + 1;
+        else r = m - 1;
+    }
+    return l;
+}
