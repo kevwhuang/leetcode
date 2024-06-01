5abc19dca6772c0dfca3b7f86280937d93782087
Kevin Huang <kevwhuang@gmail.com>
2024-06-01 13:14:53 -0500

    LC 1838

--- problems/medium/1501-2000/1838.js
+++ problems/medium/1501-2000/1838.js
@@ -1,3 +1,25 @@
 // 1838 - Frequency of the Most Frequent Element
 
-
+function maxFrequency(nums, k) {
+    let max = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > max) max = nums[i];
+    }
+    const bucket = new Uint32Array(max + 1);
+    for (let i = 0; i < nums.length; i++) {
+        bucket[nums[i]]++;
+    }
+    for (let i = 0, n = 1; i < nums.length; i++) {
+        while (!bucket[n]) n++;
+        bucket[n]--;
+        nums[i] = n;
+    }
+    let res = 1, acc = nums[0];
+    for (let l = 0, r = 1; r < nums.length; r++) {
+        const num = nums[r];
+        acc += num;
+        while ((r - l + 1) * num - acc > k) acc -= nums[l++];
+        if (r - l + 1 > res) res = r - l + 1;
+    }
+    return res;
+}
