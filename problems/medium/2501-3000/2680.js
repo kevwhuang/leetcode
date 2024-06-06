68b737bae5e68ebac30a10789d1ad5b78176a005
Kevin Huang <kevwhuang@gmail.com>
2024-06-05 20:42:08 -0500

    LC 2680/2865

--- problems/medium/2501-3000/2680.js
+++ problems/medium/2501-3000/2680.js
@@ -1,3 +1,17 @@
 // 2680 - Maximum OR
 
-
+function maximumOr(nums, k) {
+    k = 2 ** k;
+    const suffix = new Uint32Array(nums.length);
+    for (let i = suffix.length - 1; i; i--) {
+        suffix[i - 1] = suffix[i] | nums[i];
+    }
+    let max = 0, prefix = 0;
+    for (let i = 0; i < nums.length; i++) {
+        const num = k * nums[i], num32 = num % 1073741824;
+        const or = num - num32 + (prefix | num32 | suffix[i]);
+        if (or > max) max = or;
+        prefix |= nums[i];
+    }
+    return max;
+}
--- problems/medium/2501-3000/2865.js
+++ problems/medium/2501-3000/2865.js
@@ -1,3 +1,21 @@
 // 2865 - Beautiful Towers I
 
-
+function maximumSumOfHeights(heights) {
+    function traverse(i, inc, limit) {
+        let acc = 0;
+        while (~i && i < heights.length) {
+            limit = Math.min(heights[i], limit);
+            acc += limit;
+            i += inc;
+        }
+        return acc;
+    }
+    let max = 0;
+    for (let i = 0; i < heights.length; i++) {
+        let sum = heights[i];
+        sum += traverse(i - 1, -1, heights[i]);
+        sum += traverse(i + 1, 1, heights[i]);
+        max = Math.max(sum, max);
+    }
+    return max;
+}
