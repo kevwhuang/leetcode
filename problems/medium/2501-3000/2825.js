fd8262cf1a492481c0d123410fbf0d2f3df0dea5
Kevin Huang <kevwhuang@gmail.com>
2024-06-08 02:24:55 -0500

    LC 2825

--- problems/medium/2501-3000/2825.js
+++ problems/medium/2501-3000/2825.js
@@ -1,3 +1,14 @@
 // 2825 - Make String a Subsequence Using Cyclic Increments
 
-
+function canMakeSubsequence(str1, str2) {
+    let i = -1, j = -1;
+    L: while (++j < str2.length) {
+        const code = str2.charCodeAt(j) - 1;
+        const char = code === 96 ? 'z' : String.fromCharCode(code);
+        while (++i < str1.length) {
+            if (str1[i] === str2[j] || str1[i] === char) continue L;
+        }
+        return false;
+    }
+    return true;
+}
