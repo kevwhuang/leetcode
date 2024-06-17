47c7c0419608b75eab937ddc455e42305ac3ce7a
Kevin Huang <kevwhuang@gmail.com>
2024-06-17 01:41:33 -0500

    LC 2024/2207

--- problems/medium/2001-2500/2024.js
+++ problems/medium/2001-2500/2024.js
@@ -1,3 +1,14 @@
 // 2024 - Maximize the Confusion of an Exam
 
-
+function maxConsecutiveAnswers(answerKey, k) {
+    function check(key) {
+        let l = 0, r = 0, rem = k;
+        while (r < answerKey.length) {
+            if (answerKey[r++] === key) rem--;
+            if (rem >= 0) continue;
+            if (answerKey[l++] === key) rem++;
+        }
+        return r - l;
+    }
+    return Math.max(check('T'), check('F'));
+}
--- problems/medium/2001-2500/2207.js
+++ problems/medium/2001-2500/2207.js
@@ -1,3 +1,11 @@
 // 2207 - Maximize Number of Subsequences in a String
 
-
+function maximumSubsequenceCount(text, pattern) {
+    let count = 0, fa = 0, fb = 0;
+    const a = pattern[0], b = pattern[1];
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] === a) fa++;
+        else if (text[i] === b) count += fa, fb++;
+    }
+    return a === b ? fa * (fa + 1) / 2 : count + Math.max(fa, fb);
+}
