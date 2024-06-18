7236d134d19f2a969cd5058373e12a591fd12e84
Kevin Huang <kevwhuang@gmail.com>
2024-06-18 05:58:53 -0500

    LC 2171/2576

--- problems/medium/2001-2500/2171.js
+++ problems/medium/2001-2500/2171.js
@@ -1,3 +1,12 @@
 // 2171 - Removing Minimum Number of Magic Beans
 
-
+function minimumRemoval(beans) {
+    beans.sort((a, b) => a - b);
+    let sum = 0, max = 0;
+    const len = beans.length;
+    for (let i = 0; i < len; i++) {
+        sum += beans[i];
+        max = Math.max(beans[i] * (len - i), max);
+    }
+    return sum - max;
+}
--- problems/medium/2501-3000/2576.js
+++ problems/medium/2501-3000/2576.js
@@ -1,3 +1,12 @@
 // 2576 - Find the Maximum Number of Marked Indices
 
-
+function maxNumOfMarkedIndices(nums) {
+    nums.sort((a, b) => a - b);
+    const len = nums.length;
+    let l = 0, r = Math.ceil(len / 2);
+    while (true) {
+        while (r < len && 2 * nums[l] > nums[r]) r++;
+        if (r === len) return l + l;
+        l++, r++;
+    }
+}
