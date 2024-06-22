07e100b08af171c8a5785876955d9f8ce8f21c8d
Kevin Huang <kevwhuang@gmail.com>
2024-06-21 19:19:36 -0500

    LC 1070/1158/1164/3048

--- problems/medium/1001-1500/1164.sql
+++ problems/medium/1001-1500/1164.sql
@@ -1,3 +1,28 @@
 -- 1164 - Product Price at a Given Date
 
-
+SELECT
+    DISTINCT product_id,
+    10 AS price
+FROM
+    Products
+WHERE
+    product_id NOT IN (
+        SELECT
+            product_id
+        FROM
+            Products
+        WHERE
+            change_date <= '2019-08-16'
+    )
+UNION
+SELECT
+    product_id,
+    FIRST_VALUE(new_price) OVER (
+        PARTITION BY product_id
+        ORDER BY
+            change_date DESC
+    ) AS price
+FROM
+    Products
+WHERE
+    change_date <= '2019-08-16';
--- problems/medium/3001-3500/3048.js
+++ problems/medium/3001-3500/3048.js
@@ -1,3 +1,23 @@
 // 3048 - Earliest Second to Mark Indices I
 
-
+function earliestSecondToMarkIndices(nums, changeIndices) {
+    changeIndices.forEach((_, i) => changeIndices[i]--);
+    let l = 0, r = changeIndices.length - 1;
+    while (l <= r) {
+        let marked = 0, ops = 0;
+        const arr = new Int16Array(nums.length).fill(-1);
+        const m = l + r >> 1;
+        for (let t = 0; t <= m; t++) {
+            arr[changeIndices[t]] = t;
+        }
+        for (let t = 0; t <= m; t++) {
+            if (t !== arr[changeIndices[t]]) { ops++; continue; }
+            ops -= nums[changeIndices[t]];
+            if (ops < 0) { marked = 0; break; }
+            marked++;
+        }
+        if (marked === nums.length) r = m - 1;
+        else l = m + 1;
+    }
+    return l === changeIndices.length ? -1 : l + 1;
+}
