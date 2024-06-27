3aea2466b65a3a7f19cdb72870a9978b883762ad
Kevin Huang <kevwhuang@gmail.com>
2024-06-26 20:15:46 -0500

    LC 880

--- problems/medium/501-1000/880.js
+++ problems/medium/501-1000/880.js
@@ -1,3 +1,16 @@
 // 880 - Decoded String at Index
 
-
+function decodeAtIndex(s, k) {
+    let size = 0, i = 0;
+    while (size < k) {
+        if (s[i] < 'a') size *= +s[i++];
+        else ++size, i++;
+    }
+    for (let j = i - 1; ~j; j--) {
+        if (s[j] < 'a') size /= +s[j];
+        k %= size;
+        if (s[j] < 'a') continue;
+        if (k === 0) return s[j];
+        size--;
+    }
+}
