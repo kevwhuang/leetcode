9e18f01b3d021808203ea856b9fdde158ed149a8
Kevin Huang <kevwhuang@gmail.com>
2024-07-05 00:04:43 -0500

    LC 1045/1193

--- problems/medium/1001-1500/1045.sql
+++ problems/medium/1001-1500/1045.sql
@@ -1,3 +1,15 @@
 -- 1045 - Customers Who Bought All Products
 
-
+SELECT
+    customer_id
+FROM
+    Customer
+GROUP BY
+    customer_id
+HAVING
+    COUNT(DISTINCT product_key) = (
+        SELECT
+            COUNT(*)
+        FROM
+            Product
+    );
