e99f8f0f00b4d3ae4960ca29eca94f4ab282473f
Kevin Huang <kevwhuang@gmail.com>
2024-07-04 20:07:01 -0500

    LC 1341/1907

--- problems/medium/1001-1500/1341.sql
+++ problems/medium/1001-1500/1341.sql
@@ -1,3 +1,34 @@
 -- 1341 - Movie Rating
 
-
+(
+    SELECT
+        name AS results
+    FROM
+        MovieRating
+        JOIN Users USING(user_id)
+    GROUP BY
+        results
+    ORDER BY
+        COUNT(*) DESC,
+        results
+    LIMIT
+        1
+)
+UNION
+ALL (
+    SELECT
+        title AS results
+    FROM
+        MovieRating
+        JOIN Movies USING(movie_id)
+    WHERE
+        YEAR(created_at) = 2020
+        AND MONTH(created_at) = 2
+    GROUP BY
+        results
+    ORDER BY
+        AVG(rating) DESC,
+        results
+    LIMIT
+        1
+);
