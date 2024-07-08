19691176e9330f8ddb28d70e6ffb7533f2f6ccc7
Kevin Huang <kevwhuang@gmail.com>
2024-07-08 02:49:19 -0500

    LC 180/184/1934

--- problems/medium/1-500/184.sql
+++ problems/medium/1-500/184.sql
@@ -1,3 +1,19 @@
 -- 184 - Department Highest Salary
 
-
+SELECT
+    D.name AS department,
+    E.name AS employee,
+    salary
+FROM
+    Employee E
+    JOIN Department D ON E.departmentId = D.id
+WHERE
+    (departmentId, salary) IN (
+        SELECT
+            departmentId,
+            MAX(salary)
+        FROM
+            Employee
+        GROUP BY
+            departmentId
+    );
--- problems/medium/1501-2000/1934.sql
+++ problems/medium/1501-2000/1934.sql
@@ -1,3 +1,17 @@
 -- 1934 - Confirmation Rate
 
-
+WITH CTE AS (
+    SELECT
+        user_id,
+        ROUND(SUM(IF(action = 'timeout', 0, 1)) / COUNT(*), 2) AS rate
+    FROM
+        Confirmations
+    GROUP BY
+        user_id
+)
+SELECT
+    user_id,
+    COALESCE(CTE.rate, 0) AS confirmation_rate
+FROM
+    Signups
+    LEFT JOIN CTE USING(user_id);
