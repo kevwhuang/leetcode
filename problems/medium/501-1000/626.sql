53a82b7fe3bc19115cdc3ed4c929eec0118bef6e
Kevin Huang <kevwhuang@gmail.com>
2024-07-09 05:03:21 -0500

    LC 626

--- problems/medium/501-1000/626.sql
+++ problems/medium/501-1000/626.sql
@@ -1,3 +1,14 @@
 -- 626 - Exchange Seats
 
-
+SELECT
+    id,
+    COALESCE(
+        IF(
+            MOD(id, 2) = 1,
+            LEAD(student) OVER(),
+            LAG(student) OVER()
+        ),
+        student
+    ) AS student
+FROM
+    Seat;
