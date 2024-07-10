88029095214efaec6d612acae7359ae436185115
Kevin Huang <kevwhuang@gmail.com>
2024-07-10 00:18:21 -0500

    LC 570/585

--- problems/medium/501-1000/570.sql
+++ problems/medium/501-1000/570.sql
@@ -1,3 +1,17 @@
 -- 570 - Managers With at Least 5 Direct Reports
 
-
+SELECT
+    name
+FROM
+    Employee
+WHERE
+    id IN (
+        SELECT
+            managerId
+        FROM
+            Employee
+        GROUP BY
+            managerId
+        HAVING
+            COUNT(*) >= 5
+    );
