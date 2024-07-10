81072bb1323863929d0ec00329b651d5656f3e99
Kevin Huang <kevwhuang@gmail.com>
2024-07-10 04:01:36 -0500

    LC 2616/2910

--- problems/medium/2501-3000/2616.js
+++ problems/medium/2501-3000/2616.js
@@ -1,3 +1,17 @@
 // 2616 - Minimize the Maximum Difference of Pairs
 
-
+function minimizeMax(nums, p) {
+    nums.sort((a, b) => a - b);
+    let min = 0, max = nums.at(-1) - nums[0];
+    while (min <= max) {
+        const m = min + max >> 1;
+        let pairs = 0, i = 1;
+        while (i < nums.length) {
+            if (nums[i] - nums[i - 1] > m) i++;
+            else pairs++, i += 2;
+        }
+        if (pairs >= p) max = m - 1;
+        else min = m + 1;
+    }
+    return min;
+}
--- problems/medium/2501-3000/2910.js
+++ problems/medium/2501-3000/2910.js
@@ -1,3 +1,24 @@
 // 2910 - Minimum Number of Groups to Create a Valid Assignment
 
-
+function minGroupsForValidAssignment(balls) {
+    const map = new Map();
+    for (let i = 0; i < balls.length; i++) {
+        map.set(balls[i], (map.get(balls[i]) ?? 0) + 1);
+    }
+    let size = Infinity;
+    for (const freq of map.values()) {
+        if (freq < size) size = freq;
+    }
+    size += 2;
+    L: while (--size >= 2) {
+        let boxes = 0;
+        for (const freq of map.values()) {
+            const count = freq / size >> 0;
+            const rem = freq % size;
+            if (rem === 0) boxes += count;
+            else if (count >= size - rem - 1) boxes += count + 1;
+            else continue L;
+        }
+        return boxes;
+    }
+}
