e566fb8d4d7e88aa7988651f98af35589575f019
Kevin Huang <kevwhuang@gmail.com>
2024-07-11 00:57:45 -0500

    LC 602/608

--- problems/medium/501-1000/602.sql
+++ problems/medium/501-1000/602.sql
@@ -1,3 +1,25 @@
 -- 602 - Friend Requests II: Who Has the Most Friends
 
-
+WITH CTE AS (
+    SELECT
+        requester_id AS id
+    FROM
+        RequestAccepted
+    UNION
+    ALL
+    SELECT
+        accepter_id AS id
+    FROM
+        RequestAccepted
+)
+SELECT
+    id,
+    COUNT(*) AS num
+FROM
+    CTE
+GROUP BY
+    id
+ORDER BY
+    num DESC
+LIMIT
+    1;
--- problems/medium/501-1000/608.sql
+++ problems/medium/501-1000/608.sql
@@ -1,3 +1,16 @@
 -- 608 - Tree Node
 
-
+SELECT
+    id,
+    CASE
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (
+            SELECT
+                DISTINCT p_id
+            FROM
+                Tree
+        ) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS type
+FROM
+    Tree;
