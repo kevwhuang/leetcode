8dceab7839d35d5be32d0e910907b93418dabf1e
Kevin Huang <kevwhuang@gmail.com>
2024-07-12 01:59:17 -0500

    LC 1204/1321

--- problems/medium/1001-1500/1204.sql
+++ problems/medium/1001-1500/1204.sql
@@ -1,3 +1,22 @@
 -- 1204 - Last Person to Fit in the Bus
 
-
+WITH CTE AS (
+    SELECT
+        person_name,
+        SUM(weight) OVER(
+            ORDER BY
+                turn
+        ) AS sum
+    FROM
+        Queue
+)
+SELECT
+    person_name
+FROM
+    CTE
+WHERE
+    sum <= 1000
+ORDER BY
+    sum DESC
+LIMIT
+    1;
--- problems/medium/1001-1500/1321.sql
+++ problems/medium/1001-1500/1321.sql
@@ -1,3 +1,25 @@
 -- 1321 - Restaurant Growth
 
-
+WITH CTE AS (
+    SELECT
+        visited_on,
+        SUM(amount) AS sum
+    FROM
+        Customer
+    GROUP BY
+        visited_on
+)
+SELECT
+    A.visited_on,
+    SUM(B.sum) AS amount,
+    ROUND(AVG(B.sum), 2) AS average_amount
+FROM
+    CTE A,
+    CTE B
+WHERE
+    DATEDIFF(A.visited_on, B.visited_on) BETWEEN 0
+    AND 6
+GROUP BY
+    visited_on
+HAVING
+    COUNT(B.visited_on) = 7;
