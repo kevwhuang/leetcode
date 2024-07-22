1d5aada7c6c500291f7300010f7d6bbaca31bfc8
Kevin Huang <kevwhuang@gmail.com>
2024-07-22 18:57:29 -0500

    LC 1733

--- problems/medium/1501-2000/1733.js
+++ problems/medium/1501-2000/1733.js
@@ -1,3 +1,26 @@
 // 1733 - Minimum Number of People to Teach
 
-
+function minimumTeachings(n, languages, friendships) {
+    for (let i = 0; i < languages.length; i++) {
+        languages[i] = new Set(languages[i]);
+    }
+    const teach = new Set();
+    L: for (let i = 0; i < friendships.length; i++) {
+        const u = --friendships[i][0], v = --friendships[i][1];
+        for (const lang of languages[u]) {
+            if (languages[v].has(lang)) continue L;
+        }
+        teach.add(u), teach.add(v);
+    }
+    const bucket = new Uint16Array(n);
+    for (const u of teach) {
+        for (const lang of languages[u]) {
+            bucket[lang - 1]++;
+        }
+    }
+    let max = 0;
+    for (let i = 0; i < n; i++) {
+        if (bucket[i] > max) max = bucket[i];
+    }
+    return teach.size - max;
+}
