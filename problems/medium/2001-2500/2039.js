447d5a19dd71c21ef83f5a2659369bbea6162b8e
Kevin Huang <kevwhuang@gmail.com>
2024-07-25 06:33:52 -0500

    LC 889/2039

--- problems/medium/2001-2500/2039.js
+++ problems/medium/2001-2500/2039.js
@@ -1,3 +1,33 @@
 // 2039 - The Time When the Network Becomes Idle
 
-
+function networkBecomesIdle(edges, patience) {
+    const adj = Array.from({ length: patience.length }, () => []);
+    for (let i = 0; i < edges.length; i++) {
+        adj[edges[i][0]].push(edges[i][1]);
+        adj[edges[i][1]].push(edges[i][0]);
+    }
+    const dists = new Uint32Array(adj.length);
+    dists[0] = 1;
+    let dist = 1, queue = [0];
+    while (queue.length) {
+        const nextQueue = [];
+        for (let i = 0; i < queue.length; i++) {
+            const neighbors = adj[queue[i]];
+            for (let j = 0; j < neighbors.length; j++) {
+                const n = neighbors[j];
+                if (dists[n]) continue;
+                dists[n] = dist;
+                nextQueue.push(n);
+            }
+        }
+        dist++;
+        queue = nextQueue;
+    }
+    let time = 0;
+    for (let n = 1; n < adj.length; n++) {
+        const first = 2 * dists[n];
+        const last = ((first - 1) / patience[n] >> 0) * patience[n];
+        time = Math.max(first + last, time);
+    }
+    return time + 1;
+}
--- problems/medium/501-1000/889.js
+++ problems/medium/501-1000/889.js
@@ -1,3 +1,13 @@
 // 889 - Construct Binary Tree From Preorder and Postorder Traversal
 
-
+function constructFromPrePost(preorder, postorder) {
+    function dfs() {
+        const node = new TreeNode(preorder[i++]);
+        if (node.val !== postorder[j]) node.left = dfs();
+        if (node.val !== postorder[j]) node.right = dfs();
+        j++;
+        return node;
+    }
+    let i = 0, j = 0;
+    return dfs();
+}
