a068c0ff4df08d27ea3d060ca125ee558692aa34
Kevin Huang <kevwhuang@gmail.com>
2024-07-26 03:25:21 -0500

    LC 955/1552

--- problems/medium/1501-2000/1552.js
+++ problems/medium/1501-2000/1552.js
@@ -1,3 +1,17 @@
 // 1552 - Magnetic Force Between Two Balls
 
-
+function maxDistance(position, m) {
+    const pos = new Uint32Array(position).sort();
+    let l = 1, r = Math.ceil(pos.at(-1) - pos[0] / (m - 1));
+    while (l <= r) {
+        const gap = l + r >> 1;
+        let balls = 0, i = 0;
+        while (i < pos.length && ++balls < m) {
+            const start = pos[i++];
+            while (i < pos.length && start + gap > pos[i]) i++;
+        }
+        if (balls === m) l = gap + 1;
+        else r = gap - 1;
+    }
+    return l - 1;
+}
--- problems/medium/501-1000/955.js
+++ problems/medium/501-1000/955.js
@@ -1,3 +1,19 @@
 // 955 - Delete Columns to Make Sorted II
 
-
+function minDeletionSize(strs) {
+    let deleted = 0;
+    const m = strs.length, n = strs[0].length;
+    const arr = new Uint8Array(m);
+    L: for (let i, j = 0; j < n; j++) {
+        i = 0;
+        while (++i < m) {
+            if (arr[i - 1]) continue;
+            if (strs[i - 1][j] <= strs[i][j]) continue;
+            deleted++;
+            continue L;
+        }
+        i = 0;
+        while (++i < m) arr[i - 1] |= strs[i - 1][j] < strs[i][j];
+    }
+    return deleted;
+}
