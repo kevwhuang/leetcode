0a55256b6ad7fd9bde9a9e6f5b5f8ad253de5c30
Kevin Huang <kevwhuang@gmail.com>
2024-07-29 05:53:09 -0500

    LC 1438/2333

--- problems/medium/1001-1500/1438.js
+++ problems/medium/1001-1500/1438.js
@@ -1,3 +1,17 @@
 // 1438 - Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
 
-
+function longestSubarray(nums, limit) {
+    let max = 0;
+    const q1 = [], q2 = [];
+    for (let l = 0, r = 0; r < nums.length; r++) {
+        while (q1.length && nums[q1.at(-1)] >= nums[r]) q1.pop();
+        while (q2.length && nums[q2.at(-1)] <= nums[r]) q2.pop();
+        if (q1[0] <= r - max - 1) q1.shift();
+        if (q2[0] <= r - max - 1) q2.shift();
+        q1.push(r);
+        q2.push(r);
+        if (nums[q2[0]] - nums[q1[0]] <= limit) max = r - l + 1;
+        else l = Math.min(q1[0], q2[0]) + 1;
+    }
+    return max;
+}
--- problems/medium/2001-2500/2333.js
+++ problems/medium/2001-2500/2333.js
@@ -1,3 +1,18 @@
 // 2333 - Minimum Sum of Squared Difference
 
-
+function minSumSquareDiff(nums1, nums2, k1, k2) {
+    const arr = new Array(nums1.length);
+    nums1.forEach((_, i) => arr[i] = Math.abs(nums1[i] - nums2[i]));
+    arr.sort((a, b) => b - a);
+    let k = k1 + k2, i = 0;
+    while (++i < arr.length) {
+        const ops = i * (arr[i - 1] - arr[i]);
+        if (k <= ops) break;
+        k -= ops;
+    }
+    const num = arr[i - 1] - (k / i >> 0);
+    let sum = (i - k % i) * Math.max(num, 0) ** 2;
+    sum += (k % i) * Math.max(num - 1, 0) ** 2;
+    while (i < arr.length) sum += arr[i++] ** 2;
+    return sum;
+}
