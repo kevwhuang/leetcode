09b56d5253ab2e404e2a1eb196bd846a7eab00aa
Kevin Huang <kevwhuang@gmail.com>
2024-08-03 06:40:32 -0500

    LC 544/1810

--- problems/medium/1501-2000/1810.mjs
+++ problems/medium/1501-2000/1810.mjs
@@ -1,3 +1,38 @@
 // 1810 - Minimum Path Cost in a Hidden Grid
 
-
+function findShortestPath(master) {
+    function dfs(node) {
+        for (let i = 0; i < 4; i++) {
+            const next = node + dirs[i][2];
+            if (M[next] !== 255) continue;
+            if (!master.canMove(dirs[i][0])) M[next] = 0;
+            if (!master.canMove(dirs[i][0])) continue;
+            M[next] = master.move(dirs[i][0]);
+            dfs(next);
+            if (master.isTarget()) end = next;
+            master.move(dirs[i][1]);
+        }
+    }
+    const n = 201, start = 101 * n;
+    const M = new Uint8Array(n * n).fill(255);
+    const dirs = [['U', 'D', -n], ['D', 'U', n], ['L', 'R', -1], ['R', 'L', 1]];
+    let end;
+    dfs(start);
+    if (!end) return -1;
+    const costs = { [start]: 0 };
+    const pq = new PriorityQueue({ compare: (a, b) => a[1] - b[1] });
+    pq.enqueue([start, 0]);
+    while (true) {
+        const [u, cost] = pq.dequeue();
+        if (u === end) return costs[u];
+        for (let i = 0; i < 4; i++) {
+            const v = u + dirs[i][2];
+            if (v < 0 || v > 40400) continue;
+            if (M[v] === 0) continue;
+            const nextCost = M[v] + cost;
+            if (nextCost >= costs[v]) continue;
+            costs[v] = nextCost;
+            pq.enqueue([v, nextCost]);
+        }
+    }
+}
--- problems/medium/501-1000/544.mjs
+++ problems/medium/501-1000/544.mjs
@@ -1,3 +1,13 @@
 // 544 - Output Contest Matches
 
-
+function findContestMatch(n) {
+    let queue = new Array(n / 2), i = 0;
+    while (i < n) queue[i] = `(${++i},${n--})`;
+    while (queue.length > 1) {
+        const nextQueue = new Array(queue.length / 2);
+        let l = 0, r = queue.length - 1;
+        while (l < r) nextQueue[l] = `(${queue[l++]},${queue[r--]})`;
+        queue = nextQueue;
+    }
+    return queue[0];
+}
