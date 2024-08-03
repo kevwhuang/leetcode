f6285f312a8625ab17e23842a8adf98990d294cc
Kevin Huang <kevwhuang@gmail.com>
2024-08-03 02:22:48 -0500

    LC 1059/1181

--- problems/medium/1001-1500/1059.mjs
+++ problems/medium/1001-1500/1059.mjs
@@ -1,3 +1,24 @@
 // 1059 - All Paths From Source Lead to Destination
 
-
+function leadsToDestination(n, edges, source, destination) {
+    function dfs(node) {
+        if (seen.has(node)) return true;
+        if (!adj[node].length) return node === destination;
+        if (state[node]) return false;
+        state[node] = 1;
+        for (const v of adj[node]) {
+            if (!dfs(v)) return false;
+        }
+        state[node] = 0;
+        seen.add(node);
+        return true;
+    }
+    const adj = Array.from({ length: n }, () => []);
+    for (let i = 0; i < edges.length; i++) {
+        if (edges[i][0] === destination) return false;
+        adj[edges[i][0]].push(edges[i][1]);
+    }
+    const seen = new Set();
+    const state = new Uint8Array(n);
+    return dfs(source);
+}
--- problems/medium/1001-1500/1181.mjs
+++ problems/medium/1001-1500/1181.mjs
@@ -1,3 +1,19 @@
 // 1181 - Before and After Puzzle
 
-
+function beforeAndAfterPuzzles(phrases) {
+    for (let i = 0; i < phrases.length; i++) {
+        phrases[i] = phrases[i].split(' ');
+    }
+    const set = new Set();
+    for (let i = 0; i < phrases.length; i++) {
+        for (let j = 0; j < phrases.length; j++) {
+            if (i === j) continue;
+            if (phrases[i].at(-1) !== phrases[j][0]) continue;
+            let merged = phrases[i].join(' ');
+            if (phrases[j].length > 1) merged += ' ';
+            merged += phrases[j].slice(1).join(' ');
+            set.add(merged);
+        }
+    }
+    return [...set].sort();
+}
