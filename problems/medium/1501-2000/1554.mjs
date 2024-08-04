a994df1e582445333f4d4fe9f5773b63da88e139
Kevin Huang <kevwhuang@gmail.com>
2024-08-04 00:22:36 -0500

    LC 1554/2505

--- problems/medium/1501-2000/1554.mjs
+++ problems/medium/1501-2000/1554.mjs
@@ -1,3 +1,28 @@
 // 1554 - Strings Differ by One Character
 
-
+function differByOne(dict) {
+    const m = dict.length, n = dict[0].length;
+    if (m < n * n) {
+        for (let i = 0; i < m; i++) {
+            L: for (let j = i + 1; j < m; j++) {
+                let flag;
+                for (let k = 0; k < n; k++) {
+                    if (dict[i][k] === dict[j][k]) continue;
+                    if (flag) continue L;
+                    flag = true;
+                }
+                return true;
+            }
+        }
+    } else {
+        const set = new Set();
+        for (let i = 0; i < m; i++) {
+            for (let j = 0; j < n; j++) {
+                const key = `${dict[i].slice(0, j)}-${dict[i].slice(j + 1)}`;
+                if (set.has(key)) return true;
+                set.add(key);
+            }
+        }
+    }
+    return false;
+}
--- problems/medium/2501-3000/2505.mjs
+++ problems/medium/2501-3000/2505.mjs
@@ -1,3 +1,10 @@
 // 2505 - Bitwise OR of All Subsequence Sums
 
-
+function subsequenceSumOr(nums) {
+    let or = BigInt(0), acc = BigInt(0);
+    for (let i = 0; i < nums.length; i++) {
+        const num = BigInt(nums[i]);
+        or |= num | (acc += num);
+    }
+    return Number(or);
+}
