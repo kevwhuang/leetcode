9f05c8299779c142537320f38799abed9724c346
Kevin Huang <kevwhuang@gmail.com>
2024-08-04 03:48:06 -0500

    LC 291/356

--- problems/medium/1-500/291.mjs
+++ problems/medium/1-500/291.mjs
@@ -1,3 +1,28 @@
 // 291 - Word Pattern II
 
-
+function wordPatternMatch(pattern, s) {
+    function backtrack(i, j) {
+        if (i === pattern.length) return j === s.length;
+        if (map.has(pattern[i])) {
+            const str = map.get(pattern[i]);
+            if (s.startsWith(str, j)) {
+                return backtrack(i + 1, j + str.length);
+            }
+        } else {
+            let str = '';
+            const bound = s.length - pattern.length + i + 1;
+            for (let k = j; k < bound; k++) {
+                str += s[k];
+                if (set.has(str)) continue;
+                set.add(str);
+                map.set(pattern[i], str);
+                if (backtrack(i + 1, k + 1)) return true;
+                map.delete(pattern[i]);
+                set.delete(str);
+            }
+        }
+        return false;
+    }
+    const map = new Map(), set = new Set();
+    return backtrack(0, 0);
+}
--- problems/medium/1-500/356.mjs
+++ problems/medium/1-500/356.mjs
@@ -1,3 +1,23 @@
 // 356 - Line Reflection
 
-
+function isReflected(points) {
+    const obj = {};
+    for (let i = 0; i < points.length; i++) {
+        obj[points[i][1]] ??= new Set();
+        obj[points[i][1]].add(points[i][0]);
+    }
+    let line;
+    for (const y in obj) {
+        const min = Math.min(...obj[y]);
+        const max = Math.max(...obj[y]);
+        line = (max + min) / 2;
+        break;
+    }
+    for (const y in obj) {
+        const set = obj[y];
+        for (const x of set) {
+            if (!set.has(2 * line - x)) return false;
+        }
+    }
+    return true;
+}
