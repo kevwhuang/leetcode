39069b7ca142d7381ab34cb672b2d072bcef8525
Kevin Huang <kevwhuang@gmail.com>
2024-08-04 08:27:54 -0500

    LC 2093/2737 (Dijkstra)

--- problems/medium/2001-2500/2093.mjs
+++ problems/medium/2001-2500/2093.mjs
@@ -1,3 +1,34 @@
 // 2093 - Minimum Cost to Reach City With Discounts
 
-
+function minimumCost(n, highways, discounts) {
+    const adj = Array.from({ length: n }, () => []);
+    for (let i = 0; i < highways.length; i++) {
+        adj[highways[i][0]].push([highways[i][1], highways[i][2]]);
+        adj[highways[i][1]].push([highways[i][0], highways[i][2]]);
+    }
+    if (adj[n - 1].length === 0) return -1;
+    let min = Infinity, queue = [[0, 0, []]], costs = new Array(n);
+    while (queue.length) {
+        const nextQueue = [];
+        for (let i = 0; i < queue.length; i++) {
+            if (queue[i][0] === n - 1) {
+                const path = new Uint32Array(queue[i][2]).sort();
+                let sum = 0, rem = discounts, j = path.length - 1;
+                while (rem--) sum += path[j--] / 2 >> 0;
+                while (j >= 0) sum += path[j--];
+                if (sum < min) min = sum;
+                continue;
+            }
+            const next = adj[queue[i][0]];
+            for (let j = 0; j < next.length; j++) {
+                const nextCost = next[j][1] + queue[i][1];
+                if (nextCost >= costs[next[j][0]]) continue;
+                costs[next[j][0]] = nextCost;
+                const nextPath = queue[i][2].concat(next[j][1]);
+                nextQueue.push([next[j][0], nextCost, nextPath]);
+            }
+        }
+        queue = nextQueue;
+    }
+    return min === Infinity ? -1 : min;
+}
--- problems/medium/2501-3000/2737.mjs
+++ problems/medium/2501-3000/2737.mjs
@@ -1,3 +1,26 @@
 // 2737 - Find the Closest Marked Node
 
-
+function minimumDistance(n, edges, s, marked) {
+    marked = new Set(marked);
+    const adj = Array.from({ length: n }, () => new Map());
+    for (let i = 0; i < edges.length; i++) {
+        const u = edges[i][0], v = edges[i][1];
+        const w = Math.min(edges[i][2], adj[u].get(v) ?? 1e6);
+        adj[u].set(v, w);
+    }
+    const costs = new Array(n);
+    const pq = new PriorityQueue({ compare: (a, b) => a[1] - b[1] });
+    pq.enqueue([s, 0]);
+    while (pq.size()) {
+        const [u, cost] = pq.dequeue();
+        if (marked.has(u)) return cost;
+        const neighbors = adj[u];
+        for (const v of neighbors.keys()) {
+            const nextCost = neighbors.get(v) + cost;
+            if (nextCost >= costs[v]) continue;
+            costs[v] = nextCost;
+            pq.enqueue([v, nextCost]);
+        }
+    }
+    return -1;
+}
