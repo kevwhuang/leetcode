b4d0397bc6805642cf82d8e172353b9178f405cc
Kevin Huang <kevwhuang@gmail.com>
2024-08-20 03:59:10 -0500

    LC 1533

--- problems/medium/1501-2000/1533.mjs
+++ problems/medium/1501-2000/1533.mjs
@@ -1,3 +1,13 @@
 // 1533 - Find the Index of the Large Integer
 
-
+function getIndex(reader) {
+    let l = 0, r = reader.length() - 1;
+    while (l < r) {
+        const d = (r - l) / 3 >> 0;
+        const sign = reader.compareSub(l, l + d, r - d, r);
+        if (sign > 0) r = l + d;
+        else if (sign < 0) l = r - d;
+        else l += d + 1, r -= d + 1;
+    }
+    return l;
+}
