18163c5fb53f8f92fb159234edda58f1987397de
Kevin Huang <kevwhuang@gmail.com>
2024-08-29 00:52:57 -0500

    LC 469/2098

--- problems/medium/1-500/469.mjs
+++ problems/medium/1-500/469.mjs
@@ -1,3 +1,16 @@
 // 469 - Convex Polygon
 
-
+function isConvex(points) {
+    points.push(points[0], points[1]);
+    let pos, neg;
+    for (let i = 2; i < points.length; i++) {
+        const x1 = points[i - 2][0], y1 = points[i - 2][1];
+        const x2 = points[i - 1][0], y2 = points[i - 1][1];
+        const x3 = points[i][0], y3 = points[i][1];
+        const cross = (x1 - x2) * (y3 - y2) - (x3 - x2) * (y1 - y2);
+        if (cross > 0) pos = true;
+        else if (cross < 0) neg = true;
+        if (pos && neg) return false;
+    }
+    return true;
+}
--- problems/medium/2001-2500/2098.mjs
+++ problems/medium/2001-2500/2098.mjs
@@ -1,3 +1,25 @@
 // 2098 - Subsequence of Size K With the Largest Even Sum
 
-
+function largestEvenSum(nums, k) {
+    nums = new Uint32Array(nums).sort();
+    let sum = 0, odd, even;
+    const bound = nums.length - 1 - k;
+    for (let i = nums.length - 1; i > bound; i--) {
+        const num = nums[i];
+        sum += num;
+        if (num % 2) odd = num;
+        else even = num;
+    }
+    if (sum % 2 === 0) return sum;
+    let newOdd, newEven;
+    for (let i = nums.length - 1 - k; ~i; i--) {
+        const num = nums[i];
+        if (num % 2 && newOdd === undefined) newOdd = num;
+        else if (num % 2 === 0 && newEven === undefined) newEven = num;
+        if (newOdd >= 0 && newEven >= 0) break;
+    }
+    const d1 = newOdd - even || -Infinity;
+    const d2 = newEven - odd || -Infinity;
+    const max = Math.max(d1, d2);
+    return max === -Infinity ? -1 : sum + max;
+}
