a897946cb6364e7e8217e15afa4879e09f223fd5
Kevin Huang <kevwhuang@gmail.com>
2024-08-30 13:59:00 -0500

    LC 1740/2764

--- problems/medium/1501-2000/1740.mjs
+++ problems/medium/1501-2000/1740.mjs
@@ -1,3 +1,25 @@
 // 1740 - Find Distance in a Binary Tree
 
-
+function findDistance(root, p, q) {
+    function dfs1(node, parent) {
+        if (!node) return;
+        if (node.val === p) start = node;
+        map.set(node, parent);
+        dfs1(node.left, node);
+        dfs1(node.right, node);
+    }
+    function dfs2(node, dist) {
+        if (res || !node || node.val === -1) return;
+        if (node.val === q) return res = dist;
+        node.val = -1;
+        dfs2(map.get(node), dist + 1);
+        dfs2(node.left, dist + 1);
+        dfs2(node.right, dist + 1);
+    }
+    let start;
+    const map = new Map();
+    dfs1(root, null);
+    let res;
+    dfs2(start, 0);
+    return res;
+}
--- problems/medium/2501-3000/2764.mjs
+++ problems/medium/2501-3000/2764.mjs
@@ -1,3 +1,12 @@
 // 2764 - Is Array a Preorder of Some ‌Binary Tree
 
-
+function isPreorder(nodes) {
+    const stack = [nodes[0][0]];
+    for (let i = 1; i < nodes.length; i++) {
+        const parent = nodes[i][1];
+        while (stack.length && stack.at(-1) !== parent) stack.pop();
+        if (stack.length === 0) return false;
+        stack.push(nodes[i][0]);
+    }
+    return true;
+}
