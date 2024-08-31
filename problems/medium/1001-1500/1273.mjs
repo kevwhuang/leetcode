eba66e39569aedcab860ae5761d87f19a217b786
Kevin Huang <kevwhuang@gmail.com>
2024-08-31 00:54:48 -0500

    LC 1273/2445

--- problems/medium/1001-1500/1273.mjs
+++ problems/medium/1001-1500/1273.mjs
@@ -1,3 +1,31 @@
 // 1273 - Delete Tree Nodes
 
-
+function deleteTreeNodes(nodes, parent, value) {
+    function dfs1(node) {
+        let sum = value[node];
+        const children = adj[node];
+        for (let i = 0; i < children?.length; i++) {
+            sum += dfs1(children[i]);
+        }
+        if (sum === 0) set.add(node);
+        return sum;
+    }
+    function dfs2(node) {
+        if (set.has(node)) return;
+        count++;
+        const children = adj[node];
+        for (let i = 0; i < children?.length; i++) {
+            dfs2(children[i]);
+        }
+    }
+    const adj = { 0: [] };
+    for (let n = 1; n < parent.length; n++) {
+        adj[parent[n]] ??= [];
+        adj[parent[n]].push(n);
+    }
+    const set = new Set();
+    dfs1(0);
+    let count = 0;
+    dfs2(0);
+    return count;
+}
--- problems/medium/2001-2500/2445.mjs
+++ problems/medium/2001-2500/2445.mjs
@@ -1,3 +1,15 @@
 // 2445 - Number of Nodes With Value One
 
-
+function numberOfNodes(n, queries) {
+    const tree = new Uint8Array(n + 1);
+    for (let i = 0; i < queries.length; i++) {
+        tree[queries[i]] = 1 - tree[queries[i]];
+    }
+    let nodes = 0;
+    for (let node = 1; node <= n; node++) {
+        const parent = tree[node >> 1];
+        tree[node] = tree[node] ? 1 - parent : parent;
+        nodes += tree[node];
+    }
+    return nodes;
+}
