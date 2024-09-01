255df6b7184627e207b8c01c10abbb99f38f7799
Kevin Huang <kevwhuang@gmail.com>
2024-09-01 06:18:20 -0500

    LC 2021/2237

--- problems/medium/2001-2500/2021.mjs
+++ problems/medium/2001-2500/2021.mjs
@@ -1,3 +1,23 @@
 // 2021 - Brightest Position on Street
 
-
+function brightestPosition(lights) {
+    const n = lights.length;
+    const start = new Int32Array(n);
+    const end = new Int32Array(n);
+    for (let i = 0; i < n; i++) {
+        start[i] = lights[i][0] - lights[i][1];
+        end[i] = lights[i][0] + lights[i][1] + 1;
+    }
+    start.sort();
+    end.sort();
+    let res, cur = 0, max = 0, i = 0, j = 0;
+    while (i < n) {
+        while (start[i] < end[j]) {
+            if (++cur > max) res = start[i], max = cur;
+            i++;
+        }
+        while (start[i] === end[j]) i++, j++;
+        while (start[i] > end[j]) cur--, j++;
+    }
+    return res;
+}
--- problems/medium/2001-2500/2237.mjs
+++ problems/medium/2001-2500/2237.mjs
@@ -1,3 +1,16 @@
 // 2237 - Count Positions on Street With Required Brightness
 
-
+function meetRequirement(n, lights, requirement) {
+    const street = new Int32Array(n);
+    for (let i = 0; i < lights.length; i++) {
+        const pos = lights[i][0], d = lights[i][1];
+        street[Math.max(0, pos - d)]++;
+        street[Math.min(n, pos + d + 1)]--;
+    }
+    let count = 0, acc = 0;
+    for (let i = 0; i < n; i++) {
+        acc += street[i];
+        if (acc >= requirement[i]) count++;
+    }
+    return count;
+}
