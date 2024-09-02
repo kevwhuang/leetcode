28cb3ba0975a6c0eb6fce3978c5f44153fcc4f3d
Kevin Huang <kevwhuang@gmail.com>
2024-09-01 21:06:13 -0500

    LC 1364/1715

--- problems/medium/1001-1500/1364.p.sql
+++ problems/medium/1001-1500/1364.p.sql
@@ -1,3 +1,32 @@
 -- 1364 - Number of Trusted Contacts of a Customer
 
-
+SELECT
+    invoice_id,
+    customer_name,
+    price,
+    contacts_cnt,
+    trusted_contacts_cnt
+FROM
+    Invoices
+    LEFT JOIN (
+        SELECT
+            customer_id,
+            customer_name,
+            COUNT(contact_name) AS contacts_cnt,
+            COUNT(T1.email) AS trusted_contacts_cnt
+        FROM
+            Customers
+            LEFT JOIN (
+                SELECT
+                    user_id,
+                    contact_name,
+                    email
+                FROM
+                    Contacts
+                    LEFT JOIN Customers ON contact_email = email
+            ) T1 ON customer_id = user_id
+        GROUP BY
+            customer_id
+    ) T2 ON user_id = customer_id
+ORDER BY
+    invoice_id;
