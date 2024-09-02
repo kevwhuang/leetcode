d4a1f309dae8dcb1958ee5cb32d78c71afde3d73
Kevin Huang <kevwhuang@gmail.com>
2024-09-02 03:55:41 -0500

    LC 1112/1398/2137

--- problems/medium/1001-1500/1398.p.sql
+++ problems/medium/1001-1500/1398.p.sql
@@ -1,3 +1,28 @@
 -- 1398 - Customers Who Bought Products A and B but Not C
 
-
+SELECT
+    *
+FROM
+    Customers
+WHERE
+    (customer_id, 'A') IN (
+        SELECT
+            customer_id,
+            product_name
+        FROM
+            Orders
+    )
+    AND (customer_id, 'B') IN (
+        SELECT
+            customer_id,
+            product_name
+        FROM
+            Orders
+    )
+    AND (customer_id, 'C') NOT IN (
+        SELECT
+            customer_id,
+            product_name
+        FROM
+            Orders
+    );
--- problems/medium/2001-2500/2137.mjs
+++ problems/medium/2001-2500/2137.mjs
@@ -1,3 +1,18 @@
 // 2137 - Pour Water Between Buckets to Make Water Levels Equal
 
-
+function equalizeWater(buckets, loss) {
+    let l = 0, r = Math.max(...buckets);
+    const k = 1 - loss / 100;
+    while (r - l > .00001) {
+        const m = (l + r) / 2;
+        let add = 0, rem = 0;
+        for (let i = 0; i < buckets.length; i++) {
+            const water = buckets[i];
+            if (water <= m) add += m - water;
+            else rem += water - m;
+        }
+        if (add <= k * rem) l = m;
+        else r = m;
+    }
+    return l;
+}
