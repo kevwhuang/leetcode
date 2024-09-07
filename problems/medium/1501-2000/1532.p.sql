fe2b5d6e5534e91491e10975cca503d92096d6d2
Kevin Huang <kevwhuang@gmail.com>
2024-09-07 17:37:03 -0500

    LC 1532/2020

--- problems/medium/1501-2000/1532.p.sql
+++ problems/medium/1501-2000/1532.p.sql
@@ -1,3 +1,30 @@
 -- 1532 - The Most Recent Three Orders
 
-
+WITH CTE AS (
+    SELECT
+        name AS customer_name,
+        customer_id,
+        order_id,
+        order_date,
+        DENSE_RANK() OVER (
+            PARTITION BY customer_id
+            ORDER BY
+                order_date DESC
+        ) AS n
+    FROM
+        Orders
+        JOIN Customers USING(customer_id)
+    ORDER BY
+        customer_name,
+        customer_id,
+        order_date DESC
+)
+SELECT
+    customer_name,
+    customer_id,
+    order_id,
+    order_date
+FROM
+    CTE
+WHERE
+    n <= 3;
--- problems/medium/2001-2500/2020.p.sql
+++ problems/medium/2001-2500/2020.p.sql
@@ -1,3 +1,17 @@
 -- 2020 - Number of Accounts That Did Not Stream
 
-
+SELECT
+    COUNT(account_id) AS accounts_count
+FROM
+    Subscriptions
+WHERE
+    account_id NOT IN (
+        SELECT
+            account_id
+        FROM
+            Streams
+        WHERE
+            YEAR(stream_date) = 2021
+    )
+    AND 2021 BETWEEN YEAR(start_date)
+    AND YEAR(end_date);
