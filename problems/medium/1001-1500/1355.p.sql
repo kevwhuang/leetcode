7bc0d9664eb022c1b451d5e1eb0a43b11584227d
Kevin Huang <kevwhuang@gmail.com>
2024-09-07 02:07:51 -0500

    LC 646/1355

--- problems/medium/1001-1500/1355.p.sql
+++ problems/medium/1001-1500/1355.p.sql
@@ -1,3 +1,29 @@
 -- 1355 - Activity Participants
 
-
+SELECT
+    activity
+FROM
+    Friends
+GROUP BY
+    activity
+HAVING
+    COUNT(*) != (
+        SELECT
+            MAX(COUNT(*)) OVER ()
+        FROM
+            Friends
+        GROUP BY
+            activity
+        LIMIT
+            1
+    )
+    AND COUNT(*) != (
+        SELECT
+            MIN(COUNT(*)) OVER ()
+        FROM
+            Friends
+        GROUP BY
+            activity
+        LIMIT
+            1
+    );
--- problems/medium/501-1000/646.js
+++ problems/medium/501-1000/646.js
@@ -1,3 +1,11 @@
 // 646 - Maximum Length of Pair Chain
 
-
+function findLongestChain(pairs) {
+    pairs.sort((a, b) => a[1] - b[1]);
+    let max = 0, end = -Infinity;
+    for (let i = 0; i < pairs.length; i++) {
+        if (pairs[i][0] <= end) continue;
+        max++, end = pairs[i][1];
+    }
+    return max;
+}
