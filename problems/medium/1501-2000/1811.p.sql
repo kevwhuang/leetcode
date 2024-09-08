be97910a7bb2a1e64568035cb1321b39f9ef7b28
Kevin Huang <kevwhuang@gmail.com>
2024-09-08 06:12:56 -0500

    LC 1811/1867

--- problems/medium/1501-2000/1811.p.sql
+++ problems/medium/1501-2000/1811.p.sql
@@ -1,3 +1,59 @@
 -- 1811 - Find Interview Candidates
 
-
+WITH CTE1 AS (
+    SELECT
+        contest_id,
+        gold_medal AS user_id
+    FROM
+        Contests
+    UNION
+    SELECT
+        contest_id,
+        silver_medal AS user_id
+    FROM
+        Contests
+    UNION
+    SELECT
+        contest_id,
+        bronze_medal AS user_id
+    FROM
+        Contests
+),
+CTE2 AS (
+    SELECT
+        contest_id,
+        user_id,
+        ROW_NUMBER() OVER (
+            PARTITION BY user_id
+            ORDER BY
+                contest_id
+        ) AS n
+    FROM
+        CTE1
+),
+CTE3 AS (
+    SELECT
+        user_id
+    FROM
+        CTE2
+    GROUP BY
+        user_id,
+        contest_id - n
+    HAVING
+        COUNT(*) >= 3
+    UNION
+    SELECT
+        gold_medal AS user_id
+    FROM
+        Contests
+    GROUP BY
+        user_id
+    HAVING
+        COUNT(*) >= 3
+)
+SELECT
+    name,
+    mail
+FROM
+    CTE3
+    JOIN Users USING(user_id);
--- problems/medium/1501-2000/1867.p.sql
+++ problems/medium/1501-2000/1867.p.sql
@@ -1,3 +1,21 @@
 -- 1867 - Orders With Maximum Quantity Above Average
 
-
+SELECT
+    order_id
+FROM
+    OrdersDetails
+GROUP BY
+    order_id
+HAVING
+    MAX(quantity) > (
+        SELECT
+            SUM(quantity) / COUNT(*)
+        FROM
+            OrdersDetails
+        GROUP BY
+            order_id
+        ORDER BY
+            1 DESC
+        LIMIT
+            1
+    );
