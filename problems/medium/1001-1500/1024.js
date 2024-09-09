1e0e3fdc7d00bdeec5105bfbf6523db91a753d45
Kevin Huang <kevwhuang@gmail.com>
2024-09-09 17:06:12 -0500

    LC 1024/1162

--- problems/medium/1001-1500/1024.js
+++ problems/medium/1001-1500/1024.js
@@ -1,3 +1,16 @@
 // 1024 - Video Stitching
 
-
+function videoStitching(clips, time) {
+    let count = 0, cur = 0;
+    while (cur < time) {
+        let next = -1;
+        for (let i = 0; i < clips.length; i++) {
+            if (cur < clips[i][0] || clips[i][1] < cur) continue;
+            next = Math.max(clips[i][1], next);
+            clips[i][0] = 1000;
+        }
+        if (next === -1) return -1;
+        count++, cur = next;
+    }
+    return count;
+}
