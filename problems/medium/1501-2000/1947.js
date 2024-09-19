2d61a147732ed9c3c15151ddc1c8de78c1c910bf
Kevin Huang <kevwhuang@gmail.com>
2024-09-19 03:31:22 -0500

    LC 1947/2305

--- problems/medium/1501-2000/1947.js
+++ problems/medium/1501-2000/1947.js
@@ -1,3 +1,26 @@
 // 1947 - Maximum Compatibility Score Sum
 
-
+function maxCompatibilitySum(students, mentors) {
+    function backtrack(i, acc) {
+        if (i === n) return max = Math.max(acc, max);
+        for (let j = 0; j < n; j++) {
+            if (seen[j]) continue;
+            seen[j] = true;
+            backtrack(i + 1, acc + mat[i][j]);
+            seen[j] = false;
+        }
+    }
+    const n = students.length, m = students[0].length;
+    const mat = Array.from({ length: n }, () => new Uint8Array(n));
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            for (let k = 0; k < m; k++) {
+                if (students[i][k] === mentors[j][k]) mat[i][j]++;
+            }
+        }
+    }
+    let max = 0;
+    const seen = new Array(n);
+    backtrack(0, 0);
+    return max;
+}
--- problems/medium/2001-2500/2305.js
+++ problems/medium/2001-2500/2305.js
@@ -1,3 +1,17 @@
 // 2305 - Fair Distribution of Cookies
 
-
+function distributeCookies(cookies, k) {
+    function backtrack(i) {
+        if (i === cookies.length) return min = Math.min(Math.max(...arr), min);
+        for (let j = 0; j < k; j++) {
+            arr[j] += cookies[i];
+            backtrack(i + 1);
+            arr[j] -= cookies[i];
+            if (arr[j] === 0 || arr[j] >= min) return;
+        }
+    }
+    let min = Infinity;
+    const arr = new Array(k).fill(0);
+    backtrack(0);
+    return min;
+}
