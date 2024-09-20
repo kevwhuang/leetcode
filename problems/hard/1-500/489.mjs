7515303cc41ca1fe5fedfe19ca399ba11503a656
Kevin Huang <kevwhuang@gmail.com>
2024-09-20 16:32:14 -0500

    LC 489/759

--- problems/hard/1-500/489.mjs
+++ problems/hard/1-500/489.mjs
@@ -1,3 +1,25 @@
 // 489 - Robot Room Cleaner
 
-
+function cleanRoom(robot) {
+    function backtrack(r, c, dir) {
+        if (seen.has(r) && seen.get(r).has(c)) return;
+        if (!seen.has(r)) seen.set(r, new Set());
+        seen.get(r).add(c);
+        robot.clean();
+        for (let n = 1; n <= 4; n++) {
+            if (robot.move()) {
+                backtrack(r + d[dir][0], c + d[dir][1], dir);
+                robot.turnLeft();
+                robot.turnLeft();
+                robot.move();
+                robot.turnLeft();
+                robot.turnLeft();
+            }
+            robot.turnLeft();
+            dir = (dir + 1) % 4;
+        }
+    }
+    const seen = new Map();
+    const d = [[-1, 0], [0, -1], [1, 0], [0, 1]];
+    backtrack(0, 0, 0);
+}
--- problems/hard/501-1000/759.mjs
+++ problems/hard/501-1000/759.mjs
@@ -1,3 +1,13 @@
 // 759 - Employee Free Time
 
-
+function employeeFreeTime(schedule) {
+    const A = [];
+    schedule.forEach(e => e.forEach(e => A.push(e)));
+    A.sort((a, b) => a.start - b.start);
+    const res = [];
+    for (let i = 1, max = A[0].end; i < A.length; i++) {
+        if (max < A[i].start) res.push(new Interval(max, A[i].start));
+        max = Math.max(A[i].end, max);
+    }
+    return res;
+}
