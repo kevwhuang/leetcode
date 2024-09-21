a8560772a5d9e0523a6c9bc59ad5836ce0ded0ab
Kevin Huang <kevwhuang@gmail.com>
2024-09-21 06:17:00 -0500

    LC 3040/3089

--- problems/medium/3001-3500/3040.js
+++ problems/medium/3001-3500/3040.js
@@ -1,3 +1,18 @@
 // 3040 - Maximum Number of Operations With the Same Score II
 
-
+function maxOperations(nums) {
+    function dfs(l, r, tgt) {
+        if (l >= r) return 0;
+        if (memo[l][r]) return memo[l][r];
+        const a = nums[l] + nums[l + 1];
+        const pathA = !tgt || tgt === a ? 1 + dfs(l + 2, r, a) : 0;
+        const b = nums[l] + nums[r];
+        const pathB = !tgt || tgt === b ? 1 + dfs(l + 1, r - 1, b) : 0;
+        const c = nums[r - 1] + nums[r];
+        const pathC = !tgt || tgt === c ? 1 + dfs(l, r - 2, c) : 0;
+        return memo[l][r] = Math.max(pathA, pathB, pathC);
+    }
+    const n = nums.length;
+    const memo = Array.from({ length: n }, () => new Uint8Array(n));
+    return dfs(0, n - 1);
+}
--- problems/medium/3001-3500/3089.p.sql
+++ problems/medium/3001-3500/3089.p.sql
@@ -1,3 +1,39 @@
 -- 3089 - Find Bursty Behavior
 
-
+WITH CTE1 AS (
+    SELECT
+        user_id,
+        COUNT(post_id) OVER (
+            PARTITION BY user_id
+            ORDER BY
+                post_date RANGE BETWEEN INTERVAL 6 DAY PRECEDING
+                AND CURRENT ROW
+        ) AS num
+    FROM
+        Posts
+),
+CTE2 AS (
+    SELECT
+        user_id,
+        COUNT(post_id) / 4 AS avg_weekly_posts
+    FROM
+        Posts
+    WHERE
+        MONTH(post_date) = 2
+        AND DAY(post_date) <= 28
+    GROUP BY
+        user_id
+)
+SELECT
+    user_id,
+    MAX(num) AS max_7day_posts,
+    avg_weekly_posts
+FROM
+    CTE1
+    JOIN CTE2 USING(user_id)
+GROUP BY
+    user_id
+HAVING
+    max_7day_posts >= 2 * avg_weekly_posts
+ORDER BY
+    user_id;
