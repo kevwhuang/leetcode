4b4cef859e8e143ee49424d2d0c4d4207496e418
Kevin Huang <kevwhuang@gmail.com>
2024-09-22 17:05:40 -0500

    LC 815/3118

--- problems/hard/501-1000/815.js
+++ problems/hard/501-1000/815.js
@@ -1,3 +1,39 @@
 // 815 - Bus Routes
 
-
+function numBusesToDestination(routes, source, target) {
+    if (source === target) return 0;
+    const map = new Map();
+    for (let i = 0; i < routes.length; i++) {
+        const route = routes[i];
+        for (let j = 0; j < route.length; j++) {
+            const cur = route[j];
+            if (map.has(cur)) map.get(cur).push(i);
+            else map.set(cur, [i]);
+        }
+    }
+    if (!map.has(source) || !map.has(target)) return -1;
+    let res = 1, queue = [source];
+    const seen = new Uint8Array(routes.length);
+    while (queue.length) {
+        const nextQueue = [];
+        for (let i = 0; i < queue.length; i++) {
+            const nextBus = map.get(queue[i]);
+            if (!nextBus) continue;
+            map.delete(queue[i]);
+            for (let j = 0; j < nextBus.length; j++) {
+                const bus = nextBus[j];
+                if (seen[bus]) continue;
+                seen[bus] = 1;
+                const route = routes[bus];
+                for (let k = 0; k < route.length; k++) {
+                    const next = route[k];
+                    if (next === target) return res;
+                    if (!map.has(next)) continue;
+                    nextQueue.push(next);
+                }
+            }
+        }
+        res++, queue = nextQueue;
+    }
+    return -1;
+}
--- problems/medium/3001-3500/3118.p.sql
+++ problems/medium/3001-3500/3118.p.sql
@@ -1,3 +1,57 @@
 -- 3118 - Friday Purchase III
 
-
+WITH Base AS (
+    SELECT
+        1 AS week_of_month,
+        'Premium' AS membership
+    UNION
+    SELECT
+        1,
+        'VIP'
+    UNION
+    SELECT
+        2,
+        'Premium'
+    UNION
+    SELECT
+        2,
+        'VIP'
+    UNION
+    SELECT
+        3,
+        'Premium'
+    UNION
+    SELECT
+        3,
+        'VIP'
+    UNION
+    SELECT
+        4,
+        'Premium'
+    UNION
+    SELECT
+        4,
+        'VIP'
+),
+Spending AS (
+    SELECT
+        WEEK(purchase_date) - 43 AS week_of_month,
+        membership,
+        SUM(amount_spend) AS sum
+    FROM
+        Purchases
+        LEFT JOIN Users USING(user_id)
+    WHERE
+        DAYOFWEEK(purchase_date) = 6
+        AND membership <> 'Standard'
+    GROUP BY
+        week_of_month,
+        membership
+)
+SELECT
+    week_of_month,
+    membership,
+    COALESCE(sum, 0) AS total_amount
+FROM
+    Base
+    LEFT JOIN Spending USING(week_of_month, membership);
