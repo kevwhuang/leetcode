d204f091423d9612dfb37b7692695eafd9a4c169
Kevin Huang <kevwhuang@gmail.com>
2024-09-24 17:23:55 -0500

    LC 2292/2346/2688

--- problems/medium/2001-2500/2292.p.sql
+++ problems/medium/2001-2500/2292.p.sql
@@ -1,3 +1,21 @@
 -- 2292 - Products With Three or More Orders in Two Consecutive Years
 
-
+WITH CTE AS (
+    SELECT
+        product_id,
+        YEAR(purchase_date) AS year
+    FROM
+        Orders
+    GROUP BY
+        product_id,
+        year
+    HAVING
+        COUNT(*) >= 3
+)
+SELECT
+    DISTINCT product_id
+FROM
+    CTE C1
+    JOIN CTE C2 USING(product_id)
+WHERE
+    C1.year + 1 = C2.year;
--- problems/medium/2501-3000/2688.p.sql
+++ problems/medium/2501-3000/2688.p.sql
@@ -1,3 +1,19 @@
 -- 2688 - Find Active Users
 
-
+WITH CTE AS (
+    SELECT
+        user_id,
+        COUNT(created_at) OVER (
+            PARTITION BY user_id
+            ORDER BY
+                created_at RANGE INTERVAL 7 DAY PRECEDING
+        ) AS count
+    FROM
+        Users
+)
+SELECT
+    DISTINCT user_id
+FROM
+    CTE
+WHERE
+    count > 1;
