e65c0d7ceeae383dee7d3271b66977f299bac67c
Kevin Huang <kevwhuang@gmail.com>
2024-09-27 23:19:29 -0500

    LC 2118/2362

--- problems/hard/2001-2500/2118.p.sql
+++ problems/hard/2001-2500/2118.p.sql
@@ -1,3 +1,27 @@
 -- 2118 - Build the Equation
 
-
+WITH CTE AS (
+    SELECT
+        CONCAT(
+            IF(factor < 0, factor, CONCAT('+', factor)),
+            CASE
+                WHEN power = 0 THEN ''
+                WHEN power = 1 THEN 'X'
+                ELSE CONCAT('X^', power)
+            END
+        ) AS term,
+        power
+    FROM
+        Terms
+)
+SELECT
+    CONCAT(
+        GROUP_CONCAT(
+            term
+            ORDER BY
+                power DESC SEPARATOR ''
+        ),
+        '=0'
+    ) AS equation
+FROM
+    CTE;
--- problems/hard/2001-2500/2362.p.sql
+++ problems/hard/2001-2500/2362.p.sql
@@ -1,3 +1,30 @@
 -- 2362 - Generate the Invoice
 
-
+WITH CTE AS (
+    SELECT
+        invoice_id
+    FROM
+        Purchases
+        JOIN Products USING(product_id)
+    GROUP BY
+        invoice_id
+    ORDER BY
+        SUM(quantity * price) DESC,
+        invoice_id
+    LIMIT
+        1
+)
+SELECT
+    product_id,
+    quantity,
+    quantity * price AS price
+FROM
+    Purchases
+    JOIN Products USING(product_id)
+WHERE
+    invoice_id = (
+        SELECT
+            *
+        FROM
+            CTE
+    );
