d2fcc2e1d1b32b4a5cf8f5c320e034de1fa1a4ce
Kevin Huang <kevwhuang@gmail.com>
2024-10-02 16:53:13 -0500

    LC 615/1225

--- problems/hard/1001-1500/1225.p.sql
+++ problems/hard/1001-1500/1225.p.sql
@@ -1,3 +1,38 @@
 -- 1225 - Report Contiguous Dates
 
-
+WITH CTE AS (
+    SELECT
+        'failed' AS period_state,
+        fail_date AS date,
+        DAYOFYEAR(fail_date) - ROW_NUMBER() OVER (
+            ORDER BY
+                fail_date
+        ) AS gap
+    FROM
+        Failed
+    WHERE
+        YEAR(fail_date) = 2019
+    UNION
+    SELECT
+        'succeeded',
+        success_date,
+        DAYOFYEAR(success_date) - ROW_NUMBER() OVER (
+            ORDER BY
+                success_date
+        )
+    FROM
+        Succeeded
+    WHERE
+        YEAR(success_date) = 2019
+)
+SELECT
+    period_state,
+    MIN(date) AS start_date,
+    MAX(date) AS end_date
+FROM
+    CTE
+GROUP BY
+    period_state,
+    gap
+ORDER BY
+    start_date;
--- problems/hard/501-1000/615.p.sql
+++ problems/hard/501-1000/615.p.sql
@@ -1,3 +1,26 @@
 -- 615 - Average Salary: Departments VS Company
 
-
+WITH CTE AS (
+    SELECT
+        SUBSTRING(pay_date, 1, 7) AS pay_month,
+        AVG(amount) AS average
+    FROM
+        Salary
+    GROUP BY
+        pay_month
+)
+SELECT
+    SUBSTRING(pay_date, 1, 7) AS pay_month,
+    department_id,
+    CASE
+        WHEN ROUND(AVG(amount), 4) < average THEN 'lower'
+        WHEN ROUND(AVG(amount), 4) > average THEN 'higher'
+        ELSE 'same'
+    END AS comparison
+FROM
+    Salary
+    JOIN Employee USING(employee_id)
+    JOIN CTE ON SUBSTRING(pay_date, 1, 7) = pay_month
+GROUP BY
+    pay_month,
+    department_id;
