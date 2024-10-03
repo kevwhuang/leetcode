055acf6a898f4bcd1a0a93b9409febdab5fb54d5
Kevin Huang <kevwhuang@gmail.com>
2024-10-03 17:02:41 -0500

    LC 1384/1412

--- problems/hard/1001-1500/1384.p.sql
+++ problems/hard/1001-1500/1384.p.sql
@@ -1,3 +1,36 @@
 -- 1384 - Total Sales Amount by Year
 
-
+WITH RECURSIVE CTE AS (
+    SELECT
+        MIN(period_start) AS date
+    FROM
+        Sales
+    UNION
+    SELECT
+        DATE_ADD(date, INTERVAL 1 DAY)
+    FROM
+        CTE
+    WHERE
+        date < (
+            SELECT
+                MAX(period_end)
+            FROM
+                Sales
+        )
+)
+SELECT
+    product_id,
+    product_name,
+    CAST(YEAR(date) AS CHAR) AS report_year,
+    SUM(average_daily_sales) AS total_amount
+FROM
+    Product
+    JOIN Sales USING(product_id)
+    JOIN CTE ON period_start <= date
+    AND date <= period_end
+GROUP BY
+    product_id,
+    report_year
+ORDER BY
+    product_id,
+    report_year;
--- problems/hard/1001-1500/1412.p.sql
+++ problems/hard/1001-1500/1412.p.sql
@@ -1,3 +1,28 @@
 -- 1412 - Find the Quiet Students in All Exams
 
-
+WITH CTE AS (
+    SELECT
+        student_id,
+        student_name,
+        score,
+        MIN(score) OVER (PARTITION BY exam_id) AS min,
+        MAX(score) OVER (PARTITION BY exam_id) AS max
+    FROM
+        Student
+        JOIN Exam USING(student_id)
+)
+SELECT
+    DISTINCT student_id,
+    student_name
+FROM
+    CTE
+EXCEPT
+SELECT
+    student_id,
+    student_name
+FROM
+    CTE
+WHERE
+    score IN (min, max)
+ORDER BY
+    student_id;
