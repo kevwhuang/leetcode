8f8889fa318f4b05601e0bd51362ef756d12e1b6
Kevin Huang <kevwhuang@gmail.com>
2024-10-22 21:36:31 -0500

    LC 3328/3329

--- problems/medium/3001-3500/3328.p.sql
+++ problems/medium/3001-3500/3328.p.sql
@@ -1,3 +1,20 @@
 -- 3328 - Find Cities in Each State II
 
-
+SELECT
+    state,
+    GROUP_CONCAT(
+        city
+        ORDER BY
+            city SEPARATOR ', '
+    ) AS cities,
+    SUM(LEFT(state, 1) = LEFT(city, 1)) AS matching_letter_count
+FROM
+    Cities
+GROUP BY
+    state
+HAVING
+    matching_letter_count
+    AND COUNT(*) >= 3
+ORDER BY
+    matching_letter_count DESC,
+    state;
