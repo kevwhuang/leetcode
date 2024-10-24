5d74d22b612dd04209b942577b39d80d573df286
Kevin Huang <kevwhuang@gmail.com>
2024-10-23 23:34:31 -0500

    LC 3323

--- problems/medium/1-500/56.js
+++ problems/medium/1-500/56.js
@@ -1,7 +1,7 @@
 // 56 - Merge Intervals
 
 function merge(intervals) {
-    intervals.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+    intervals.sort((a, b) => a[0] - b[0]);
     const res = [intervals[0]];
     for (let i = 1; i < intervals.length; i++) {
         if (intervals[i][0] > res.at(-1)[1]) res.push(intervals[i]);
--- problems/medium/3001-3500/3323.mjs
+++ problems/medium/3001-3500/3323.mjs
@@ -1,3 +1,17 @@
 // 3323 - Minimize Connected Groups by Inserting Interval
 
-
+function minConnectedGroups(intervals, k) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    const merged = [intervals[0]];
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] > merged.at(-1)[1]) merged.push(intervals[i]);
+        else merged.at(-1)[1] = Math.max(intervals[i][1], merged.at(-1)[1]);
+    }
+    const n = merged.length;
+    let min = n;
+    for (let l = 0, r = 0; l < n; l++) {
+        while (r < n && merged[l][1] + k >= merged[r][0]) r++;
+        min = Math.min(n + l - r + 1, min);
+    }
+    return min;
+}
