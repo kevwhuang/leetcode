c3f95dd342983e4fa215edaa5a41cb0667ca433a
Kevin Huang <kevwhuang@gmail.com>
2024-11-05 20:54:12 -0600

    LC 3340/3341/3342

---
+++ problems/hard/3001-3500/3343.js
@@ -0,0 +1,3 @@
+// 3343 - Count Number of Balanced Permutations
+
+
---
+++ problems/medium/3001-3500/3341.js
@@ -0,0 +1,22 @@
+// 3341 - Find Minimum Time to Reach Last Room I
+
+function minTimeToReach(moveTime) {
+    const m = moveTime.length, n = moveTime[0].length;
+    const costs = Array.from({ length: m }, () => new Uint32Array(n).fill(-1));
+    costs[0][0] = 0;
+    const pq = new PriorityQueue({ compare: (a, b) => a[2] - b[2] });
+    pq.enqueue([0, 0, 0]);
+    const d = [0, -1, 0, 1, 0], mm = m - 1, nn = n - 1;
+    while (true) {
+        const cur = pq.dequeue();
+        for (let i = 0; i < 4; i++) {
+            const r = cur[0] + d[i], c = cur[1] + d[i + 1];
+            if (r === -1 || r === m || c === -1 || c === n) continue;
+            const next = Math.max(cur[2], moveTime[r][c]) + 1;
+            if (next >= costs[r][c]) continue;
+            if (r === mm && c === nn) return next;
+            costs[r][c] = next;
+            pq.enqueue([r, c, next]);
+        }
+    }
+}
