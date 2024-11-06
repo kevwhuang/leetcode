313762d348e6559c8eee3346cfaa991b1d5fb286
Kevin Huang <kevwhuang@gmail.com>
2024-11-06 05:51:42 -0600

    LC 63

--- problems/medium/1-500/63.js
+++ problems/medium/1-500/63.js
@@ -1,3 +1,19 @@
 // 63 - Unique Paths II
 
-
+function uniquePathsWithObstacles(obstacleGrid) {
+    const dp = obstacleGrid, m = dp.length, n = dp[0].length;
+    let paths = 1;
+    for (let c = 0; c < n; c++) {
+        if (dp[0][c]) paths = 0;
+        dp[0][c] = paths;
+    }
+    paths = dp[0][0];
+    for (let r = 1; r < m; r++) {
+        if (dp[r][0]) paths = 0;
+        dp[r][0] = paths;
+        for (let c = 1; c < n; c++) {
+            dp[r][c] = dp[r][c] ? 0 : dp[r - 1][c] + dp[r][c - 1];
+        }
+    }
+    return dp[m - 1][n - 1];
+}
