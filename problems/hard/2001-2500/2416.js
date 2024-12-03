7c87528fdbdf85427a370f9e4b440e11d3f30617
Kevin Huang <kevwhuang@gmail.com>
2024-12-03 00:29:20 -0600

    LC 2416

--- problems/hard/2001-2500/2416.js
+++ problems/hard/2001-2500/2416.js
@@ -1,3 +1,22 @@
 // 2416 - Sum of Prefix Scores of Strings
 
-
+function sumPrefixScores(words) {
+    const trie = {};
+    for (let i = 0; i < words.length; i++) {
+        let node = trie;
+        for (let j = 0; j < words[i].length; j++) {
+            node[words[i][j]] ??= { count: 0 };
+            node[words[i][j]].count++;
+            node = node[words[i][j]];
+        }
+    }
+    const res = new Uint32Array(words.length);
+    for (let i = 0; i < words.length; i++) {
+        let node = trie;
+        for (let j = 0; j < words[i].length; j++) {
+            res[i] += node[words[i][j]].count;
+            node = node[words[i][j]];
+        }
+    }
+    return res;
+}
