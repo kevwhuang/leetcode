92dbf38781945f7d9b706eb720d393a00ed55e83
Kevin Huang <kevwhuang@gmail.com>
2024-12-13 15:22:05 -0600

    LC 3036

--- problems/hard/3001-3500/3036.js
+++ problems/hard/3001-3500/3036.js
@@ -1,3 +1,22 @@
 // 3036 - Number of Subarrays That Match a Pattern II
 
-
+function countMatchingSubarrays(nums, pattern) {
+    const m = pattern.length, n = nums.length;
+    const lps = new Uint32Array(m);
+    let l = 0, r = 1;
+    while (r < m) {
+        if (pattern[l] === pattern[r]) lps[r++] = ++l;
+        else if (l) l = lps[l - 1];
+        else r++;
+    }
+    let res = 0, i = 1, j = 0;
+    while (i < n) {
+        const a = nums[i - 1], b = nums[i], c = pattern[j];
+        if ((a < b && c > 0) || (a === b && !c) || (a > b && c < 0)) {
+            if (++i && ++j === m && ++res) j = lps[j - 1];
+        }
+        else if (j) j = lps[j - 1];
+        else i++;
+    }
+    return res;
+}
