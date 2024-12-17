7237995e80a3f97a98c4f0ff617efb6cebd1ff47
Kevin Huang <kevwhuang@gmail.com>
2024-12-17 03:23:46 -0600

    LC 3386/3387

--- problems/easy/3001-3500/3386.js
+++ problems/easy/3001-3500/3386.js
@@ -1,3 +1,11 @@
 // 3386 - Button With Longest Push Time
 
-
+function buttonWithLongestTime(events) {
+    let res = events[0][0], max = events[0][1];
+    for (let i = 1; i < events.length; i++) {
+        const d = events[i][1] - events[i - 1][1];
+        if (d === max) res = Math.min(events[i][0], res);
+        else if (d > max) res = events[i][0], max = d;
+    }
+    return res;
+}
--- problems/medium/3001-3500/3387.js
+++ problems/medium/3001-3500/3387.js
@@ -1,3 +1,18 @@
 // 3387 - Maximize Amount After Two Days of Conversions
 
-
+function maxAmount(initialCurrency, pairs1, rates1, pairs2, rates2) {
+    function check(pairs, rates) {
+        for (let i = 0; i < pairs.length; i++) {
+            for (let j = 0; j < pairs.length; j++) {
+                const u = pairs[j][0], v = pairs[j][1], w = rates[j];
+                C[u] ??= 0, C[v] ??= 0;
+                C[u] = Math.max(C[v] / w, C[u]);
+                C[v] = Math.max(C[u] * w, C[v]);
+            }
+        }
+    }
+    const C = { [initialCurrency]: 1 };
+    check(pairs1, rates1);
+    check(pairs2, rates2);
+    return C[initialCurrency];
+}
