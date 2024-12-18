0188ae0072551469b5bf6438ea67812f7a8cd93e
Kevin Huang <kevwhuang@gmail.com>
2024-12-18 03:13:47 -0600

    LC 377/3390

--- problems/hard/3001-3500/3390.p.sql
+++ problems/hard/3001-3500/3390.p.sql
@@ -1,3 +1,44 @@
 -- 3390 - Longest Team Pass Streak
 
-
+WITH CTE1 AS (
+    SELECT
+        T1.team_name AS team1,
+        T2.team_name AS team2,
+        ROW_NUMBER() OVER (
+            PARTITION BY T1.team_name
+            ORDER BY
+                time_stamp
+        ) AS r
+    FROM
+        Passes
+        JOIN Teams T1 ON pass_from = player_id
+        JOIN Teams T2 ON pass_to = T2.player_id
+),
+CTE2 AS (
+    SELECT
+        team1 AS team_name,
+        r - ROW_NUMBER() OVER (PARTITION BY team1) AS gap
+    FROM
+        CTE1
+    WHERE
+        team1 = team2
+),
+CTE3 AS (
+    SELECT
+        team_name,
+        COUNT(*) AS count
+    FROM
+        CTE2
+    GROUP BY
+        team_name,
+        gap
+)
+SELECT
+    team_name,
+    MAX(count) AS longest_streak
+FROM
+    CTE3
+GROUP BY
+    team_name
+ORDER BY
+    team_name;
--- problems/medium/1-500/377.js
+++ problems/medium/1-500/377.js
@@ -1,3 +1,13 @@
 // 377 - Combination Sum IV
 
-
+function combinationSum4(nums, target) {
+    const dp = new Uint32Array(target + 1);
+    dp[0] = 1;
+    for (let i = 1; i <= target; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[j] > i) continue;
+            dp[i] += dp[i - nums[j]];
+        }
+    }
+    return dp[target];
+}
