3af1f2743b030ad2040b461c13fa3c5f7ccac865
Kevin Huang <kevwhuang@gmail.com>
2024-12-22 03:10:44 -0600

    LC 688/2320

--- problems/medium/2001-2500/2320.js
+++ problems/medium/2001-2500/2320.js
@@ -1,3 +1,12 @@
 // 2320 - Count Number of Ways to Place Houses
 
-
+function countHousePlacements(n) {
+    function init() {
+        dp = new Uint32Array(10001), dp[0] = 1, dp[1] = 2;
+        for (let i = 2; i < 10001; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000007;
+        }
+    }
+    if (this.dp === undefined) init();
+    return Number(BigInt(dp[n]) ** 2n % 1000000007n);
+}
--- problems/medium/501-1000/688.js
+++ problems/medium/501-1000/688.js
@@ -1,3 +1,29 @@
 // 688 - Knight Probability in Chessboard
 
-
+function knightProbability(n, k, row, column) {
+    let dp = Array.from({ length: n }, () => new Uint8Array(n));
+    dp[row][column] = 1;
+    const dr = [-2, -2, -1, -1, 1, 1, 2, 2];
+    const dc = [-1, 1, -2, 2, -2, 2, -1, 1];
+    for (let i = 0; i < k; i++) {
+        const N = Array.from({ length: n }, () => new Array(n).fill(0));
+        for (let r = 0; r < n; r++) {
+            for (let c = 0; c < n; c++) {
+                if (dp[r][c] === 0) continue;
+                for (let j = 0; j < 8; j++) {
+                    const rr = r + dr[j], cc = c + dc[j];
+                    if (rr < 0 || rr >= n || cc < 0 || cc >= n) continue;
+                    N[rr][cc] += dp[r][c];
+                }
+            }
+        }
+        dp = N;
+    }
+    let sum = 0;
+    for (let r = 0; r < n; r++) {
+        for (let c = 0; c < n; c++) {
+            sum += dp[r][c];
+        }
+    }
+    return sum / 8 ** k;
+}
