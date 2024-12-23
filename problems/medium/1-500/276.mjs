bf9ad43fb7897e59dcf62eab77ae569e4dfb9301
Kevin Huang <kevwhuang@gmail.com>
2024-12-23 17:23:48 -0600

    LC 276/1746

--- problems/medium/1-500/276.mjs
+++ problems/medium/1-500/276.mjs
@@ -1,3 +1,11 @@
 // 276 - Paint Fence
 
-
+function numWays(n, k) {
+    if (n === 1) return k;
+    let a = k, b = k * k, i = 1;
+    while (++i < n) {
+        const next = (k - 1) * (a + b);
+        a = b, b = next;
+    }
+    return b;
+}
--- problems/medium/1501-2000/1746.mjs
+++ problems/medium/1501-2000/1746.mjs
@@ -1,3 +1,11 @@
 // 1746 - Maximum Subarray Sum After One Operation
 
-
+function maxSumAfterOperation(nums) {
+    let res = 0, max1 = 0, max2 = 0, i = -1;
+    while (++i < nums.length) {
+        max1 = Math.max(max1 + nums[i], max2 + nums[i] ** 2);
+        max2 = Math.max(0, max2 + nums[i]);
+        res = Math.max(max1, res);
+    }
+    return res;
+}
