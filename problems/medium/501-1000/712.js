d26ad8c9277d6532763b79e9fb858d6710814eb6
Kevin Huang <kevwhuang@gmail.com>
2024-12-23 08:26:57 -0600

    LC 712/714

--- problems/medium/501-1000/712.js
+++ problems/medium/501-1000/712.js
@@ -1,3 +1,18 @@
 // 712 - Minimum ASCII Delete Sum for Two Strings
 
-
+function minimumDeleteSum(s1, s2) {
+    let sum = 0;
+    const m = s1.length, n = s2.length;
+    const dp = new Uint32Array(n);
+    for (let i = 0; i < m; i++) {
+        sum += s1.charCodeAt(i);
+        for (let prev = 0, j = 0; j < n; j++) {
+            if (i === 0) sum += s2.charCodeAt(j);
+            const cur = dp[j];
+            if (s1[i] === s2[j]) dp[j] = prev + s1.charCodeAt(i);
+            else if (j) dp[j] = Math.max(dp[j - 1], dp[j]);
+            prev = cur;
+        }
+    }
+    return sum - 2 * dp[n - 1];
+}
--- problems/medium/501-1000/714.js
+++ problems/medium/501-1000/714.js
@@ -1,3 +1,10 @@
 // 714 - Best Time to Buy and Sell Stock With Transaction Fee
 
-
+function maxProfit(prices, fee) {
+    let res = 0, max = -Infinity, i = -1;
+    while (++i < prices.length) {
+        res = Math.max(max + prices[i], res);
+        max = Math.max(res - fee - prices[i], max);
+    }
+    return res;
+}
