2a4b5c0ee241fbd8491aaf1b2105e25aaf7532fa
Kevin Huang <kevwhuang@gmail.com>
2024-12-26 17:15:08 -0600

    LC 764

--- problems/medium/501-1000/764.js
+++ problems/medium/501-1000/764.js
@@ -1,3 +1,18 @@
 // 764 - Largest Plus Sign
 
-
+function orderOfLargestPlusSign(n, mines) {
+    function check(d, flag) {
+        for (let r = 0; r < n; r++) {
+            for (let acc = 0, c = ~d ? 0 : n - 1; ~c && c < n; c += d) {
+                const a = dp[r][c], b = dp[c][r];
+                if (flag) a ? dp[r][c] = min(++acc, a) : acc = 0;
+                else b ? dp[c][r] = min(++acc, b) : acc = 0;
+            }
+        }
+    }
+    const dp = Array.from({ length: n }, () => new Uint16Array(n).fill(-1));
+    mines.forEach(e => dp[e[0]][e[1]] = 0);
+    const { min, max } = Math;
+    [[-1], [1], [-1, true], [1, true]].forEach(e => check(...e));
+    return dp.reduce((s, e) => max(e.reduce((s, e) => max(e, s), 0), s), 0);
+}
