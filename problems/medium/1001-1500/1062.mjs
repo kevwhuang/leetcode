812da5580a88469fe53e860a07ac47a44c9144d8
Kevin Huang <kevwhuang@gmail.com>
2024-12-27 06:35:18 -0600

    LC 1062/2378

--- problems/medium/1001-1500/1062.mjs
+++ problems/medium/1001-1500/1062.mjs
@@ -1,3 +1,25 @@
 // 1062 - Longest Repeating Substring
 
-
+function longestRepeatingSubstring(s) {
+    function init() {
+        dict = new Uint32Array(1999);
+        let k = 1, i = 0;
+        while (i < 1999) dict[i++] = k, k = 29 * k % mod;
+    }
+    const key = i => s.charCodeAt(i) - 97;
+    const n = s.length, mod = 1000000007;
+    if (this.dict === undefined) init();
+    let l = 1, r = n - 1;
+    while (l <= r) {
+        let hash = 0, i = 0;
+        const set = new Set(), m = (l + r >> 1) - 1, k = dict[m];
+        while (i < m) hash = (hash + dict[m - i] * key(i++)) % mod;
+        while (i < n) {
+            if (set.has(hash += key(i)) && (hash = -1)) break;
+            set.add(hash);
+            hash = 29 * (hash + mod - k * key(i++ - m) % mod) % mod;
+        }
+        hash < 0 ? l = m + 2 : r = m;
+    }
+    return r;
+}
--- problems/medium/2001-2500/2378.mjs
+++ problems/medium/2001-2500/2378.mjs
@@ -1,3 +1,20 @@
 // 2378 - Choose Edges to Maximize Score in a Tree
 
-
+function maxScore(edges) {
+    function dfs(node) {
+        let max = 0, sum = 0;
+        const next = adj[node];
+        for (let i = 0; i < next.length; i++) {
+            const res = dfs(next[i][0]);
+            max = Math.max(next[i][1] - res[0], max);
+            sum += res[0] + res[1];
+        }
+        return [max, sum];
+    }
+    const adj = Array.from({ length: edges.length }, () => []);
+    for (let i = 1; i < edges.length; i++) {
+        adj[edges[i][0]].push(edges[i]);
+        edges[i][0] = i;
+    }
+    return dfs(0).reduce((s, e) => s + e);
+}
