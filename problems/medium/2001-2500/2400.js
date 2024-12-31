7e652888c4fa85e8a902fea89a7dca920ccc477a
Kevin Huang <kevwhuang@gmail.com>
2024-12-30 23:06:55 -0600

    LC 2400/2915

--- problems/medium/2001-2500/2400.js
+++ problems/medium/2001-2500/2400.js
@@ -1,3 +1,18 @@
 // 2400 - Number of Ways to Reach a Position After Exactly K Steps
 
-
+function numberOfWays(startPos, endPos, k) {
+    function init() {
+        dp = new Array(1001), dp[0] = [1];
+        for (let i = 1; i < 1001; i++) {
+            const prev = dp[i - 1], n = prev.length;
+            const cur = dp[i] = new Uint32Array(n + 2);
+            for (let j = 0; j < n; j++) {
+                cur[j] = (cur[j] + prev[j]) % 1000000007;
+                cur[j + 2] = prev[j];
+            }
+        }
+    }
+    if (this.dp === undefined) init();
+    const abs = Math.abs(startPos - endPos);
+    return abs <= k ? dp[k][k + abs] : 0;
+}
--- problems/medium/2501-3000/2915.js
+++ problems/medium/2501-3000/2915.js
@@ -1,3 +1,13 @@
 // 2915 - Length of the Longest Subsequence That Sums to Target
 
-
+function lengthOfLongestSubsequence(nums, target) {
+    const dp = new Uint16Array(target + 1);
+    for (let i = 0; i < nums.length; i++) {
+        const cur = nums[i];
+        for (let j = target - cur; j > 0; j--) {
+            if (dp[j]) dp[j + cur] = Math.max(dp[j] + 1, dp[j + cur]);
+        }
+        dp[cur] ||= 1;
+    }
+    return dp[target] || -1;
+}
