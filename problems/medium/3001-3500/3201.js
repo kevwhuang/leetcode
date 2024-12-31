56cd7beb7348dd628158cce394c41505a575cdd4
Kevin Huang <kevwhuang@gmail.com>
2024-12-31 06:50:18 -0600

    LC 3201/3202

--- problems/medium/3001-3500/3201.js
+++ problems/medium/3001-3500/3201.js
@@ -1,3 +1,11 @@
 // 3201 - Find the Maximum Length of Valid Subsequence I
 
-
+function maximumLength(nums) {
+    const dp = [(nums[0] + 1) % 2, nums[0] % 2, 1];
+    for (let flag = dp[1], i = 1; i < nums.length; i++) {
+        dp[nums[i] % 2]++;
+        if (nums[i] % 2 === flag) continue;
+        dp[2]++, flag = flag ? 0 : 1;
+    }
+    return Math.max(...dp);
+}
--- problems/medium/3001-3500/3202.js
+++ problems/medium/3001-3500/3202.js
@@ -1,3 +1,34 @@
 // 3202 - Find the Maximum Length of Valid Subsequence II
 
-
+function maximumLength(nums, k) {
+    function check(arr1, arr2) {
+        if (arr1[0] > arr2[0]) return check(arr2, arr1);
+        let acc = 2, i = 0, j = 0;
+        const m = arr1.length, n = arr2.length;
+        while (true) {
+            while (i < m && arr1[i] < arr2[j]) i++;
+            if (i === m) break;
+            acc++;
+            while (j < n && arr1[i] > arr2[j]) j++;
+            if (j === n) break;
+            acc++;
+        }
+        res = Math.max(acc, res);
+    }
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const mod = nums[i] % k;
+        if (!map.has(mod)) map.set(mod, []);
+        map.get(mod).push(i);
+    }
+    const M = [...map.values()].sort((a, b) => b.length - a.length);
+    let res = M[0].length;
+    for (let i = 0; i < M.length; i++) {
+        if (2 * M[i].length <= res) break;
+        for (let j = i + 1; j < M.length; j++) {
+            if (M[i].length + M[j].length <= res) break;
+            check(M[i], M[j]);
+        }
+    }
+    return res;
+}
