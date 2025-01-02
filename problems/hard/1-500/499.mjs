081bb208f57523487dcdfa04e534421f3d656980
Kevin Huang <kevwhuang@gmail.com>
2025-01-01 23:00:42 -0600

    LC 499/2577

--- problems/hard/1-500/499.mjs
+++ problems/hard/1-500/499.mjs
@@ -1,3 +1,31 @@
 // 499 - The Maze III
 
-
+function findShortestWay(maze, ball, hole) {
+    const M = maze, m = M.length, n = M[0].length;
+    M[hole[0]][hole[1]] = 1;
+    const C = Array.from({ length: m }, () => new Uint16Array(n).fill(-1));
+    C[ball[0]][ball[1]] = 0;
+    const pq = new PriorityQueue({ compare: (a, b) => a[2] - b[2] });
+    pq.enqueue(ball.concat(0, ''));
+    const D = [[-1, 0, 'u'], [1, 0, 'd'], [0, -1, 'l'], [0, 1, 'r']];
+    let res, min = Infinity;
+    while (pq.front()?.[2] <= min) {
+        const arr = pq.dequeue();
+        for (let i = 0; i < 4; i++) {
+            const dr = D[i][0], dc = D[i][1], t = arr[3] + D[i][2];
+            let acc = arr[2], rr = arr[0] + dr, cc = arr[1] + dc;
+            while (~rr && rr < m && ~cc && cc < n && !M[rr][cc]) {
+                acc++, rr += dr, cc += dc;
+            }
+            if (rr === hole[0] && cc === hole[1]) {
+                if (acc === min && t < res) res = t;
+                else if (acc < min) res = t, min = acc;
+            }
+            rr -= dr, cc -= dc;
+            if (acc === arr[2] || acc > C[rr][cc]) continue;
+            C[rr][cc] = acc;
+            pq.enqueue([rr, cc, acc, t]);
+        }
+    }
+    return res || 'impossible';
+}
--- problems/hard/2501-3000/2577.js
+++ problems/hard/2501-3000/2577.js
@@ -1,3 +1,23 @@
 // 2577 - Minimum Time to Visit a Cell in a Grid
 
-
+function minimumTime(grid) {
+    const M = grid, m = M.length, n = M[0].length, mm = m - 1, nn = n - 1;
+    if (M[0][1] > 1 && M[1][0] > 1) return -1;
+    const C = Array.from({ length: m }, () => new Uint32Array(n));
+    const pq = new PriorityQueue({ compare: (a, b) => a[2] - b[2] });
+    pq.enqueue([0, 0, 0]);
+    const D = [0, -1, 0, 1, 0];
+    while (true) {
+        const r = pq.front()[0], c = pq.front()[1], w = pq.dequeue()[2];
+        for (let i = 0; i < 4; i++) {
+            const rr = r + D[i], cc = c + D[i + 1];
+            if (rr === -1 || rr === m || cc === -1 || cc === n) continue;
+            const cur = M[rr][cc];
+            const next = Math.max(w + 1, (cur - w) % 2 ? cur : cur + 1);
+            if (rr === mm && cc === nn) return next;
+            if (next >= (C[rr][cc] || Infinity)) continue;
+            C[rr][cc] = next;
+            pq.enqueue([rr, cc, next]);
+        }
+    }
+}
