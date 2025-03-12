04a7a08ad5d399341a3e0d89b56507540170a555
Kevin Huang <kevwhuang@gmail.com>
2025-03-12 12:05:42 -0500

    LC 467/3144

--- problems/medium/1-500/467.js
+++ problems/medium/1-500/467.js
@@ -1,3 +1,15 @@
 // 467 - Unique Substrings in Wraparound String
 
-
+function findSubstringInWraproundString(s) {
+    const arr = new Uint32Array(26);
+    let prev, acc, i = -1;
+    while (++i < s.length) {
+        const key = s.charCodeAt(i) - 97;
+        if (prev + 1 === key) acc++;
+        else if (prev === 25 && key === 0) acc++;
+        else acc = 1;
+        arr[key] = Math.max(acc, arr[key]);
+        prev = key;
+    }
+    return arr.reduce((s, e) => s + e);
+}
--- problems/medium/3001-3500/3144.js
+++ problems/medium/3001-3500/3144.js
@@ -1,3 +1,18 @@
 // 3144 - Minimum Substring Partition of Equal Character Frequency
 
-
+function minimumSubstringsInPartition(s) {
+    const n = s.length;
+    const dp = new Uint16Array(n).fill(n);
+    const B = new Uint16Array(26);
+    for (let r = 0; r < n; r++) {
+        B.fill(0);
+        for (let acc = 0, max = 0, l = r; ~l; l--) {
+            const key = s.charCodeAt(l) - 97;
+            if (B[key] === 0) acc++;
+            max = Math.max(++B[key], max);
+            if (acc * max > r - l + 1) continue;
+            dp[r] = l ? Math.min(dp[l - 1] + 1, dp[r]) : 1;
+        }
+    }
+    return dp[n - 1];
+}
