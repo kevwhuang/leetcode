8b11c38fac67e980c0fd77b88b7de8fd80e34b6f
Kevin Huang <kevwhuang@gmail.com>
2025-03-22 19:28:32 -0500

    LC 294/375

--- problems/medium/1-500/294.mjs
+++ problems/medium/1-500/294.mjs
@@ -1,3 +1,25 @@
 // 294 - Flip Game II
 
-
+function canWin(currentState) {
+    function dfs() {
+        const key = cur.join``;
+        if (memo1.has(key)) return true;
+        if (memo2.has(key)) return false;
+        let flag = false, i = 0;
+        while (!flag && ++i < n) {
+            if (cur[i - 1] === 0 || cur[i] === 0) continue;
+            cur[i - 1] = cur[i] = 0;
+            flag = !dfs();
+            cur[i - 1] = cur[i] = 1;
+        }
+        (flag ? memo1 : memo2).add(key);
+        return flag;
+    }
+    const n = currentState.length;
+    const cur = new Array(n);
+    for (let i = 0; i < n; i++) {
+        cur[i] = Number(currentState[i] === '+');
+    }
+    const memo1 = new Set(), memo2 = new Set();
+    return dfs();
+}
--- problems/medium/1-500/375.js
+++ problems/medium/1-500/375.js
@@ -1,3 +1,20 @@
 // 375 - Guess Number Higher or Lower II
 
-
+function getMoneyAmount(n) {
+    function init() {
+        memo = Array.from({ length: 201 }, () => new Uint16Array(201));
+        dfs(1, 200);
+    }
+    function dfs(l, r) {
+        if (l >= r) return 0;
+        if (memo[l][r]) return memo[l][r];
+        let min = Infinity, i = l + r >> 1;
+        while (i < r) {
+            const max = Math.max(dfs(l, i - 1), dfs(i + 1, r));
+            min = Math.min(max + i++, min);
+        }
+        return memo[l][r] = min;
+    }
+    if (!this.memo) init();
+    return memo[1][n];
+}
