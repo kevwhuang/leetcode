6181c99b85860230cb4d1047167a68b91b67c239
Kevin Huang <kevwhuang@gmail.com>
2025-03-27 05:21:38 -0500

    LC 368/3469

--- problems/medium/1-500/368.js
+++ problems/medium/1-500/368.js
@@ -1,3 +1,23 @@
 // 368 - Largest Divisible Subset
 
-
+function largestDivisibleSubset(nums) {
+    nums = new Uint32Array(nums).sort();
+    const n = nums.length;
+    const dp = new Uint16Array(n).fill(1);
+    const arr = new Uint16Array(n).fill(10000);
+    let idx = 0;
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] % nums[j]) continue;
+            if (dp[j] + 1 <= dp[i]) continue;
+            dp[i] = dp[j] + 1, arr[i] = j;
+        }
+        if (dp[i] > dp[idx]) idx = i;
+    }
+    const res = [];
+    while (idx < 10000) {
+        res.push(nums[idx]);
+        idx = arr[idx];
+    }
+    return res.reverse();
+}
--- problems/medium/3001-3500/3469.js
+++ problems/medium/3001-3500/3469.js
@@ -1,3 +1,22 @@
 // 3469 - Find Minimum Cost to Remove Array Elements
 
-
+function minCost(nums) {
+    let dp = new Map([[nums[0], 0]]);
+    const n = nums.length;
+    for (let i = 1; i <= n; i += 2) {
+        const N = new Map();
+        const cur = i < n ? nums[i] : 0;
+        const next = i + 1 < n ? nums[i + 1] : 1e9;
+        for (const e of dp) {
+            let a = Math.min(e[0], next);
+            let b = Math.max(e[0], next);
+            let c = cur;
+            if (cur < a) c = b, b = a, a = cur;
+            else if (cur < b) c = b, b = cur;
+            N.set(a, Math.min(N.get(a) ?? 1e9, e[1] + c));
+            N.set(c, Math.min(N.get(c) ?? 1e9, e[1] + b));
+        }
+        dp = N;
+    }
+    return Math.min(...dp.values());
+}
