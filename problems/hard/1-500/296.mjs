9440e625e0f3d1e58dc548ccb315828aa412d5eb
Kevin Huang <kevwhuang@gmail.com>
2025-04-15 01:27:16 -0500

    LC 296/2468

--- problems/hard/1-500/296.mjs
+++ problems/hard/1-500/296.mjs
@@ -1,3 +1,20 @@
 // 296 - Best Meeting Point
 
-
+function minTotalDistance(grid) {
+    let acc = 0;
+    grid.forEach(e => e.forEach(f => f && acc++));
+    const arr1 = new Uint8Array(acc);
+    const arr2 = new Uint8Array(acc);
+    const m = grid.length, n = grid[0].length;
+    for (let i = 0, r = 0; r < m; r++) {
+        for (let c = 0; c < n; c++) {
+            if (grid[r][c]) arr1[i] = r, arr2[i++] = c;
+        }
+    }
+    arr2.sort();
+    let res = 0;
+    const a = arr1[acc >> 1], b = arr2[acc >> 1];
+    arr1.forEach(e => res += Math.abs(e - a));
+    arr2.forEach(e => res += Math.abs(e - b));
+    return res;
+}
--- problems/hard/2001-2500/2468.js
+++ problems/hard/2001-2500/2468.js
@@ -1,3 +1,18 @@
 // 2468 - Split Message Based on Limit
 
-
+function splitMessage(message, limit) {
+    const n = message.length;
+    let acc = 1, d = 1, r = 1;
+    while (n + acc > r * (limit - d - 3)) {
+        if (d + d + 3 >= limit) return [];
+        if (++r === 10 || r === 100 || r === 1000) d++;
+        acc += d;
+    }
+    const res = new Array(r);
+    let l = 0, i = 0;
+    while (++l <= r) {
+        const s = `<${l}/${r}>`;
+        res[l - 1] = message.slice(i, i += limit - s.length) + s;
+    }
+    return res;
+}
