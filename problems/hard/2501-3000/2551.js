39cf1884a6fd64be7890d93d124bb0b442cdf0d7
Kevin Huang <kevwhuang@gmail.com>
2025-05-05 18:22:52 -0500

    LC 632/2551/3540

--- problems/hard/2501-3000/2551.js
+++ problems/hard/2501-3000/2551.js
@@ -1,3 +1,14 @@
 // 2551 - Put Marbles in Bags
 
-
+function putMarbles(weights, k) {
+    k--;
+    const n = weights.length - 1;
+    const arr = new Uint32Array(n);
+    for (let i = 0; i < n; i++) {
+        arr[i] = weights[i] + weights[i + 1];
+    }
+    arr.sort();
+    let res = 0;
+    while (k--) res += arr[n - k - 1] - arr[k];
+    return res;
+}
--- problems/hard/501-1000/632.js
+++ problems/hard/501-1000/632.js
@@ -1,3 +1,21 @@
 // 632 - Smallest Range Covering Elements From K Lists
 
-
+function smallestRange(nums) {
+    const M = nums, n = nums.length;
+    const arr = new Uint8Array(n);
+    const pq = new PriorityQueue((a, b) => M[a][arr[a]] - M[b][arr[b]]);
+    let right = -Infinity, i = -1;
+    while (++i < n) {
+        pq.enqueue(i);
+        right = Math.max(M[i][0], right);
+    }
+    const res = [0, Infinity];
+    while (true) {
+        const j = pq.dequeue(), left = M[j][arr[j]];
+        if (right - left < res[1] - res[0]) res[0] = left, res[1] = right;
+        if (++arr[j] === M[j].length) break;
+        pq.enqueue(j);
+        right = Math.max(M[j][arr[j]], right);
+    }
+    return res;
+}
