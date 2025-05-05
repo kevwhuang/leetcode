b400f64af6020e715de46028276390a58eb619d5
Kevin Huang <kevwhuang@gmail.com>
2025-05-05 01:43:07 -0500

    LC 3522/3523

--- problems/medium/3501-4000/3522.js
+++ problems/medium/3501-4000/3522.js
@@ -1,3 +1,11 @@
 // 3522 - Calculate Score After Performing Instructions
 
-
+function calculateScore(instructions, values) {
+    let res = 0, cur, i = 0;
+    while (i >= 0 && i < values.length && values[i] !== null) {
+        cur = values[i], values[i] = null;
+        if (instructions[i] > 'b') i += cur;
+        else res += cur, i++;
+    }
+    return res;
+}
--- problems/medium/3501-4000/3523.js
+++ problems/medium/3501-4000/3523.js
@@ -1,3 +1,7 @@
 // 3523 - Make Array Non-Decreasing
 
-
+function maximumPossibleSize(nums) {
+    let res = 0, max = 0, i = -1;
+    while (++i < nums.length) if (nums[i] >= max) res++, max = nums[i];
+    return res;
+}
