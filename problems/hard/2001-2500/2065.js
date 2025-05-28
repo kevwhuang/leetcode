49056a96a5f24bc97efaa207da97ab76b9e8b614
Kevin Huang <kevwhuang@gmail.com>
2025-05-28 02:41:28 -0500

    LC 2065/2258

--- problems/hard/2001-2500/2065.js
+++ problems/hard/2001-2500/2065.js
@@ -1,3 +1,28 @@
 // 2065 - Maximum Path Quality of a Graph
 
-
+function maximalPathQuality(values, edges, maxTime) {
+    function backtrack(node, acc1, acc2) {
+        if (node === 0 && acc1 <= maxTime) res = Math.max(acc2, res);
+        if (acc1 + 10 > maxTime) return;
+        const arr = adj[node];
+        for (let i = 0; i < arr.length; i++) {
+            const v = arr[i][0];
+            if (seen[v]) backtrack(v, acc1 + arr[i][1], acc2);
+            if (seen[v]) continue;
+            seen[v] = 1;
+            backtrack(v, acc1 + arr[i][1], acc2 + values[v]);
+            seen[v] = 0;
+        }
+    }
+    const adj = Array.from({ length: values.length }, () => []);
+    for (let i = 0; i < edges.length; i++) {
+        const u = edges[i][0], v = edges[i][1], w = edges[i][2];
+        adj[u].push([v, w]);
+        adj[v].push([u, w]);
+    }
+    let res = 0;
+    const seen = new Uint8Array(values.length);
+    seen[0] = 1;
+    backtrack(0, 0, values[0]);
+    return res;
+}
--- problems/hard/2001-2500/2258.js
+++ problems/hard/2001-2500/2258.js
@@ -1,3 +1,51 @@
 // 2258 - Escape the Spreading Fire
 
-
+function maximumMinutes(grid) {
+    function check(Q, acc, flag) {
+        while (Q.length) {
+            const N = [];
+            for (let i = 0; i < Q.length; i++) {
+                const r = Q[i][0], c = Q[i][1];
+                for (let j = 0; j < 4; j++) {
+                    const rr = r + D[j], cc = c + D[j + 1];
+                    if (rr < 0 || rr === m || cc < 0 || cc === n) continue;
+                    if (grid[rr][cc]) continue;
+                    if (flag) {
+                        if (M[rr][cc] < 50000) continue;
+                        M[rr][cc] = acc;
+                        N.push([rr, cc]);
+                    } else {
+                        if (seen[rr][cc]) continue;
+                        seen[rr][cc] = 1;
+                        if (M[rr][cc] < acc) continue;
+                        if (M[rr][cc] > acc) N.push([rr, cc]);
+                        if (rr === mm && cc === nn) return true;
+                    }
+                }
+            }
+            acc++, Q = N;
+        }
+    }
+    const m = grid.length, n = grid[0].length, mm = m - 1, nn = n - 1;
+    const M = Array.from({ length: m }, () => new Uint16Array(n).fill(50000));
+    const Q = [], D = [0, -1, 0, 1, 0];
+    for (let r = 0; r < m; r++) {
+        for (let c = 0; c < n; c++) {
+            if (grid[r][c] !== 1) continue;
+            M[r][c] = 0;
+            Q.push([r, c]);
+        }
+    }
+    check(Q, 1, true);
+    const seen = Array.from({ length: m }, () => new Uint8Array(n));
+    let l = 0, r = m * n >> 1;
+    while (l <= r) {
+        const m = l + r >> 1;
+        if (check([[0, 0]], m + 1)) l = m + 1;
+        else r = m - 1;
+        if (M[mm][nn] === 50000) return l === m + 1 ? 1e9 : -1;
+        seen.forEach(e => e.fill(0));
+        seen[0][0] = 1;
+    }
+    return r;
+}
