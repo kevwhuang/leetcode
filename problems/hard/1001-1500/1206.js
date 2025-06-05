910756fcd90be274d397916fc981736ea65961dc
Kevin Huang <kevwhuang@gmail.com>
2025-06-05 18:39:32 -0500

    LC 1206/2781

--- problems/hard/1001-1500/1206.js
+++ problems/hard/1001-1500/1206.js
@@ -1,3 +1,16 @@
 // 1206 - Design Skiplist
 
-
+class Skiplist {
+    constructor() {
+        this.B = new Uint16Array(20001);
+    }
+    add(num) {
+        this.B[num]++;
+    }
+    erase(num) {
+        return this.B[num] ? Boolean(this.B[num]--) : false;
+    }
+    search(target) {
+        return Boolean(this.B[target]);
+    }
+}
--- problems/hard/2501-3000/2781.js
+++ problems/hard/2501-3000/2781.js
@@ -1,3 +1,16 @@
 // 2781 - Length of the Longest Valid Substring
 
-
+function longestValidSubstring(word, forbidden) {
+    let res = 0, l = 0, r = 0;
+    const set = new Set(forbidden);
+    while (r < word.length) {
+        let s = '', i = ++r;
+        const max = Math.max(l, r - 10);
+        while (--i >= max) {
+            s = word[i] + s;
+            if (set.has(s)) l = i + 1, i = 0;
+        }
+        res = Math.max(r - l, res);
+    }
+    return res;
+}
