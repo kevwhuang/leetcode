df36f2a5f7e181573805c126fbc814678fe78a5e
Kevin Huang <kevwhuang@gmail.com>
2025-06-06 03:46:39 -0500

    LC 2306/2514

--- problems/hard/2001-2500/2306.js
+++ problems/hard/2001-2500/2306.js
@@ -1,3 +1,17 @@
 // 2306 - Naming a Company
 
-
+function distinctNames(ideas) {
+    const arr = Array.from({ length: 26 }, () => new Set());
+    ideas.forEach(e => arr[e.charCodeAt() - 97].add(e.slice(1)));
+    let res = 0;
+    for (let i = 0; i < 25; i++) {
+        for (let j = i + 1; j < 26; j++) {
+            let acc = 0;
+            for (const s of arr[i]) {
+                if (arr[j].has(s)) acc++;
+            }
+            res += (arr[i].size - acc) * (arr[j].size - acc);
+        }
+    }
+    return 2 * res;
+}
--- problems/hard/2501-3000/2514.js
+++ problems/hard/2501-3000/2514.js
@@ -1,3 +1,25 @@
 // 2514 - Count Anagrams
 
-
+function countAnagrams(s) {
+    function init() {
+        dict1 = new BigUint64Array(100001), dict1[0] = 1n;
+        for (let i = 1n; i < 100001n; i++) {
+            dict1[Number(i)] = (dict1[i - 1n] * i) % 1000000007n;
+        }
+        dict2 = new BigUint64Array(100001), dict2[100000] = 716327852n;
+        for (let i = 99999n; ~i; i--) {
+            dict2[Number(i)] = (dict2[i + 1n] * (i + 1n)) % 1000000007n;
+        }
+    }
+    if (!this.dict1) init();
+    let res = 1n, i = 0;
+    const B = new Uint32Array(26), n = s.length;
+    while (i < n) {
+        let j = i;
+        while (j < n && s[j] !== ' ') B[s.charCodeAt(j++) - 97]++;
+        res = res * dict1[j - i] % 1000000007n, i = j + 1, j = 0;
+        while (j < 26) res = res * dict2[B[j++]] % 1000000007n;
+        B.fill(0);
+    }
+    return Number(res);
+}
