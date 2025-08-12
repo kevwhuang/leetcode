9ae2129bfc24da65be397c3f1dfca7d493e1bb1b
Kevin Huang <kevwhuang@gmail.com>
2025-08-11 23:34:35 -0500

    LC 3608/3623

--- problems/medium/3501-4000/3608.js
+++ problems/medium/3501-4000/3608.js
@@ -1,3 +1,14 @@
 // 3608 - Minimum Time for K Connected Components
 
-
+function minTime(n, edges, k) {
+    const find = v => v === uf[v] ? v : uf[v] = find(uf[v]);
+    edges.sort((a, b) => b[2] - a[2]);
+    const uf = Array.from({ length: n }, (_, i) => i);
+    for (let i = 0; i < edges.length; i++) {
+        const u = edges[i][0], v = edges[i][1];
+        if (find(u) === find(v)) continue;
+        uf[uf[u]] = uf[v];
+        if (--n < k) return edges[i][2];
+    }
+    return 0;
+}
--- problems/medium/3501-4000/3623.js
+++ problems/medium/3501-4000/3623.js
@@ -1,3 +1,13 @@
 // 3623 - Count Number of Trapezoids I
 
-
+function countTrapezoids(points) {
+    const map = new Map();
+    points.forEach(e => map.set(e[1], (map.get(e[1]) ?? 0) + 1));
+    let res = 0n, acc = 0n;
+    for (const e of map.values()) {
+        const cur = BigInt((e * e - e) / 2);
+        res = (res + acc * cur) % 1000000007n;
+        acc = (acc + cur) % 1000000007n;
+    }
+    return Number(res);
+}
