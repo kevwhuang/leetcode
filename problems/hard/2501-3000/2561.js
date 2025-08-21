7e131cc86497be76010b20c3b07b5eb1246cc402
Kevin Huang <kevwhuang@gmail.com>
2025-08-21 07:04:56 -0500

    LC 992/2561

--- problems/hard/2501-3000/2561.js
+++ problems/hard/2501-3000/2561.js
@@ -1,3 +1,21 @@
 // 2561 - Rearranging Fruits
 
-
+function minCost(basket1, basket2) {
+    let res = 0, arr = [], min = 1e9, n = basket1.length, i = -1;
+    const B1 = new Map(), B2 = new Map();
+    while (++i < n) {
+        const a = basket1[i], b = basket2[i];
+        min = Math.min(a, b, min);
+        B1.set(a, (B1.get(a) ?? 0) + 1);
+        B2.set(a, (B2.get(a) ?? 0) + 1).set(b, (B2.get(b) ?? 0) + 1);
+    }
+    for (const e of B2) {
+        const a = e[0], b = e[1];
+        if (b & 1) return -1;
+        let acc = Math.abs((B1.get(a) ?? 0) - b / 2);
+        while (~--acc) arr.push(a);
+    }
+    arr = new Uint32Array(arr).sort(), min *= 2, n = arr.length / 2, i = -1;
+    while (++i < n) res += Math.min(min, arr[i]);
+    return res;
+}
--- problems/hard/501-1000/992.js
+++ problems/hard/501-1000/992.js
@@ -1,3 +1,13 @@
 // 992 - Subarrays With K Different Integers
 
-
+function subarraysWithKDistinct(nums, k) {
+    let res = 0, l = 0, m = 0, r = -1;
+    const B = new Uint16Array(nums.length + 1);
+    while (++r < nums.length) {
+        if (++B[nums[r]] === 1 && --k < 0) B[nums[m++]] = 0, l = m;
+        if (k > 0) continue;
+        while (B[nums[m]] > 1) B[nums[m++]]--;
+        res += m - l + 1;
+    }
+    return res;
+}
