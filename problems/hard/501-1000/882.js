54fd92244a1fee22bbe1bd34ca714c159ac80263
Kevin Huang <kevwhuang@gmail.com>
2025-08-21 02:58:18 -0500

    LC 882

--- problems/hard/501-1000/882.js
+++ problems/hard/501-1000/882.js
@@ -1,3 +1,27 @@
 // 882 - Reachable Nodes in Subdivided Graph
 
-
+function reachableNodes(edges, maxMoves, n) {
+    const adj = Array.from({ length: n }, () => []);
+    edges.forEach(e => adj[e[0]].push([e[1], e[2] + 1]));
+    edges.forEach(e => adj[e[1]].push([e[0], e[2] + 1]));
+    const pq = new PriorityQueue((a, b) => a[1] - b[1]);
+    pq.enqueue([0, 0]);
+    const C = { 0: 0 };
+    let res = 1;
+    while (pq.size()) {
+        const arr = adj[pq.front()[0]], w = pq.dequeue()[1];
+        for (let i = 0; i < arr.length; i++) {
+            const v = arr[i][0], next = w + arr[i][1];
+            if (next > maxMoves || next >= C[v]) continue;
+            if (!C[v]) res++;
+            C[v] = next;
+            pq.enqueue([v, next]);
+        }
+    }
+    for (let i = 0; i < edges.length; i++) {
+        const a = maxMoves - C[edges[i][0]] || 0;
+        const b = maxMoves - C[edges[i][1]] || 0;
+        res += a + b - Math.max(0, a + b - edges[i][2]);
+    }
+    return res;
+}
--- problems/medium/2001-2500/2420.js
+++ problems/medium/2001-2500/2420.js
@@ -1,18 +1,14 @@
 // 2420 - Find All Good Indices
 
 function goodIndices(nums, k) {
-    const n = nums.length;
-    const prefix = new Array(n).fill(1);
-    for (let i = 1; i < n; i++) {
-        if (nums[i - 1] >= nums[i]) prefix[i] = prefix[i - 1] + 1;
-    }
-    const suffix = new Array(n).fill(1);
+    const n = nums.length, nn = n - k, ss = new Uint32Array(n).fill(1);
     for (let i = n - 2; ~i; i--) {
-        if (nums[i] <= nums[i + 1]) suffix[i] = suffix[i + 1] + 1;
+        if (nums[i] <= nums[i + 1]) ss[i] += ss[i + 1];
     }
-    const indices = [], lim = n - k;
-    for (let i = k; i < lim; i++) {
-        if (prefix[i - 1] >= k && suffix[i + 1] >= k) indices.push(i);
+    const res = [];
+    for (let acc = 1, i = 1; i < nn; i++) {
+        if (acc >= k && ss[i + 1] >= k) res.push(i);
+        acc = nums[i - 1] >= nums[i] ? acc + 1 : 1;
     }
-    return indices;
+    return res;
 }
