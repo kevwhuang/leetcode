8318084119af910b73417852677fd10abdae43d6
Kevin Huang <kevwhuang@gmail.com>
2025-08-22 03:49:54 -0500

    LC 2045/2699

--- problems/hard/2001-2500/2045.js
+++ problems/hard/2001-2500/2045.js
@@ -1,3 +1,24 @@
 // 2045 - Second Minimum Time to Reach Destination
 
-
+function secondMinimum(n, edges, time, change) {
+    const adj = Array.from({ length: n + 1 }, () => []);
+    edges.forEach(e => adj[e[0]].push(e[1]) && adj[e[1]].push(e[0]));
+    const C1 = new Uint32Array(n + 1), C2 = new Uint32Array(n + 1);
+    let res = time, Q = [1];
+    while (true) {
+        const N = [];
+        for (let i = 0; i < Q.length; i++) {
+            const arr = adj[Q[i]];
+            for (let j = 0; j < arr.length; j++) {
+                const v = arr[j];
+                if (C2[v]) continue;
+                if (C1[v] === 0) C1[v] = res;
+                else if (C2[v] === 0 && res > C1[v]) C2[v] = res;
+                if (v === n && C2[n]) return res;
+                N.push(v);
+            }
+        }
+        if (res / change >> 0 & 1) res += change - res % change;
+        res += time, Q = N;
+    }
+}
--- problems/hard/2501-3000/2699.js
+++ problems/hard/2501-3000/2699.js
@@ -1,3 +1,40 @@
 // 2699 - Modify Graph Edge Weights
 
-
+function modifiedGraphEdges(n, edges, source, destination, target) {
+    function check() {
+        C.fill(-1)[source] = 0;
+        const pq = new PriorityQueue((a, b) => a[1] - b[1]);
+        pq.enqueue([source, 0]);
+        while (pq.size()) {
+            const u = pq.front()[0], w = pq.dequeue()[1], arr = adj[u];
+            if (u === destination) return w;
+            for (let i = 0; i < arr.length; i++) {
+                const v = arr[i][0], next = w + arr[i][1];
+                if (next > target || next >= C[v]) continue;
+                C[v] = next;
+                pq.enqueue([v, next]);
+            }
+        }
+        return Infinity;
+    }
+    const adj = Array.from({ length: n }, () => []);
+    edges.forEach(e => ~e[2] && adj[e[0]].push([e[1], e[2]]));
+    edges.forEach(e => ~e[2] && adj[e[1]].push([e[0], e[2]]));
+    const C = new Uint32Array(n), cur = check();
+    if (cur < target) return [];
+    if (cur === target) edges.forEach(e => ~e[2] || (e[2] = 2e9));
+    if (cur === target) return edges;
+    for (let i = 0; i < edges.length; i++) {
+        const u = edges[i][0], v = edges[i][1], w = edges[i][2];
+        if (~w) continue;
+        edges[i][2] = 1;
+        adj[u].push([v, 1]);
+        adj[v].push([u, 1]);
+        const cur = check();
+        if (cur > target) continue;
+        edges[i][2] += target - cur;
+        edges.forEach(e => ~e[2] || (e[2] = 2e9));
+        return edges;
+    }
+    return [];
+}
