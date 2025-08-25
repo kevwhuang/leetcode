fbe7d2b18ee1bbb1009cdd12e202d14bb854efdc
Kevin Huang <kevwhuang@gmail.com>
2025-08-25 09:22:16 -0500

    LC 2959/3620

--- problems/hard/2501-3000/2959.js
+++ problems/hard/2501-3000/2959.js
@@ -1,3 +1,40 @@
 // 2959 - Number of Possible Sets of Closing Branches
 
-
+function numberOfSets(n, maxDistance, roads) {
+    function backtrack(i) {
+        if (i < n) arr[i]++ | backtrack(i + 1) | arr[i]--;
+        if (i < n) return backtrack(i + 1);
+        for (let u = 0; u < n; u++) {
+            if (arr[u] === 0) continue;
+            C.fill(-1)[u] = 0;
+            let Q = [[u, 0]];
+            while (Q.length) {
+                const N = [];
+                for (let j = 0; j < Q.length; j++) {
+                    const adj = M[Q[j][0]], w = Q[j][1];
+                    for (let v = 0; v < n; v++) {
+                        if (arr[v] === 0) continue;
+                        const next = w + adj[v];
+                        if (next >= C[v]) continue;
+                        C[v] = next;
+                        N.push([v, next]);
+                    }
+                }
+                Q = N;
+            }
+            for (let v = 0; v < n; v++) {
+                if (arr[v] && C[v] > maxDistance) return;
+            }
+        }
+        return res++;
+    }
+    const M = Array.from({ length: n }, () => new Uint32Array(n).fill(-1));
+    for (let i = 0; i < roads.length; i++) {
+        const u = roads[i][0], v = roads[i][1];
+        M[u][v] = M[v][u] = Math.min(roads[i][2], M[u][v]);
+    }
+    let res = 0;
+    const arr = new Uint8Array(n), C = new Uint32Array(n);
+    backtrack(0);
+    return res;
+}
--- problems/hard/3501-4000/3620.js
+++ problems/hard/3501-4000/3620.js
@@ -1,3 +1,32 @@
 // 3620 - Network Recovery Pathways
 
-
+function findMaxPathScore(edges, online, k) {
+    const n = online.length, set = new Set();
+    const adj = Array.from({ length: n }, () => []);
+    for (let i = 0; i < edges.length; i++) {
+        const u = edges[i][0], v = edges[i][1], w = edges[i][2];
+        if (!online[u] || !online[v] || w > k) continue;
+        adj[u].push([v, w]);
+        set.add(w);
+    }
+    const arr = new Uint32Array(set).sort(), C = new Array(n);
+    let flag, l = 0, r = arr.length - 1;
+    while (l <= r) {
+        C.fill(Infinity)[0] = 0, flag = true;
+        const m = l + r >> 1, tgt = arr[m];
+        while (flag) {
+            flag = false;
+            for (let u = 0; u < n; u++) {
+                const cur = adj[u], w = C[u];
+                if (w === Infinity) continue;
+                for (let i = 0; i < cur.length; i++) {
+                    const v = cur[i][0], ww = cur[i][1];
+                    if (ww >= tgt && w + ww < C[v]) C[v] = w + ww, flag = true;
+                }
+            }
+        }
+        if (C[n - 1] <= k) l = m + 1;
+        else r = m - 1;
+    }
+    return ~r ? arr[r] : r;
+}
