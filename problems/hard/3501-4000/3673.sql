62761c46e9aab828354586f22879ba57b7e9b36d
Kevin Huang <kevwhuang@gmail.com>
2025-09-26 20:58:51 -0500

    LC 3673/3689

--- problems/hard/3501-4000/3673.sql
+++ problems/hard/3501-4000/3673.sql
@@ -1,3 +1,23 @@
 -- 3673 - Find Zombie Sessions
 
-
+SELECT
+    session_id,
+    user_id,
+    TIMESTAMPDIFF(
+        MINUTE,
+        MIN(event_timestamp),
+        MAX(event_timestamp)
+    ) AS session_duration_minutes,
+    SUM(event_type = 'scroll') AS scroll_count
+FROM
+    App_Events
+GROUP BY
+    session_id
+HAVING
+    session_duration_minutes > 30
+    AND scroll_count >= 5
+    AND SUM(event_type = 'purchase') = 0
+    AND SUM(event_type = 'click') / scroll_count < 0.2
+ORDER BY
+    scroll_count DESC,
+    session_id;
--- problems/medium/3501-4000/3689.js
+++ problems/medium/3501-4000/3689.js
@@ -1,3 +1,5 @@
 // 3689 - Maximum Total Subarray Value I
 
-
+function maxTotalValue(nums, k) {
+    return k * (Math.max(...nums) - Math.min(...nums));
+}
