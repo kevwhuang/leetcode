94e37ef11d41abf1e9bab3ea5c1af8b3b9684d11
Kevin Huang <kevwhuang@gmail.com>
2025-09-28 04:15:46 -0500

    LC 3680/3682

--- problems/medium/3501-4000/3680.js
+++ problems/medium/3501-4000/3680.js
@@ -1,3 +1,15 @@
 // 3680 - Generate Schedule
 
-
+function generateSchedule(n) {
+    if (n < 5) return [];
+    const res = [];
+    for (let d = 2; d < n - 1; d++) {
+        for (let i = 0; i < n; i++) {
+            res.push([i, (i + d) % n]);
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        res.push([i, (i + 1) % n], [(i + n - 1) % n, (i + n - 2) % n]);
+    }
+    return res;
+}
--- problems/medium/3501-4000/3682.mjs
+++ problems/medium/3501-4000/3682.mjs
@@ -1,3 +1,13 @@
 // 3682 - Minimum Index Sum of Common Elements
 
-
+function minimumSum(nums1, nums2) {
+    const map = new Map(), n = nums1.length;
+    for (let i = 0; i < n; i++) {
+        if (!map.has(nums1[i])) map.set(nums1[i], i);
+    }
+    let res = 500000;
+    for (let i = 0; i < n; i++) {
+        if (map.has(nums2[i])) res = Math.min(map.get(nums2[i]) + i, res);
+    }
+    return res < 500000 ? res : -1;
+}
