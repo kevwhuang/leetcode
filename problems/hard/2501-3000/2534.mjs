12ea768478cc1bb53e3ee0facd9529fa4a1d1c85
Kevin Huang <kevwhuang@gmail.com>
2025-10-02 01:25:20 -0500

    LC 2534/3699

--- problems/hard/2501-3000/2534.mjs
+++ problems/hard/2501-3000/2534.mjs
@@ -1,3 +1,18 @@
 // 2534 - Time Taken to Cross the Door
 
-
+function timeTaken(arrival, state) {
+    const arr1 = [], arr2 = [];
+    state.forEach((e, i) => (e ? arr2 : arr1).push(i));
+    const res = new Uint32Array(state.length), m = arr1.length, n = arr2.length;
+    let prev, cur = 0, i = 0, j = 0;
+    while (i < m && j < n) {
+        const a = arrival[arr1[i]], b = arrival[arr2[j]];
+        if (prev === 0 && cur >= a) res[arr1[i++]] = cur++;
+        else if (prev === 1 && cur >= b) res[arr2[j++]] = cur++;
+        else if (a < b) prev = 0, cur = (res[arr1[i++]] = Math.max(cur, a)) + 1;
+        else prev = 1, cur = (res[arr2[j++]] = Math.max(cur, b)) + 1;
+    }
+    while (i < m) cur = (res[arr1[i]] = Math.max(cur, arrival[arr1[i++]])) + 1;
+    while (j < n) cur = (res[arr2[j]] = Math.max(cur, arrival[arr2[j++]])) + 1;
+    return res;
+}
--- problems/hard/3501-4000/3699.js
+++ problems/hard/3501-4000/3699.js
@@ -1,3 +1,12 @@
 // 3699 - Number of Zigzag Arrays I
 
-
+function zigZagArrays(n, l, r) {
+    const m = r - l, dp = new Uint32Array(m + 1).fill(1), mod = 1000000007;
+    while (--n) {
+        const d = n % 2 ? 1 : -1;
+        for (let cur, prev = 0, i = n % 2 ? 0 : m; ~i && i <= m; i += d) {
+            cur = dp[i], dp[i] = prev, prev = (prev + cur) % mod;
+        }
+    }
+    return 2 * dp.reduce((s, e) => s + e % mod) % mod;
+}
