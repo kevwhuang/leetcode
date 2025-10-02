352f9da378e815ed412f22866056912abc16d08e
Kevin Huang <kevwhuang@gmail.com>
2025-10-01 21:57:56 -0500

    LC 765/3695

--- problems/hard/3501-4000/3695.js
+++ problems/hard/3501-4000/3695.js
@@ -1,3 +1,23 @@
 // 3695 - Maximize Alternating Sum Using Swaps
 
-
+function maxAlternatingSum(nums, swaps) {
+    const find = v => v === uf[v] ? v : uf[v] = find(uf[v]);
+    if (!this.uf) uf = new Uint32Array(100000);
+    nums.forEach((_, i) => uf[i] = i);
+    swaps.forEach(e => uf[find(e[0])] = find(e[1]));
+    const map = new Map();
+    for (let u = 0; u < nums.length; u++) {
+        const v = find(u), arr = map.get(v) ?? [0];
+        if (!map.has(v)) map.set(v, arr);
+        if (u % 2 < 1) arr[0]--;
+        arr.push(nums[u]);
+    }
+    let res = 0;
+    for (const e of map.values()) {
+        e.sort((a, b) => a - b);
+        let acc = e[0], i = e.length - 1;
+        while (acc++) res += e[i--];
+        while (i) res -= e[i--];
+    }
+    return res;
+}
--- problems/hard/501-1000/765.js
+++ problems/hard/501-1000/765.js
@@ -1,3 +1,10 @@
 // 765 - Couples Holding Hands
 
-
+function minSwapsCouples(row) {
+    const find = v => v === uf[v] ? v : uf[v] = find(uf[v]);
+    const uf = Array.from({ length: row.length / 2 }, (_, v) => v);
+    for (let i = 0; i < row.length; i += 2) {
+        uf[find(row[i] >> 1)] = find(row[i + 1] >> 1);
+    }
+    return uf.reduce((s, _, v) => s - (v === find(v)), uf.length);
+}
