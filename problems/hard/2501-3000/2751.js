a09ec95d845111ffb6bfce26c50c0588adb52d33
Kevin Huang <kevwhuang@gmail.com>
2025-10-02 04:43:54 -0500

    LC 2751

--- problems/hard/2501-3000/2751.js
+++ problems/hard/2501-3000/2751.js
@@ -1,3 +1,18 @@
 // 2751 - Robot Collisions
 
-
+function survivedRobotsHealths(positions, healths, directions) {
+    const res = healths, S = [];
+    const arr = Array.from({ length: res.length }, (_, i) => i);
+    arr.sort((a, b) => positions[a] - positions[b]);
+    for (let i = 0; i < arr.length; i++) {
+        const j = arr[i];
+        if (directions[j] === 'R' && S.push(j)) continue;
+        while (res[j] && S.length) {
+            const k = res[j] < res[S.at(-1)] ? S.at(-1) : S.pop();
+            if (res[j] < res[k]) res[j] = 0, res[k]--;
+            else if (res[j] > res[k]) res[j]--, res[k] = 0;
+            else res[j] = res[k] = 0;
+        }
+    }
+    return res.filter(Boolean);
+}
