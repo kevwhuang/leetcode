18c8f526a5f615d12ffc70a13789cbf397905909
Kevin Huang <kevwhuang@gmail.com>
2025-10-20 05:56:42 -0500

    1458

--- problems/hard/1001-1500/1458.js
+++ problems/hard/1001-1500/1458.js
@@ -1,3 +1,16 @@
 // 1458 - Max Dot Product of Two Subsequences
 
-
+function maxDotProduct(nums1, nums2) {
+    const m = nums1.length, n = nums2.length;
+    if (m < n) return maxDotProduct(nums2, nums1);
+    const dp = new Int32Array(n + 1).fill(-1000000);
+    for (let i = 0; i < m; i++) {
+        for (let prev = 0, j = 0; j < n; j++) {
+            const cur = dp[j + 1];
+            const max = Math.max(prev, 0) + nums1[i] * nums2[j];
+            dp[j + 1] = Math.max(max, dp[j], cur);
+            prev = cur;
+        }
+    }
+    return dp[n];
+}
