500f64d2acbdda204e26b8f581c9971dd691d883
Kevin Huang <kevwhuang@gmail.com>
2025-11-02 15:26:35 -0600

    LC 3723/3727

--- problems/medium/3501-4000/3723.js
+++ problems/medium/3501-4000/3723.js
@@ -1,3 +1,7 @@
 // 3723 - Maximize Sum of Squares of Digits
 
-
+function maxSumOfSquares(num, sum) {
+    if (9 * num < sum) return '';
+    const a = sum / 9 >> 0, b = sum - 9 * a;
+    return ('9'.repeat(a) + (b ? b : '')).padEnd(num, '0');
+}
--- problems/medium/3501-4000/3727.js
+++ problems/medium/3501-4000/3727.js
@@ -1,3 +1,14 @@
 // 3727 - Maximum Alternating Sum of Squares
 
-
+function maxAlternatingSum(nums) {
+    const arr = new Uint16Array(nums.length);
+    nums.forEach((e, i) => arr[i] = Math.abs(e));
+    arr.sort();
+    let res = 0, l = 0, r = arr.length - 1;
+    while (l <= r) {
+        res += arr[r--] ** 2;
+        if (l > r) break;
+        res -= arr[l++] ** 2;
+    }
+    return res;
+}
