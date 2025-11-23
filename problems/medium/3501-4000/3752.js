ba52fc32c08a1e3d67e2c343ab5a69a789b0583f
Kevin Huang <kevwhuang@gmail.com>
2025-11-23 03:20:20 -0600

    LC 3752/3756

--- problems/medium/3501-4000/3752.js
+++ problems/medium/3501-4000/3752.js
@@ -1,3 +1,14 @@
 // 3752 - Lexicographically Smallest Negated Permutation That Sums to Target
 
-
+function lexSmallestNegatedPerm(n, target) {
+    let acc = n * (n + 1) / 2;
+    if (acc < Math.abs(target) || (acc - target) % 2) return [];
+    acc -= target;
+    const res = new Int32Array(n);
+    let i = n + 1, l = 0, r = n - 1;
+    while (--i) {
+        if (i + i > acc) res[r--] = i;
+        else res[l++] = -i, acc -= i + i;
+    }
+    return res;
+}
--- problems/medium/3501-4000/3756.js
+++ problems/medium/3501-4000/3756.js
@@ -1,3 +1,30 @@
 // 3756 - Concatenate Non-Zero Digits and Multiply by Sum II
 
-
+function sumAndMultiply(s, queries) {
+    function init() {
+        ps1 = new Uint32Array(100001);
+        ps2 = new BigUint64Array(100001);
+        ps3 = new BigUint64Array(100001);
+        dict = new BigUint64Array(100001).fill(1n, 0, 1);
+        let i = 0;
+        while (++i < 100001) dict[i] = 10n * dict[i - 1] % mod;
+    }
+    const mod = 1000000007n;
+    if (!this.dict) init();
+    ps1[0] = s[0] === '0' ? 0 : 1;
+    ps2[0] = ps3[0] = BigInt(s[0]);
+    for (let i = 1; i < s.length; i++) {
+        const cur = BigInt(s[i]);
+        ps1[i] = ps1[i - 1] + (cur ? 1 : 0);
+        ps2[i] = cur ? (10n * ps2[i - 1] + cur) % mod : ps2[i - 1];
+        ps3[i] = ps3[i - 1] + cur;
+    }
+    for (let i = 0; i < queries.length; i++) {
+        const l = queries[i][0], r = queries[i][1];
+        const j = ps1[r] - (l ? ps1[l - 1] : 0);
+        const a = ps2[r] - (l ? ps2[l - 1] : 0n) * dict[j] % mod + mod;
+        const b = ps3[r] - (l ? ps3[l - 1] : 0n);
+        queries[i] = Number(a * b % mod);
+    }
+    return queries;
+}
