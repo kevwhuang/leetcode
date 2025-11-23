4f03ab17e7913786cd5eaa7c9489b12adf4660e4
Kevin Huang <kevwhuang@gmail.com>
2025-11-23 01:05:12 -0600

    LC 3751/3755

--- problems/medium/3501-4000/3751.js
+++ problems/medium/3501-4000/3751.js
@@ -1,3 +1,18 @@
 // 3751 - Total Waviness of Numbers in Range I
 
-
+function totalWaviness(num1, num2) {
+    function init() {
+        dict = new Uint32Array(100001);
+        for (let i = 100; i <= 100000; i++) {
+            let acc = 0, cur1 = i / 100 >> 0, cur2 = i / 10 >> 0, c = i % 10;
+            while (cur1) {
+                const a = cur1 % 10, b = cur2 % 10;
+                if (b > a && b > c || b < a && b < c) acc++;
+                cur1 = cur1 / 10 >> 0, cur2 = cur2 / 10 >> 0, c = b;
+            }
+            dict[i] = dict[i - 1] + acc;
+        }
+    }
+    if (!this.dict) init();
+    return dict[num2] - dict[num1 - 1];
+}
--- problems/medium/3501-4000/3755.js
+++ problems/medium/3501-4000/3755.js
@@ -1,3 +1,15 @@
 // 3755 - Find Maximum Balanced XOR Subarray Length
 
-
+function maxBalancedSubarray(nums) {
+    let res = 0, xor = 0, acc = 0, i = -1;
+    const map = new Map([[0, -1]]);
+    while (++i < nums.length) {
+        xor ^= nums[i];
+        if (nums[i] & 1) acc++;
+        else acc--;
+        const key = 1000000 * xor + acc;
+        if (!map.has(key)) map.set(key, i);
+        else res = Math.max(i - map.get(key), res);
+    }
+    return res;
+}
