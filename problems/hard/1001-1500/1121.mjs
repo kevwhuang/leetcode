778a89cffaaf586bc0d17e1b809374f8c654c111
Kevin Huang <kevwhuang@gmail.com>
2025-11-23 18:31:54 -0600

    LC 1121/1274

--- problems/hard/1001-1500/1121.mjs
+++ problems/hard/1001-1500/1121.mjs
@@ -1,3 +1,13 @@
 // 1121 - Divide Array Into Increasing Sequences
 
-
+function canDivideIntoSubsequences(nums, k) {
+    const map = new Map(), n = nums.length;
+    let max = 1, i = -1;
+    while (++i < n) {
+        const next = (map.get(nums[i]) ?? 0) + 1;
+        map.set(nums[i], next);
+        max = Math.max(next, max);
+        if (k * max > n) return false;
+    }
+    return true;
+}
--- problems/hard/1001-1500/1274.mjs
+++ problems/hard/1001-1500/1274.mjs
@@ -1,3 +1,14 @@
 // 1274 - Number of Ships in a Rectangle
 
-
+function countShips(sea, topRight, bottomLeft) {
+    if (!sea.hasShips(topRight, bottomLeft)) return 0;
+    const r1 = bottomLeft[0], r2 = topRight[0];
+    const c1 = bottomLeft[1], c2 = topRight[1];
+    if (r1 === r2 && c1 === c2) return 1;
+    const m1 = r1 + r2 >> 1, m2 = c1 + c2 >> 1;
+    const a = countShips(sea, [m1, m2], bottomLeft);
+    const b = countShips(sea, [m1, c2], [r1, m2 + 1]);
+    const c = countShips(sea, [r2, m2], [m1 + 1, c1]);
+    const d = countShips(sea, topRight, [m1 + 1, m2 + 1]);
+    return a + b + c + d;
+}
