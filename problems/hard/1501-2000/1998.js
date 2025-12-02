13e81357a58041c24ad3f05e2e2dfd16a6a1fbed
Kevin Huang <kevwhuang@gmail.com>
2025-12-02 07:18:12 -0600

    1998

--- problems/hard/1501-2000/1998.js
+++ problems/hard/1501-2000/1998.js
@@ -1,3 +1,21 @@
 // 1998 - GCD Sort of an Array
 
-
+function gcdSort(nums) {
+    const find = v => v === uf[v] ? v : uf[v] = find(uf[v]);
+    const uf = Array.from({ length: Math.max(...nums) + 1 }, (_, v) => v);
+    for (let i = 0; i < nums.length; i++) {
+        const u = nums[i];
+        if (u > uf[u]) continue;
+        let cur = u;
+        const sqrt = Math.sqrt(u);
+        for (let v = 2; v <= sqrt; v++) {
+            if (cur % v) continue;
+            uf[find(u)] = find(v);
+            while (cur % v === 0) cur /= v;
+            if (cur === 1) break;
+        }
+        if (cur > 1) uf[find(u)] = find(cur);
+    }
+    const arr = new Uint32Array(nums).sort();
+    return nums.every((e, i) => find(e) === find(arr[i]));
+}
