7e77bb43b174f6a3d17f6dceded7ad955cdc5fbb
Kevin Huang <kevwhuang@gmail.com>
2025-12-07 04:40:48 -0600

    3766/3767

--- problems/medium/3501-4000/3766.js
+++ problems/medium/3501-4000/3766.js
@@ -1,3 +1,19 @@
 // 3766 - Minimum Operations to Make Binary Palindrome
 
-
+function minOperations(nums) {
+    function init() {
+        dict = [];
+        for (let i = 1, j = 1, k; i <= 5049; i++) {
+            let l = 31 - Math.clz32(i), r = 0;
+            while (l > r) if ((i >> l-- ^ i >> r++) & 1) l = -1;
+            if (l < 0) continue;
+            while (j < i) dict[j] = Math.min(j - k, i - j++);
+            k = j;
+        }
+    }
+    if (!this.dict) init();
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = dict[nums[i]];
+    }
+    return nums;
+}
--- problems/medium/3501-4000/3767.js
+++ problems/medium/3501-4000/3767.js
@@ -1,3 +1,11 @@
 // 3767 - Maximize Points After Choosing K Tasks
 
-
+function maxPoints(technique1, technique2, k) {
+    const A1 = technique1, A2 = technique2, n = A1.length;
+    const ind = Array.from({ length: n }, (_, i) => i);
+    ind.sort((a, b) => A1[b] + A2[a] - A1[a] - A2[b]);
+    let res = 0, i = 0;
+    while (i < k) res += A1[ind[i++]];
+    while (i < n) res += Math.max(A1[ind[i]], A2[ind[i++]]);
+    return res;
+}
