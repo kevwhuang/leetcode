dc335536afb36b90f1347e04453c81c4128d36f8
Kevin Huang <kevwhuang@gmail.com>
2025-12-07 08:00:21 -0600

    940/1987

--- problems/hard/1501-2000/1987.js
+++ problems/hard/1501-2000/1987.js
@@ -1,3 +1,11 @@
 // 1987 - Number of Unique Good Subsequences
 
-
+function numberOfUniqueGoodSubsequences(binary) {
+    let a = 0, b = 0, c = 0;
+    const mod = 1000000007;
+    for (let i = 0; i < binary.length; i++) {
+        if (binary[i] === '0') a = (a + b) % mod, c = 1;
+        else b = (a + b + 1) % mod;
+    }
+    return (a + b + c) % mod;
+}
--- problems/hard/501-1000/940.js
+++ problems/hard/501-1000/940.js
@@ -1,3 +1,11 @@
 // 940 - Distinct Subsequences II
 
-
+function distinctSubseqII(s) {
+    let res = 0, i = -1;
+    const seen = new Uint32Array(26), n = s.length, mod = 1000000007;
+    while (++i < n) {
+        const j = s.charCodeAt(i) - 97, prev = seen[j];
+        seen[j] = (res + 1) % mod, res = (2 * res + mod - prev + 1) % mod;
+    }
+    return res;
+}
