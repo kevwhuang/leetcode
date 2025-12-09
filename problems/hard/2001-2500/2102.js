afc3531d84168dc80944deadaf3e7d584ef48949
Kevin Huang <kevwhuang@gmail.com>
2025-12-08 19:19:37 -0600

    2102

--- problems/hard/2001-2500/2102.js
+++ problems/hard/2001-2500/2102.js
@@ -1,3 +1,19 @@
 // 2102 - Sequentially Ordinal Rank Tracker
 
-
+class SORTracker {
+    constructor() {
+        const f1 = (a, b) => b[0] - a[0] || a[1].localeCompare(b[1]);
+        const f2 = (a, b) => a[0] - b[0] || b[1].localeCompare(a[1]);
+        this.heap1 = new PriorityQueue(f1);
+        this.heap2 = new PriorityQueue(f2);
+    }
+    add(name, score) {
+        this.heap2.enqueue([score, name]);
+        this.heap1.enqueue(this.heap2.dequeue());
+    }
+    get() {
+        const res = this.heap1.front()[1];
+        this.heap2.enqueue(this.heap1.dequeue());
+        return res;
+    }
+}
--- problems/medium/3501-4000/3767.js
+++ problems/medium/3501-4000/3767.js
@@ -1,11 +1,16 @@
 // 3767 - Maximize Points After Choosing K Tasks
 
 function maxPoints(technique1, technique2, k) {
-    const A1 = technique1, A2 = technique2, n = A1.length;
-    const ind = Array.from({ length: n }, (_, i) => i);
-    ind.sort((a, b) => A1[b] + A2[a] - A1[a] - A2[b]);
-    let res = 0, i = 0;
-    while (i < k) res += A1[ind[i++]];
-    while (i < n) res += Math.max(A1[ind[i]], A2[ind[i++]]);
+    let res = 0, A3 = [];
+    const A1 = technique1, A2 = technique2;
+    for (let i = 0; i < A1.length; i++) {
+        const d = A2[i] - A1[i];
+        res += d < 0 ? A1[i] : A2[i];
+        if (d <= 0) k--;
+        else A3.push(d);
+    }
+    if (k <= 0) return res;
+    A3 = new Uint32Array(A3).sort();
+    while (~--k) res -= A3[k];
     return res;
 }
