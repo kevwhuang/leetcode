9892005013c92bef6d840f29be8473a2137a4772
Kevin Huang <kevwhuang@gmail.com>
2026-02-15 04:03:45 -0600

    3844

--- problems/medium/3501-4000/3844.js
+++ problems/medium/3501-4000/3844.js
@@ -1,3 +1,17 @@
 // 3844 - Longest Almost Palindromic Substring
 
-
+function almostPalindromic(s) {
+    function check(acc, l, r, flag) {
+        while (~l && r < n && s[l] === s[r]) acc += 2, l--, r++;
+        if (l < 0 && r === n || flag) return acc;
+        if (l < 0 && r < n || ~l && r === n) return acc + 1;
+        return acc + Math.max(check(1, l - 1, r, 1), check(1, l, r + 1, 1));
+    }
+    let res = 2;
+    const n = s.length;
+    for (let i = 1; i < n; i++) {
+        res = Math.max(check(1, i - 1, i + 1), res);
+        if (s[i - 1] === s[i]) res = Math.max(check(2, i - 2, i + 1), res);
+    }
+    return res;
+}
