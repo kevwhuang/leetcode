d57bbcfbf8cd6a9582709ef62de55efe64536333
Kevin Huang <kevwhuang@gmail.com>
2026-02-18 04:37:11 -0600

    403/2296

--- problems/hard/1-500/403.js
+++ problems/hard/1-500/403.js
@@ -1,3 +1,22 @@
 // 403 - Frog Jump
 
-
+function canCross(stones) {
+    if (stones[1] !== 1) return false;
+    const map = new Map(), n = stones.length;
+    for (let i = 1; i < n; i++) {
+        map.set(stones[i], new Set());
+    }
+    map.get(1).add(1);
+    const set = map.get(stones[n - 1]);
+    for (let i = 1; i < n; i++) {
+        const cur = stones[i];
+        for (const a of map.get(cur)) {
+            const b = a + 1, c = a - 1;
+            if (map.has(cur + a)) map.get(cur + a).add(a);
+            if (map.has(cur + b)) map.get(cur + b).add(b);
+            if (c && map.has(cur + c)) map.get(cur + c).add(c);
+        }
+        if (set.size) return true;
+    }
+    return false;
+}
--- problems/hard/2001-2500/2296.js
+++ problems/hard/2001-2500/2296.js
@@ -1,3 +1,30 @@
 // 2296 - Design a Text Editor
 
-
+class TextEditor {
+    constructor() {
+        this.S1 = [];
+        this.S2 = [];
+    }
+    addText(text) {
+        for (let i = 0; i < text.length; i++) {
+            this.S1.push(text[i]);
+        }
+    }
+    cursorLeft(k) {
+        while (k-- && this.S1.length) this.S2.push(this.S1.pop());
+        let res = '', i = this.S1.length;
+        while (res.length < 10 && ~--i) res = this.S1[i] + res;
+        return res;
+    }
+    cursorRight(k) {
+        while (k-- && this.S2.length) this.S1.push(this.S2.pop());
+        let res = '', i = this.S1.length;
+        while (res.length < 10 && ~--i) res = this.S1[i] + res;
+        return res;
+    }
+    deleteText(k) {
+        const kk = k;
+        while (k-- && this.S1.length) this.S1.pop();
+        return kk - k - 1;
+    }
+}
