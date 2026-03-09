f5396f9e73907982a467ddfc0e1865d8db12447d
Kevin Huang <kevwhuang@gmail.com>
2026-03-09 03:11:10 -0500

    3853/3854/3857/3860/3861

--- problems/easy/3501-4000/3861.js
+++ problems/easy/3501-4000/3861.js
@@ -1,3 +1,10 @@
 // 3861 - Minimum Capacity Box
 
-
+function minimumIndex(capacity, itemSize) {
+    let res = -1;
+    for (let i = 0; i < capacity.length; i++) {
+        if (capacity[i] < itemSize) continue;
+        if (res < 0 || capacity[i] < capacity[res]) res = i;
+    }
+    return res;
+}
