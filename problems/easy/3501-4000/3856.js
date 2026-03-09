aa226a19ddafa6e0b4b9ed07bdcf2cc72d5790fe
Kevin Huang <kevwhuang@gmail.com>
2026-03-09 01:07:20 -0500

    3852/3856/3858/3862/3863

--- problems/easy/3501-4000/3856.js
+++ problems/easy/3501-4000/3856.js
@@ -1,3 +1,8 @@
 // 3856 - Trim Trailing Vowels
 
-
+function trimTrailingVowels(s) {
+    const A = ['a', 'e', 'i', 'o', 'u'];
+    let i = s.length - 1;
+    while (~i && A.includes(s[i])) i--;
+    return s.slice(0, i + 1);
+}
